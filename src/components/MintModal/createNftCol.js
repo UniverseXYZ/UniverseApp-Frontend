c5af72ca74371070ba4c31a242258a810f8fc20b
armen5 <arm.khachatryan11@gmail.com>
2021-03-26 17:16:16 +0400

    Single NFT and Collectable modal

--- src/components/MintModal/createNftCol.js
+++ src/components/MintModal/createNftCol.js
@@ -1,6 +1,67 @@
-const CreateNftCol = ({   }) =>{
+// import Collection from '../../assets/images/Collection.svg';
+// import Collection1 from '../../assets/images/Collection1.svg';
+import Button from '../Button'
+import Input from '../Input'
+import defaultImage from '../../assets/images/default-image.png'
+import infoIcon from '../../assets/images/icon.png'
+import arrow from '../../assets/images/Arrow.png'
+import { useRef, useState } from 'react'
+
+const CreateNftCol = ({ onClick }) =>{
+
+    const [hideIcon, setHideIcon] = useState(true)
+    const [previewImage, setPreviewImage]=useState(null)
+    const inputFile = useRef(null)
+
     return(
-    <h1>Create NFT Collectible</h1>
-    )
+        <div className="mintNftCollection-div">
+            <div className="back-nft" onClick={() => onClick(null)}><img src={arrow} alt="back"/><span>MINT NFT COLLECTION</span></div>
+            <div className="nft-collectible">
+                <h2 className="nft-coll-title">Create NFT Collectible</h2>
+                <div className="nft-coll-content">
+                    <div className="nft-coll-upload">
+                        <h5>Upload file</h5>
+                        <div className="nft-coll-upload-file">
+                            <p>PNG, GIF, WEBP, MP4 or MP3. Max 30mb</p>
+                            <Button className="light-border-button" onClick={()=>inputFile.current.click()}>CHOOSE FILE</Button>
+                            <input type="file" className="inp-disable" ref={inputFile}
+                                onChange={(e)=>setPreviewImage(e.target.files[0])}/>
+                        </div>
+                        <div className="nft-coll-name">
+                            <h5>Name</h5>
+                            <Input className="inp"/>
+                        </div>
+                        <div className="nft-coll-description">
+                            <h5>Description (optional)</h5>
+                            <Input className="inp"/>
+                        </div>
+                        <div className="nft-coll-editions">
+                            <h5>Number of editions <img onMouseOver={()=>setHideIcon(false)} 
+                                    onMouseLeave={()=>setHideIcon(true)} 
+                                    src={infoIcon}/>
+                            </h5>
+                            <div hidden={hideIcon} className="info-text">
+                                <p>Total amount of NFTs that will be distributed to the current revard tier winners</p>
+                            </div>
+                            <Input className="inp"/>
+                        </div>
+                        <div className="nft-coll-buttons">
+                            <Button className="light-button">ADD TO COLLECTION</Button>
+                            <Button className="light-border-button">ADD AND CREATE NEW</Button>
+                        </div>
+                    </div>
+                    <div className="nft-coll-preview">
+                        <h5>Preview</h5>
+                        <div className="nft-coll-picture">
+                        {previewImage ? 
+                                    <img className="preview-image" src={URL.createObjectURL(previewImage)} alt='Cover' />
+                                :
+                                    <img className="default-image" src={defaultImage} alt='Cover' />}
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+        )
     }
 export default CreateNftCol
--- src/components/MintModal/mintSingleNft.js
+++ src/components/MintModal/mintSingleNft.js
@@ -1,11 +1,62 @@
 import arrow from '../../assets/images/Arrow.png'
+import infoIcon from '../../assets/images/icon.png'
+import defaultImage from '../../assets/images/default-image.png'
+import Button from '../Button'
+import Input from '../Input'
+import { useRef, useState } from 'react'
+
 const MintSingleNft = ({onClick}) => {
+
+    const [hideIcon, setHideIcon] = useState(true)
+    const [previewImage, setPreviewImage]=useState(null)
+    const inputFile = useRef(null)
+
     return (
     <div className="mintNftCollection-div">
-    <div className="back-nft" onClick={() => onClick(null)}><img src={arrow} alt="back"/><span>MINT NFT</span></div>
-    <h2>Mint Single NFT</h2>
-</div>
-)
+        <div className="back-nft" onClick={() => onClick(null)}><img src={arrow} alt="back"/><span>MINT NFT</span></div>
+        <h2 className="single-nft-title">Mint Single NFT</h2>
+        <div className="single-nft-content">
+            <div className="single-nft-upload">
+                <h5>Upload file</h5>
+                <div className="single-nft-upload-file">
+                    <p>PNG, GIF, WEBP, MP4 or MP3. Max 30mb</p>
+                    <Button className="light-border-button" onClick={()=>inputFile.current.click()}>CHOOSE FILE</Button>
+                    <input type="file" className="inp-disable" ref={inputFile}
+                        onChange={(e)=>setPreviewImage(e.target.files[0])}/>
+                </div>
+                <div className="single-nft-name">
+                    <h5>Name</h5>
+                    <Input className="inp"/>
+                </div>
+                <div className="single-nft-description">
+                    <h5>Description (optional)</h5>
+                    <Input className="inp"/>
+                </div>
+                <div className="single-nft-editions">
+                    <h5>Number of editions <img src={infoIcon} onMouseOver={()=>setHideIcon(false)}
+                    onMouseLeave={()=>setHideIcon(true)}/></h5>
+                    <div hidden={hideIcon} className="info-text">
+                        <p>NFTs are minted to our auction contract by default. Turn the toggle on if you want them to be minted to your wallet instead</p>
+                    </div>    
+                    <Input className="inp"/>
+                </div>
+                <div className="single-nft-buttons">
+                    <Button className="light-button">MINT NOW</Button>
+                    <Button className="light-border-button">SAVE FOR LATER</Button>
+                </div>
+            </div>
+            <div className="single-nft-preview">
+                <h5>Preview</h5>
+                <div className="single-nft-picture">
+                {previewImage ? 
+                    <img className="preview-image" src={URL.createObjectURL(previewImage)} alt='Cover' />
+                    :
+                    <img className="default-image" src={defaultImage} alt='Cover' />}
+                </div>
+            </div>
+        </div>
+    </div>
+    )
 }
 
 export default MintSingleNft;
