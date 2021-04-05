89aedb2d6d57baf805d9043dcbc565ec8b5d00fb
VahanMuradyan <10vahan.m16@gmail.com>
2021-04-05 18:41:08 +0400

    Tablet and Mobile versions for header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,45 @@
+import { useEffect, useState } from 'react';
 import './Header.scss';
 import appLogo from '../../assets/images/dark.svg';
-import Nav from './Nav';
 import { Link } from 'react-router-dom';
+import DesktopView from './dimensions/DesktopView';
+import TabletView from './dimensions/TabletView';
+import MobileView from './dimensions/MobileView';
 
 const Header = () => {
+    const PLACEHOLDER_ETHEREUM_ADDRESS = '0x5493a5a6f...ef8b';
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+        window.addEventListener("resize", handleResize);
+        handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return (
         <header>
             <div className='app__logo'>
                 <Link to={'/'}><img src={appLogo} alt='App Logo' /></Link>
             </div>
-            <div className='nav'>
-                <Nav />
-            </div>
+            {windowSize.width > 992 &&
+                <DesktopView ethereumAddress={PLACEHOLDER_ETHEREUM_ADDRESS} />
+            }
+            {windowSize.width <= 992 && windowSize.width > 576 &&
+                <TabletView ethereumAddress={PLACEHOLDER_ETHEREUM_ADDRESS} />
+            }
+            {windowSize.width <= 576 &&
+                <MobileView ethereumAddress={PLACEHOLDER_ETHEREUM_ADDRESS} />
+            }
         </header>
     )
 }
