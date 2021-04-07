7fc5dc12af194c95a51300e555f3fffcc3d357a2
VahanMuradyan <10vahan.m16@gmail.com>
2021-04-07 13:03:55 +0400

    Homepage done

---
+++ src/components/homepage/BuyUniverseNFTs.jsx
@@ -0,0 +1,20 @@
+import buyUniverseNFTsImg from '../../assets/images/buy-universe-nft.png';
+
+const BuyUniverseNFTs = () => {
+    return (
+        <div className='buy__universe__nfts__section'>
+            <div className='buy__universe__nfts__section__container'>
+                <div>
+                    <h1 className='title'>Buy Universe NFTs</h1>
+                    <p className='desc'>A constant stream of default tokens are released every day. These tokens are distributed between investors, contributors, and the community.</p>
+                    <span className='coming__soon'>Coming soon</span>
+                </div>
+                <div>
+                    <img src={buyUniverseNFTsImg} alt='Buy Universe NFTs' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BuyUniverseNFTs;
