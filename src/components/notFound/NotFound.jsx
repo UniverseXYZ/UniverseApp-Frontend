6c954f9b0474b349ae888d0aaa170e9d799d222c
VahanMuradyan <10vahan.m16@gmail.com>
2021-05-25 17:30:53 +0400

    404 page

---
+++ src/components/notFound/NotFound.jsx
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import AppContext from '../../ContextAPI';
+import Button from '../button/Button';
+import './NotFound.scss';
+import notFoundImg from '../../assets/images/404img.png';
+
+const NotFound = () => {
+  const { setWebsite } = useContext(AppContext);
+  const history = useHistory();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setWebsite(false);
+  }, []);
+  return (
+    <div className="not__found__page">
+      <div className="not__found__page__box">
+        <h1>
+          <img src={notFoundImg} alt="404" />
+        </h1>
+        <p>Oops.. page not found</p>
+        <Button className="light-button" onClick={() => history.push('/')}>
+          Go back
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
