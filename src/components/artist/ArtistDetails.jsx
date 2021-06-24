3cf03d7fbcc82047e9396f6ced4e7032477038e7
VahanMuradyan <10vahan.m16@gmail.com>
2021-06-24 17:05:13 +0400

    my profile changes

--- src/components/artist/ArtistDetails.jsx
+++ src/components/artist/ArtistDetails.jsx
@@ -75,22 +75,30 @@ const ArtistDetails = ({ onArtist }) => {
                 <h1 className="title">{loggedInArtist.name}</h1>
                 <p className="desc">{loggedInArtist.about}</p>
                 <div className="social__links">
-                  <a
-                    href={`https://www.instagram.com/${loggedInArtist.instagramLink}`}
-                    target="_blank"
-                    title={`https://www.instagram.com/${loggedInArtist.instagramLink}`}
-                    rel="noreferrer"
-                  >
-                    <img src={instagramIcon} alt="Instagram" />
-                  </a>
-                  <a
-                    href={`https://twitter.com/${loggedInArtist.twitterLink}`}
-                    target="_blank"
-                    title={`https://twitter.com/${loggedInArtist.twitterLink}`}
-                    rel="noreferrer"
-                  >
-                    <img src={twitterIcon} alt="Twitter" />
-                  </a>
+                  {loggedInArtist.instagramLink ? (
+                    <a
+                      href={`https://www.instagram.com/${loggedInArtist.instagramLink}`}
+                      target="_blank"
+                      title={`https://www.instagram.com/${loggedInArtist.instagramLink}`}
+                      rel="noreferrer"
+                    >
+                      <img src={instagramIcon} alt="Instagram" />
+                    </a>
+                  ) : (
+                    <></>
+                  )}
+                  {loggedInArtist.twitterLink ? (
+                    <a
+                      href={`https://twitter.com/${loggedInArtist.twitterLink}`}
+                      target="_blank"
+                      title={`https://twitter.com/${loggedInArtist.twitterLink}`}
+                      rel="noreferrer"
+                    >
+                      <img src={twitterIcon} alt="Twitter" />
+                    </a>
+                  ) : (
+                    <></>
+                  )}
                   <div className="copy-div">
                     <div className="copy" title="Copy to clipboard">
                       <div className="copied-div" hidden={!copied}>
