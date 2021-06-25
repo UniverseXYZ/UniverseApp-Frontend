2d432cc564a10ed85746b5504274102e9251183d
anahit-71 <>
2021-06-25 17:57:45 +0400

    remove popup

---
+++ src/stories/Popups/RemovePopup.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RemovePopup from './RemovePopup';
+
+export default {
+  title: 'Form/RemovePopup',
+  component: RemovePopup,
+};
+export const RemovePopups = () => <RemovePopup />;
