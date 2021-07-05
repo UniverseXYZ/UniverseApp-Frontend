61939103e3f129bf6d154e8dd9baa96fdcfd0120
Hovhannes-S <sargsyanhovhannes213@gmail.com>
2021-07-05 18:54:23 +0400

    create sell NFT Set Price Tab Functionality

--- src/components/sellNFT/SetPriceTabContent.jsx
+++ src/components/sellNFT/SetPriceTabContent.jsx
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Animated } from 'react-animated-css';
 import Input from '../input/Input';
 import Switch from '../ui-elements/Switch';
+import SelectPrice from '../input/SelectPrice';
+import EndDatePicker from '../calendar/EndDatePicker';
 import './styles/SetPriceTabContent.scss';
 
-const SetPrice = () => {
+const SetPrice = (props) => {
   const [endingPrice, setEndingPrice] = useState(false);
   const [scheduleFutureTime, setScheduleFutureTime] = useState(false);
   const [privacy, setPrivacy] = useState(false);
+  const [typeSwitch, setTypeSwitch] = useState(null);
+  const { startPrice, setStartPrice, endPrice, setEndPrice, priceType, setPriceType, setFormType } =
+    props;
+
+  useEffect(() => {
+    if (!endingPrice && !privacy && !scheduleFutureTime) {
+      setTypeSwitch(null);
+      setFormType(null);
+    } else setFormType(typeSwitch);
+  }, [endingPrice, privacy, scheduleFutureTime]);
 
   return (
     <div className="set--price--parent">
@@ -17,11 +31,16 @@ const SetPrice = () => {
             <p className="hint--text">Will be on sale until you transfer this item or cancel it.</p>
           </div>
           <div className="right--block">
-            <Input type="number" />
+            <Input
+              type="number"
+              placeholder="Amount"
+              value={startPrice}
+              onChange={(e) => setStartPrice(e.target.value)}
+            />
+            <SelectPrice value={priceType} onChange={setPriceType} />
           </div>
         </div>
       </div>
-
       <div className="row">
         <div className="row--child">
           <div className="left--block">
@@ -32,24 +51,94 @@ const SetPrice = () => {
             </p>
           </div>
           <div className="right--block">
-            <Switch value={endingPrice} onChange={setEndingPrice} />
+            <Switch
+              value={endingPrice}
+              onChange={() => {
+                setEndingPrice(!endingPrice);
+                setTypeSwitch('endingPrice');
+                setPrivacy(false);
+                setScheduleFutureTime(false);
+              }}
+            />
           </div>
         </div>
+        {endingPrice && typeSwitch === 'endingPrice' ? (
+          <Animated animationIn="fadeIn">
+            <div className="row--child switch--block">
+              <div className="left--block">
+                <h3 className="title">Ending price</h3>
+                <p className="hint--text">
+                  Must be less than or equal to the starting price. The price will progress linearly
+                  to this amount until the expiration date.
+                </p>
+              </div>
+              <div className="right--block">
+                <Input
+                  type="number"
+                  placeholder="Amount"
+                  value={endPrice}
+                  onChange={(e) => setEndPrice(e.target.value)}
+                />
+                <SelectPrice value={priceType} onChange={setPriceType} />
+              </div>
+            </div>
+            <div className="row--child switch--block">
+              <div className="left--block">
+                <h3 className="title">Expiration date</h3>
+                <p className="hint--text">
+                  Your listing will automatically end at this time. No need to cancel it!
+                </p>
+              </div>
+              <div className="right--block">
+                <EndDatePicker />
+              </div>
+            </div>
+          </Animated>
+        ) : (
+          <></>
+        )}
       </div>
 
-      <div className="row">
-        <div className="row--child">
-          <div className="left--block">
-            <h3 className="title">Schedule for a future time</h3>
-            <p className="hint--text">
-              You can schedle this listing to only be buyable at a future date.
-            </p>
+      {!endingPrice && (
+        <div className="row">
+          <div className="row--child">
+            <div className="left--block">
+              <h3 className="title">Schedule for a future time</h3>
+              <p className="hint--text">
+                You can schedle this listing to only be buyable at a future date.
+              </p>
+            </div>
+            <div className="right--block">
+              <Switch
+                value={scheduleFutureTime}
+                onChange={() => {
+                  setScheduleFutureTime(!scheduleFutureTime);
+                  setTypeSwitch('scheduleTime');
+                  setEndingPrice(false);
+                  setPrivacy(false);
+                }}
+              />
+            </div>
           </div>
-          <div className="right--block">
-            <Switch value={scheduleFutureTime} onChange={setScheduleFutureTime} />
-          </div>
+          {scheduleFutureTime && typeSwitch === 'scheduleTime' ? (
+            <Animated animationIn="fadeIn">
+              <div className="row--child switch--block">
+                <div className="left--block">
+                  <h3 className="title">Schedule for a future time</h3>
+                  <p className="hint--text">
+                    You can schedle this listing to only be buyable at a future date.
+                  </p>
+                </div>
+                <div className="right--block">
+                  <EndDatePicker />
+                </div>
+              </div>
+            </Animated>
+          ) : (
+            <></>
+          )}
         </div>
-      </div>
+      )}
 
       <div className="row">
         <div className="row--child">
@@ -61,11 +150,52 @@ const SetPrice = () => {
             </p>
           </div>
           <div className="right--block">
-            <Switch value={privacy} onChange={setPrivacy} />
+            <Switch
+              value={privacy}
+              onChange={() => {
+                setPrivacy(!privacy);
+                setTypeSwitch('privacy');
+                setEndingPrice(false);
+                setScheduleFutureTime(false);
+              }}
+            />
           </div>
         </div>
+        {privacy && (
+          <Animated animationIn="fadeIn">
+            <div className="row--child switch--block">
+              <Input
+                type="text"
+                placeholder="Buyer address"
+                value={endPrice}
+                onChange={(e) => setEndPrice(e.target.value)}
+              />
+            </div>
+          </Animated>
+        )}
       </div>
     </div>
   );
 };
+
+SetPrice.propTypes = {
+  startPrice: PropTypes.string,
+  setStartPrice: PropTypes.func,
+  endPrice: PropTypes.string,
+  setEndPrice: PropTypes.func,
+  priceType: PropTypes.string,
+  setPriceType: PropTypes.func,
+  setFormType: PropTypes.func,
+};
+
+SetPrice.defaultProps = {
+  startPrice: '',
+  setStartPrice: (e) => console.log(e),
+  endPrice: '',
+  setEndPrice: (e) => console.log(e),
+  priceType: 'eth',
+  setPriceType: () => {},
+  setFormType: () => {},
+};
+
 export default SetPrice;
--- src/containers/sellNFT/NFTMarketplace.jsx
+++ src/containers/sellNFT/NFTMarketplace.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CollectionName from '../../components/sellNFT/CollectionName';
 import SellNFTContainer from '../../components/sellNFT/SellNFTContainer';
 import NFTMarketplaceTab from '../../components/tabs/NFTMarketplaceTab';
@@ -7,6 +7,7 @@ import SetPrice from '../../components/sellNFT/SetPriceTabContent';
 import HighestBid from '../../components/sellNFT/HighestBidTabContent';
 import Bundle from '../../components/sellNFT/BundleTabContent';
 import Submenu from '../../components/submenu/Submenu';
+import SummaryBlock from '../../components/sellNFT/SummaryBlock';
 import './NFTMarketplace.scss';
 
 const headerTabLabels = [
@@ -15,20 +16,39 @@ const headerTabLabels = [
   { className: '', label: 'Bundle', hintText: 'Group this item with others to sell' },
 ];
 
-const contents = [
-  { index: 0, component: <SetPrice /> },
-  { index: 1, component: <HighestBid /> },
-  { index: 2, component: <Bundle /> },
-];
-
-const NFTMarketplace = () => (
-  <div className="nft--marketplace">
-    <Submenu title="NFT Marketplace" subtitles={['Browse NFTs', 'Activity']} />
-    <CollectionName image={collectionImg} name="NFT name" price="0.5" />
-    <SellNFTContainer title="Select your sell method" contentClassName="content--marketplace">
-      <NFTMarketplaceTab headerLabels={headerTabLabels} contents={contents} />
-    </SellNFTContainer>
-  </div>
-);
+const NFTMarketplace = () => {
+  const [startPrice, setStartPrice] = useState('');
+  const [endPrice, setEndPrice] = useState('');
+  const [priceType, setPriceType] = useState('eth');
+  const [formType, setFormType] = useState(null);
+  const contents = [
+    {
+      index: 0,
+      component: (
+        <SetPrice
+          startPrice={startPrice}
+          setStartPrice={setStartPrice}
+          endPrice={endPrice}
+          setEndPrice={setEndPrice}
+          priceType={priceType}
+          setPriceType={setPriceType}
+          setFormType={setFormType}
+        />
+      ),
+    },
+    { index: 1, component: <HighestBid /> },
+    { index: 2, component: <Bundle /> },
+  ];
+  return (
+    <div className="nft--marketplace">
+      <Submenu title="NFT Marketplace" subtitles={['Browse NFTs', 'Activity']} />
+      <CollectionName image={collectionImg} name="NFT name" price="0.5" />
+      <SellNFTContainer title="Select your sell method" contentClassName="content--marketplace">
+        <NFTMarketplaceTab headerLabels={headerTabLabels} contents={contents} />
+        <SummaryBlock startPrice={startPrice} endPrice={endPrice} formType={formType} />
+      </SellNFTContainer>
+    </div>
+  );
+};
 
 export default NFTMarketplace;
