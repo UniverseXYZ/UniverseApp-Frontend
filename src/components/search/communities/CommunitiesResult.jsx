159fdcbbd2988f955b63eb1210d8a413cc0d7b61
VahanMuradyan <10vahan.m16@gmail.com>
2021-07-07 18:45:46 +0400

    Marketplace search

--- src/components/search/communities/CommunitiesResult.jsx
+++ src/components/search/communities/CommunitiesResult.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './CommunitiesResult.scss';
+import Button from '../../button/Button.jsx';
 
-const CommunitiesResult = ({ query }) => {
-  console.log(query);
-  return (
-    <div>
-      <h1>CommunitiesResult {query}</h1>
-    </div>
-  );
-};
+const CommunitiesResult = ({ query, data }) => (
+  <div className="communities--search--result">
+    {data
+      .filter((item) => item.name.toLowerCase().includes(query.toLowerCase()))
+      .map((community) => (
+        <div className="community--box">
+          <div className="community--box--header">
+            <div className="avatar--title--members">
+              <div>
+                <img src={community.photo} alt={community.name} />
+              </div>
+              <div>
+                <h3>{community.name}</h3>
+                <p>{`${community.members} members`}</p>
+              </div>
+            </div>
+            <div>
+              <Button className="light-button">Join</Button>
+            </div>
+          </div>
+          <div className="community--box--body">
+            <p className="desc">{community.description}</p>
+          </div>
+        </div>
+      ))}
+  </div>
+);
 
 CommunitiesResult.propTypes = {
   query: PropTypes.string,
+  data: PropTypes.oneOfType([PropTypes.array]),
 };
 
 CommunitiesResult.defaultProps = {
   query: '',
+  data: [],
 };
 
 export default CommunitiesResult;
