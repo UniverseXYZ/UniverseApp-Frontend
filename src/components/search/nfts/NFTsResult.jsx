471a4aefd70db080ac5b44f53509bdb0916c5863
VahanMuradyan <10vahan.m16@gmail.com>
2021-07-09 11:21:20 +0400

    show likers with tooltip

--- src/components/search/nfts/NFTsResult.jsx
+++ src/components/search/nfts/NFTsResult.jsx
@@ -54,22 +54,35 @@ const NFTsResult = ({ query, data }) => {
                   </div>
                 </div>
                 <div className="likes--count">
-                  <svg
-                    className={nft.liked ? 'fill' : ''}
-                    onClick={() => handleLikeClick(nft.id)}
-                    width="16"
-                    height="14"
-                    viewBox="0 0 16 14"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M7.9998 13.3996C8.15207 13.3996 8.36959 13.302 8.52911 13.2114C12.6113 10.7016 15.1998 7.78044 15.1998 4.8105C15.1998 2.34253 13.4379 0.599609 11.1611 0.599609C9.7974 0.599609 8.7372 1.30007 8.07164 2.38196C8.03914 2.4348 7.96094 2.43454 7.92872 2.38153C7.27515 1.30607 6.20174 0.599609 4.83848 0.599609C2.56174 0.599609 0.799805 2.34253 0.799805 4.8105C0.799805 7.78044 3.38832 10.7016 7.47775 13.2114C7.63002 13.302 7.84754 13.3996 7.9998 13.3996Z"
-                      stroke="black"
-                      strokeOpacity="0.4"
-                      strokeLinecap="round"
-                    />
-                  </svg>
+                  <div>
+                    <svg
+                      className={nft.liked ? 'fill' : ''}
+                      onClick={() => handleLikeClick(nft.id)}
+                      width="16"
+                      height="14"
+                      viewBox="0 0 16 14"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M7.9998 13.3996C8.15207 13.3996 8.36959 13.302 8.52911 13.2114C12.6113 10.7016 15.1998 7.78044 15.1998 4.8105C15.1998 2.34253 13.4379 0.599609 11.1611 0.599609C9.7974 0.599609 8.7372 1.30007 8.07164 2.38196C8.03914 2.4348 7.96094 2.43454 7.92872 2.38153C7.27515 1.30607 6.20174 0.599609 4.83848 0.599609C2.56174 0.599609 0.799805 2.34253 0.799805 4.8105C0.799805 7.78044 3.38832 10.7016 7.47775 13.2114C7.63002 13.302 7.84754 13.3996 7.9998 13.3996Z"
+                        stroke="black"
+                        strokeOpacity="0.4"
+                        strokeLinecap="round"
+                      />
+                    </svg>
+                    <div className="tooltiptext">
+                      <div className="likers--text">{`${nft.likesCount} people liked this`}</div>
+                      <div className="likers--avatars">
+                        <img src={nft.owner.avatar} alt="Liker" />
+                        <img src={nft.owner.avatar} alt="Liker" />
+                        <img src={nft.owner.avatar} alt="Liker" />
+                        <img src={nft.owner.avatar} alt="Liker" />
+                        <img src={nft.owner.avatar} alt="Liker" />
+                        <img src={nft.owner.avatar} alt="Liker" />
+                      </div>
+                    </div>
+                  </div>
                   <span>{nft.likesCount}</span>
                 </div>
               </div>
