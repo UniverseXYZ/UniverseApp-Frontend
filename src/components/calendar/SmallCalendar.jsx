d2971a1eb65fa9aa4f38bd2214032bd8c69180e1
VahanMuradyan <10vahan.m16@gmail.com>
2021-07-13 12:51:25 +0400

    Done with Make an offer popup

--- src/components/calendar/SmallCalendar.jsx
+++ src/components/calendar/SmallCalendar.jsx
@@ -1,36 +1,22 @@
 import React, { useRef, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import uuid from 'react-uuid';
 import arrow from '../../assets/images/arrow.svg';
 
-const SmallCalendar = () => {
+const SmallCalendar = ({
+  showCalendar,
+  setShowCalendar,
+  monthNames,
+  offerExpirationDate,
+  setOfferExpirationDate,
+}) => {
   const ref = useRef();
   const d = new Date();
-  const monthNames = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
   const weekNames = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
   const [currentMonth, setCurrentMonth] = useState([]);
   const [selectedDate, setSelectedDate] = useState({
     year: d.getFullYear(),
     month: d.getMonth(),
-  });
-  const [startDateTemp, setStartDateTemp] = useState({
-    month: monthNames[d.getMonth()],
-    day: d.getDate(),
-    year: d.getFullYear(),
-    hours: new Date().getHours(),
-    minutes: new Date().getMinutes() < 10 ? `0${new Date().getMinutes()}` : new Date().getMinutes(),
   });
 
   const createDaysArray = () => {
@@ -79,9 +65,9 @@ const SmallCalendar = () => {
   const handleHoursChange = (val) => {
     const value = val.replace(/[^\d]/, '');
     if (value.length < 3 && Number(value) < 25) {
-      setStartDateTemp((prevState) => ({
+      setOfferExpirationDate((prevState) => ({
         ...prevState,
-        hours: value,
+        hours: Number(value),
       }));
     }
   };
@@ -89,34 +75,32 @@ const SmallCalendar = () => {
   const handleMinutesChange = (val) => {
     const value = val.replace(/[^\d]/, '');
     if (value.length < 3 && Number(value) < 60) {
-      setStartDateTemp((prevState) => ({
+      setOfferExpirationDate((prevState) => ({
         ...prevState,
-        minutes: value,
+        minutes: Number(value),
       }));
     }
   };
 
   const handleDayClick = (day) => {
     if (day) {
-      if (
-        new Date(new Date(selectedDate.year, selectedDate.month, day).toDateString()) <
-        new Date(new Date().toDateString())
-      ) {
-        alert('Start date can not be before today!');
-      } else {
-        setStartDateTemp((prevState) => ({
-          ...prevState,
-          month: monthNames[selectedDate.month],
-          day,
-          year: selectedDate.year,
-        }));
-      }
+      setOfferExpirationDate((prevState) => ({
+        ...prevState,
+        month: monthNames[selectedDate.month],
+        day,
+        year: selectedDate.year,
+      }));
     }
   };
 
   const handleClickOutside = (event) => {
-    if (ref.current && !ref.current.contains(event.target)) {
-      onClose();
+    if (
+      ref.current &&
+      !ref.current.contains(event.target) &&
+      !event.target.classList.contains('calendar--icon') &&
+      event.target.alt !== 'Calendar'
+    ) {
+      setShowCalendar(!showCalendar);
     }
   };
 
@@ -135,7 +119,12 @@ const SmallCalendar = () => {
     <div className="calendar__wrapper" ref={ref}>
       <div className="calendar-first">
         <div className="calendar__header">
-          <button className="left-btn" type="button" onClick={() => changeMonth('prev')}>
+          <button
+            className="left-btn"
+            type="button"
+            onClick={() => changeMonth('prev')}
+            disabled={selectedDate.month === new Date().getMonth()}
+          >
             <img className="left" src={arrow} alt="Left arrow" />
           </button>
           <h2>{`${monthNames[selectedDate.month]}`}</h2>
@@ -154,8 +143,15 @@ const SmallCalendar = () => {
           </div>
           {currentMonth.map((week) => (
             <div key={uuid()} className="week__days__numbers">
-              {week.map((day, index) => (
+              {week.map((day) => (
                 <button
+                  className={
+                    day === offerExpirationDate.day &&
+                    monthNames[selectedDate.month] === offerExpirationDate.month &&
+                    selectedDate.year === offerExpirationDate.year
+                      ? 'selectedDay'
+                      : ''
+                  }
                   type="button"
                   key={uuid()}
                   aria-hidden="true"
@@ -177,19 +173,27 @@ const SmallCalendar = () => {
         <div className="time">
           <input
             type="text"
-            value={startDateTemp.hours}
+            value={offerExpirationDate.hours}
             onChange={(e) => handleHoursChange(e.target.value)}
           />
           <span>:</span>
           <input
             type="text"
-            value={startDateTemp.minutes}
+            value={offerExpirationDate.minutes}
             onChange={(e) => handleMinutesChange(e.target.value)}
           />
         </div>
       </div>
     </div>
   );
+};
+
+SmallCalendar.propTypes = {
+  showCalendar: PropTypes.bool.isRequired,
+  setShowCalendar: PropTypes.func.isRequired,
+  monthNames: PropTypes.oneOfType([PropTypes.array]).isRequired,
+  offerExpirationDate: PropTypes.oneOfType([PropTypes.any]).isRequired,
+  setOfferExpirationDate: PropTypes.func.isRequired,
 };
 
 export default SmallCalendar;
