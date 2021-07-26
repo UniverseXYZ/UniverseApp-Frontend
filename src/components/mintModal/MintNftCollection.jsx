7a700f2d355e94e25427333554ed1a2808d95855
entrpyc <contact@a-angelov.eu>
2021-07-26 13:14:44 +0300

    added mock for getting my collections

--- src/components/mintModal/MintNftCollection.jsx
+++ src/components/mintModal/MintNftCollection.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import React, { useContext, useRef, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import uuid from 'react-uuid';
@@ -17,7 +18,12 @@ import removeIcon from '../../assets/images/remove.svg';
 import cloudIcon from '../../assets/images/ion_cloud.svg';
 import mp3Icon from '../../assets/images/mp3-icon.png';
 import videoIcon from '../../assets/images/video-icon.svg';
-import { generateTokenURIForCollection } from '../../utils/api/mintNFT';
+import {
+  generateTokenURIForCollection,
+  saveCollection,
+  attachTxHashToCollection,
+  getMyCollections,
+} from '../../utils/api/mintNFT';
 
 const MintNftCollection = ({ onClick }) => {
   const {
@@ -273,6 +279,36 @@ const MintNftCollection = ({ onClick }) => {
                 properties: nft.properties,
               });
             });
+
+            const requestData = {
+              file: coverImage,
+              name: collectionName,
+              symbol: tokenName,
+              description,
+              shortUrl: shortURL,
+            };
+            const { id } = await saveCollection(requestData);
+
+            const unsignedMintTx = await universeERC721FactoryContract.deployUniverseERC721(
+              collectionName,
+              tokenName
+            );
+
+            const { transactionHash } = await unsignedMintTx.wait();
+
+            const response = await attachTxHashToCollection(transactionHash, id);
+            console.log('res', response);
+            if (!response.ok && response.status !== 201) {
+              console.error(`Error while trying to save a new collection: ${response.statusText}`);
+              return;
+            }
+
+            // const nftUrls = newMyNFTs.map((nft) => nft.shortURL);
+            // const fee = [{ recipient: tx.from, value: 1 }];
+            // const unsignedMintTx = await universeERC721CoreContract.batchMint(tx.to, nftUrls, fee);
+            // const mintTx = await unsignedMintTx.wait();
+            // console.log(mintTx);
+
             setMyNFTs(newMyNFTs);
           }
           setDeployedCollections([
@@ -301,6 +337,14 @@ const MintNftCollection = ({ onClick }) => {
       }
     }
   }, [errors]);
+
+  useEffect(async () => {
+    const collectionsReturn = await getMyCollections();
+    const reader = collectionsReturn.body.getReader();
+    reader.read().then(({ done, value }) => {
+      console.log('val', value, done);
+    });
+  }, []);
 
   return !showCollectible ? (
     <div className="mintNftCollection-div">
