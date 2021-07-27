b38e6287e8fd228fb18fefcaba0ab616abb6e22f
Milena Atanasova <milena.atanasova42@gmail.com>
2021-07-27 14:32:45 +0300

    get my nfts base implementation

--- src/components/myNFTs/Wallet.jsx
+++ src/components/myNFTs/Wallet.jsx
@@ -40,6 +40,19 @@ const Wallet = ({
   const [previewNFTs, setPreviewNFTs] = useState([]);
   const history = useHistory();
 
+  // let myNFTs = [];
+  // (async () => {
+
+  //   const nfts = await getMyNFTs();
+  //   const reader = nfts.body.pipeThrough(new TextDecoderStream()).getReader();
+  //   const readMyCollectionsStream = async () => {
+  //     const { done, value } = await reader.read();
+  //     myNFTs = [...myNFTs, ...value.collections];
+  //     console.log(myNFTs);
+  //     if (!done) readMyCollectionsStream();
+  //   };
+  // });
+
   const saveIndexes = (index) => {
     const temp = [...indexes];
     temp.push(index);
@@ -53,6 +66,7 @@ const Wallet = ({
   const isCreatingAction = location.pathname === '/create-tiers';
   const tierById = !!(winners && nftsPerWinner);
   const editMode = auction.tiers.find((element) => element.id === location.state);
+
   const handleCollectionsMobile = () => {
     setCollections(draftCollections);
     setMobileVersion(true);
