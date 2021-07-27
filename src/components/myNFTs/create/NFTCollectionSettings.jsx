00b6ddfbfcd68ae61a363dd812f3661871a415f8
VahanMuradyan <10vahan.m16@gmail.com>
2021-07-27 17:00:22 +0400

    NFT Collection settings

--- src/components/myNFTs/create/NFTCollectionSettings.jsx
+++ src/components/myNFTs/create/NFTCollectionSettings.jsx
@@ -1,19 +1,32 @@
-import React, { useState, useRef, useContext } from 'react';
+import React, { useState, useRef, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import uuid from 'react-uuid';
+import Slider from 'react-slick';
+import Popup from 'reactjs-popup';
+import { useHistory } from 'react-router-dom';
 import Input from '../../input/Input.jsx';
 import Button from '../../button/Button.jsx';
+import AppContext from '../../../ContextAPI.js';
+import NFTCollectible from './NFTCollectible.jsx';
 import uploadIcon from '../../../assets/images/ion_cloud.svg';
 import closeIcon from '../../../assets/images/close-menu.svg';
 import plusIcon from '../../../assets/images/plus.svg';
 import bigPlusGradientIcon from '../../../assets/images/Union.svg';
 import errorIcon from '../../../assets/images/error-icon.svg';
-import AppContext from '../../../ContextAPI.js';
-import NFTCollectible from './NFTCollectible.jsx';
+import editIcon from '../../../assets/images/edit.svg';
+import removeIcon from '../../../assets/images/remove.svg';
+import videoIcon from '../../../assets/images/video-icon.svg';
+import RemovePopup from '../../popups/RemoveNftPopup.jsx';
+import testUserImage from '../../../assets/images/marketplace/users/user1.png';
+import leftArrow from '../../../assets/images/marketplace/bundles-left-arrow.svg';
+import rightArrow from '../../../assets/images/marketplace/bundles-right-arrow.svg';
+import LoadingPopup from '../../popups/LoadingPopup.jsx';
+import CongratsPopup from '../../popups/CongratsPopup.jsx';
+import { defaultColors } from '../../../utils/helpers.js';
+import Pagination from '../../pagination/Pagionation.jsx';
 
 const NFTCollectionSettings = ({ showCollectible, setShowCollectible }) => {
   const {
-    setShowModal,
-    savedCollections,
     savedNfts,
     savedCollectionID,
     myNFTs,
@@ -22,7 +35,12 @@ const NFTCollectionSettings = ({ showCollectible, setShowCollectible }) => {
     setDeployedCollections,
   } = useContext(AppContext);
 
+  const [offset, setOffset] = useState(0);
+  const [perPage, setPerPage] = useState(6);
+
+  const ref = useRef(null);
   const inputFile = useRef(null);
+  const history = useHistory();
 
   const [coverImage, setCoverImage] = useState(null);
   const [collectionName, setCollectionName] = useState('');
@@ -31,6 +49,10 @@ const NFTCollectionSettings = ({ showCollectible, setShowCollectible }) => {
   const [shortURL, setShortURL] = useState('universe.xyz/c/shorturl');
   const [inputClass, setInputClass] = useState('inp empty');
   const [collectionNFTs, setCollectionNFTs] = useState([]);
+  const [collectionNFTsID, setCollectionNFTsID] = useState(null);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [dropdownID, setDropdownID] = useState(0);
+  const [mintNowClick, setMintNowClick] = useState(false);
 
   const [errors, setErrors] = useState({
     coverImage: '',
@@ -40,7 +62,13 @@ const NFTCollectionSettings = ({ showCollectible, setShowCollectible }) => {
     shorturl: '',
   });
 
-  const [mintNowClick, setMintNowClick] = useState(false);
+  const [sliderSettings, setSliderSettings] = useState({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
 
   const validateFile = (file) => {
     setMintNowClick(false);
@@ -158,8 +186,185 @@ const NFTCollectionSettings = ({ showCollectible, setShowCollectible }) => {
     }
   };
 
+  const handleMinting = () => {
+    setMintNowClick(true);
+    if (
+      !collectionName ||
+      !tokenName ||
+      !collectionNFTs.length ||
+      shortURL.length <= 15 ||
+      shortURL === 'universe.xyz/c/shorturl'
+    ) {
+      setErrors({
+        collectionName: !collectionName ? '“Collection name” is not allowed to be empty' : '',
+        tokenName: !tokenName ? '“Token name” is not allowed to be empty' : '',
+        collectible: !collectionNFTs.length ? '“NFT collectible” is required' : '',
+        shorturl:
+          shortURL.length <= 15 || shortURL === 'universe.xyz/c/shorturl'
+            ? '“Short URL” is not allowed to be empty'
+            : '',
+      });
+      if (errors.shorturl.length > 0 || shortURL === 'universe.xyz/c/shorturl') {
+        setInputClass('empty__error');
+      } else {
+        setInputClass('inp');
+      }
+    } else {
+      const collectionNameExists = deployedCollections.length
+        ? deployedCollections.filter(
+            (collection) => collection.name.toLowerCase() === collectionName.toLowerCase()
+          )
+        : [];
+      const existsInMyNfts = myNFTs.length
+        ? myNFTs.filter((nft) => nft.collectionName?.toLowerCase() === collectionName.toLowerCase())
+        : [];
+      if ((collectionNameExists.length || existsInMyNfts.length) && !savedCollectionID) {
+        setErrors({
+          ...errors,
+          collectionName: '“Collection name” already exists',
+        });
+      } else {
+        setErrors({
+          collectionName: '',
+          tokenName: '',
+          collectible: '',
+          shorturl: '',
+        });
+      }
+    }
+  };
+
+  const handleEdit = (id) => {
+    setCollectionNFTsID(id);
+    setShowCollectible(true);
+  };
+
+  const handleClickOutside = (event) => {
+    if (!event.target.classList.contains('three__dots')) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        if (document.getElementById('popup-root')) {
+          if (!document.getElementById('popup-root').hasChildNodes()) {
+            setShowDropdown(false);
+          }
+        } else {
+          setShowDropdown(false);
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  });
+
+  useEffect(() => {
+    // Prev Icon
+    const prev = document.querySelectorAll('.slick-prev');
+    if (prev) {
+      prev.forEach((el) => {
+        const prevIcon = document.createElement('img');
+        prevIcon.src = leftArrow;
+        el.innerHTML = '';
+        el.appendChild(prevIcon);
+      });
+    }
+
+    // Next icon
+    const next = document.querySelectorAll('.slick-next');
+    if (next) {
+      next.forEach((el) => {
+        const nextIcon = document.createElement('img');
+        nextIcon.src = rightArrow;
+        el.innerHTML = '';
+        el.appendChild(nextIcon);
+      });
+    }
+  });
+
+  useEffect(() => {
+    if (mintNowClick) {
+      if (!errors.collectionName && !errors.tokenName && !errors.shorturl && !errors.collectible) {
+        document.getElementById('loading-hidden-btn').click();
+        setTimeout(() => {
+          document.getElementById('popup-root').remove();
+          document.getElementById('congrats-hidden-btn').click();
+          setTimeout(() => {
+            if (collectionNFTs.length) {
+              const newMyNFTs = [...myNFTs];
+              collectionNFTs.forEach((nft) => {
+                newMyNFTs.push({
+                  id: uuid(),
+                  type: 'collection',
+                  collectionId: collectionName,
+                  collectionName,
+                  collectionAvatar:
+                    coverImage || defaultColors[Math.floor(Math.random() * defaultColors.length)],
+                  tokenName,
+                  collectionDescription: description,
+                  shortURL,
+                  previewImage: nft.previewImage,
+                  name: nft.name,
+                  description: nft.description,
+                  numberOfEditions: Number(nft.editions),
+                  generatedEditions: nft.generatedEditions,
+                  releasedDate: new Date(),
+                  properties: nft.properties,
+                  royaltySplits: nft.royaltySplits,
+                });
+              });
+              setMyNFTs(newMyNFTs);
+            }
+            setDeployedCollections([
+              ...deployedCollections,
+              {
+                id: collectionName,
+                previewImage:
+                  coverImage || defaultColors[Math.floor(Math.random() * defaultColors.length)],
+                name: collectionName,
+                tokenName,
+                description,
+                shortURL,
+              },
+            ]);
+          }, 2000);
+        }, 3000);
+      }
+    }
+  }, [errors]);
+
+  useEffect(() => {
+    console.log('collectionNFTs', collectionNFTs);
+  }, [collectionNFTs]);
+
   return !showCollectible ? (
     <div className="nft--collection--settings--page">
+      <Popup
+        trigger={
+          <button
+            type="button"
+            id="loading-hidden-btn"
+            aria-label="hidden"
+            style={{ display: 'none' }}
+          />
+        }
+      >
+        {(close) => <LoadingPopup onClose={close} />}
+      </Popup>
+      <Popup
+        trigger={
+          <button
+            type="button"
+            id="congrats-hidden-btn"
+            aria-label="hidden"
+            style={{ display: 'none' }}
+          />
+        }
+      >
+        {(close) => <CongratsPopup onClose={close} />}
+      </Popup>
       <h1 className="nft--collection--settings--page--title">NFT collection settings</h1>
       <div className="image--name--token">
         <div className="collection--cover--image">
@@ -196,6 +401,16 @@ const NFTCollectionSettings = ({ showCollectible, setShowCollectible }) => {
             )}
           </div>
           {errors.coverImage && <p className="error-message">{errors.coverImage}</p>}
+        </div>
+        <div className="show--on--mobile--only">
+          <h3>
+            Cover image <span>(opt)</span>
+          </h3>
+          <p>(min 200x200px, PNG/JPEG/GIF, max 1mb)</p>
+          <Button className="light-border-button" onClick={() => inputFile.current.click()}>
+            Choose file
+          </Button>
+          <input type="file" ref={inputFile} onChange={(e) => validateFile(e.target.files[0])} />
         </div>
         <div className="collection--name--and--token">
           <div className="collection--name">
@@ -247,7 +462,7 @@ const NFTCollectionSettings = ({ showCollectible, setShowCollectible }) => {
         <div className="collection--nfts--title">
           <h1>NFTs</h1>
           {collectionNFTs.length ? (
-            <Button className="light-border-button">
+            <Button className="light-border-button" onClick={handleShowCollectible}>
               Create NFT
               <img src={plusIcon} alt="Plus" />
             </Button>
@@ -271,6 +486,172 @@ const NFTCollectionSettings = ({ showCollectible, setShowCollectible }) => {
         ) : (
           <></>
         )}
+        <div className="browse--nft--list">
+          {collectionNFTs.slice(offset, offset + perPage).map((nft) => (
+            <div className="nft--box" key={uuid()}>
+              <div className="nft--box--header">
+                <div className="three--images">
+                  <div className="creator--details">
+                    <img
+                      src={
+                        nft.creator.avatar ? URL.createObjectURL(nft.creator.avatar) : testUserImage
+                      }
+                      alt={nft.creator.name ? nft.creator.name : 'Test'}
+                    />
+                    <span className="tooltiptext">{`Creator: ${
+                      nft.creator.name ? nft.creator.name : 'Test'
+                    }`}</span>
+                  </div>
+                  <div className="collection--details">
+                    {typeof nft.collection.avatar === 'string' &&
+                    nft.collection.avatar.startsWith('#') ? (
+                      <div
+                        className="random--bg--color"
+                        style={{ backgroundColor: nft.collection.avatar }}
+                      >
+                        {nft.collection.name.charAt(0)}
+                      </div>
+                    ) : (
+                      <img
+                        src={URL.createObjectURL(nft.collection.avatar)}
+                        alt={nft.collection.name}
+                      />
+                    )}
+                    <span className="tooltiptext">{`Collection: ${nft.collection.name}`}</span>
+                  </div>
+                  <div className="owner--details">
+                    <img
+                      src={nft.owner.avatar ? URL.createObjectURL(nft.owner.avatar) : testUserImage}
+                      alt={nft.owner.name ? nft.owner.name : 'Test'}
+                    />
+                    <span className="tooltiptext">{`Owner: ${
+                      nft.owner.name ? nft.owner.name : 'Test'
+                    }`}</span>
+                  </div>
+                </div>
+                <div className="three--dots">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setShowDropdown(!showDropdown);
+                      setDropdownID(nft.id);
+                    }}
+                  >
+                    <span />
+                    <span />
+                    <span />
+                    {dropdownID === nft.id && showDropdown && (
+                      <ul ref={ref} className="edit__remove">
+                        <li className="edit" onClick={() => handleEdit(nft.id)} aria-hidden="true">
+                          <img src={editIcon} alt="Edit Icon" />
+                          <p>Edit</p>
+                        </li>
+                        <Popup
+                          trigger={
+                            <li className="remove">
+                              <img src={removeIcon} alt="Remove Icon" />
+                              <p>Remove</p>
+                            </li>
+                          }
+                        >
+                          {(close) => (
+                            <RemovePopup
+                              close={close}
+                              nftID={nft.id}
+                              removedItemName={nft.name}
+                              removeFrom="collection"
+                              collectionNFTs={collectionNFTs}
+                              setCollectionNFTs={setCollectionNFTs}
+                            />
+                          )}
+                        </Popup>
+                      </ul>
+                    )}
+                  </button>
+                </div>
+              </div>
+              <div className="nft--box--body" aria-hidden="true">
+                {!nft.allItems.length ? (
+                  <div>
+                    {nft.previewImage.type !== 'video/mp4' && (
+                      <img
+                        className="nft--image"
+                        src={URL.createObjectURL(nft.previewImage)}
+                        alt={nft.name}
+                      />
+                    )}
+                    {nft.previewImage.type === 'video/mp4' && (
+                      <video
+                        onMouseOver={(event) => event.target.play()}
+                        onFocus={(event) => event.target.play()}
+                        onMouseOut={(event) => event.target.pause()}
+                        onBlur={(event) => event.target.pause()}
+                        muted
+                      >
+                        <source src={URL.createObjectURL(nft.previewImage)} type="video/mp4" />
+                        <track kind="captions" />
+                        Your browser does not support the video tag.
+                      </video>
+                    )}
+                    {nft.previewImage.type === 'video/mp4' && (
+                      <div className="video__icon">
+                        <img src={videoIcon} alt="Video Icon" />
+                      </div>
+                    )}
+                  </div>
+                ) : (
+                  <>
+                    <Slider {...sliderSettings}>
+                      {nft.allItems.map(
+                        (item, index) =>
+                          index < 7 && (
+                            <div className="slider--box" key={uuid()}>
+                              {item.type !== 'video/mp4' && (
+                                <img className="nft--image" src={item.url} alt={nft.name} />
+                              )}
+                              {item.type === 'video/mp4' && (
+                                <video
+                                  onMouseOver={(event) => event.target.play()}
+                                  onFocus={(event) => event.target.play()}
+                                  onMouseOut={(event) => event.target.pause()}
+                                  onBlur={(event) => event.target.pause()}
+                                  muted
+                                >
+                                  <source src={item.url} type="video/mp4" />
+                                  <track kind="captions" />
+                                  Your browser does not support the video tag.
+                                </video>
+                              )}
+                              {item.type === 'video/mp4' && (
+                                <div className="video__icon">
+                                  <img src={videoIcon} alt="Video Icon" />
+                                </div>
+                              )}
+                            </div>
+                          )
+                      )}
+                    </Slider>
+                  </>
+                )}
+              </div>
+              <div className="nft--box--footer">
+                <div className="name--and--price">
+                  <h4>{nft.name}</h4>
+                </div>
+                <div className="quantity--and--offer">
+                  <p>{`${nft.numberOfEditions}/${nft.numberOfEditions}`}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        {collectionNFTs.length > 6 ? (
+          <div className="pagination__container">
+            <Pagination data={collectionNFTs} perPage={perPage} setOffset={setOffset} />
+          </div>
+        ) : (
+          <></>
+        )}
         {errors.collectible && <p className="error-message">{errors.collectible}</p>}
         {(errors.collectionName || errors.tokenName || errors.tokenName || errors.shorturl) && (
           <div className="collection--final--error">
@@ -285,6 +666,7 @@ const NFTCollectionSettings = ({ showCollectible, setShowCollectible }) => {
       <div className="create--collection--btn">
         <Button
           className="light-button"
+          onClick={handleMinting}
           disabled={!collectionName || !tokenName || !shortURL || !collectionNFTs.length}
         >
           Create collection
@@ -292,7 +674,15 @@ const NFTCollectionSettings = ({ showCollectible, setShowCollectible }) => {
       </div>
     </div>
   ) : (
-    <NFTCollectible />
+    <NFTCollectible
+      setShowCollectible={setShowCollectible}
+      collectionName={collectionName}
+      coverImage={coverImage}
+      collectionNFTs={collectionNFTs}
+      setCollectionNFTs={setCollectionNFTs}
+      collectionNFTsID={collectionNFTsID}
+      setCollectionNFTsID={setCollectionNFTsID}
+    />
   );
 };
 
--- src/components/popups/CongratsPopup.jsx
+++ src/components/popups/CongratsPopup.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import Button from '../button/Button.jsx';
 import closeIcon from '../../assets/images/cross.svg';
+import checkIcon from '../../assets/images/bid-submitted.png';
 
-const CongratsPopup = ({ onClose }) => (
-  <div className="popup-div congrats-popup">
-    <button type="button" className="popup-close" onClick={onClose}>
-      <img src={closeIcon} alt="" />
-    </button>
-    <div className="popup-title">
-      <h4>Congratulations</h4>
+const CongratsPopup = ({ onClose }) => {
+  const history = useHistory();
+  return (
+    <div className="popup-div congrats-popup">
+      <button type="button" className="popup-close" onClick={onClose}>
+        <img src={closeIcon} alt="" />
+      </button>
+      <div className="popup-title">
+        <div className="congrats--icon">
+          <img src={checkIcon} alt="Congrats" />
+        </div>
+        <h4>Congratulations!</h4>
+      </div>
+      <div className="popup-text">
+        <p>
+          NFT collection was successfully created and should be displayed in your wallet shortly
+        </p>
+      </div>
+      <div className="popup-btns">
+        <Button className="light-button" onClick={() => history.push('/my-nfts')}>
+          Go to my NFTs
+        </Button>
+        <Button className="light-border-button" onClick={() => history.push('/my-nfts/create')}>
+          Create more
+        </Button>
+      </div>
     </div>
-    <div className="popup-text">
-      <p>
-        NFT Collection with collectibles was successfully created. It should be visible in your
-        wallet shortly.
-      </p>
-    </div>
-    <div className="popup-btns">
-      <Button className="light-border-button" onClick={onClose}>
-        Close
-      </Button>
-    </div>
-  </div>
-);
+  );
+};
 
 CongratsPopup.propTypes = {
   onClose: PropTypes.func.isRequired,
