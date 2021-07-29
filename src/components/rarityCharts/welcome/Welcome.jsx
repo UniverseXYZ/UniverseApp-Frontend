a4f92d59a9efa11261ac343ba10c0306c83c1241
Hovhannes-S <sargsyanhovhannes213@gmail.com>
2021-07-29 12:43:50 +0400

    create Rarity Welcome Responsive Section UI

--- src/components/rarityCharts/welcome/Welcome.jsx
+++ src/components/rarityCharts/welcome/Welcome.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import WelcomeWrapper from '../../ui-elements/WelcomeWrapper';
 import './Welcome.scss';
+
+const marquee = () => (
+  <p>
+    <span className="marquee--text--polymorph">POLYMORPH</span>
+    <span className="marquee--text--universe">UNIVERSE</span>
+    <span className="marquee--text--polymorph">POLYMORPH</span>
+    <span className="marquee--text--universe">UNIVERSE</span>
+    <span className="marquee--text--polymorph">POLYMORPH</span>
+    <span className="marquee--text--universe">UNIVERSE</span>
+    <span className="marquee--text--polymorph">POLYMORPH</span>
+    <span className="marquee--text--universe">UNIVERSE</span>
+    <span className="marquee--text--polymorph">POLYMORPH</span>
+    <span className="marquee--text--universe">UNIVERSE</span>
+    <span className="marquee--text--polymorph">POLYMORPH</span>
+    <span className="marquee--text--universe">UNIVERSE</span>
+    <span className="marquee--text--polymorph">POLYMORPH</span>
+    <span className="marquee--text--universe">UNIVERSE</span>
+    <span className="marquee--text--polymorph">POLYMORPH</span>
+    <span className="marquee--text--universe">UNIVERSE</span>
+  </p>
+);
 
 const Welcome = () => {
   console.log('Welcome');
-  return <div>Welcome</div>;
+  return (
+    <div className="welcome--section--rarity--charts">
+      <WelcomeWrapper
+        title="Polymorph Rarity Chart"
+        hintText="10,000 Total Polymorphs"
+        ellipsesLeft={false}
+        ellipsesRight={false}
+        marquee={marquee()}
+        bgTextRight
+      />
+    </div>
+  );
 };
 
 export default Welcome;
