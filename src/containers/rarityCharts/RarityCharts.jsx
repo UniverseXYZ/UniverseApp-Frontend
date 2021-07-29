02665b2e675e7e3cbee956f74d2073a94e4fecbd
armen5 <arm.khachatryan11@gmail.com>
2021-07-29 19:03:09 +0400

    Rarity charts popup

--- src/containers/rarityCharts/RarityCharts.jsx
+++ src/containers/rarityCharts/RarityCharts.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useContext } from 'react';
+import Popup from 'reactjs-popup';
+import RarityRankPopup from '../../components/popups/RarityRankPopup';
 import Filters from '../../components/rarityCharts/filters/Filters';
 import List from '../../components/rarityCharts/list/List';
 import Welcome from '../../components/rarityCharts/welcome/Welcome';
@@ -17,6 +19,15 @@ const RarityCharts = () => {
         <Filters />
         <List />
       </div>
+      <Popup
+        trigger={
+          <button type="button" className="light-button">
+            Rarity rank
+          </button>
+        }
+      >
+        {(close) => <RarityRankPopup onClose={close} />}
+      </Popup>
     </div>
   );
 };
