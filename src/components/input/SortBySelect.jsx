60f51fcad2289cfab0276641aa84d1993350ea7c
vikinatora <vikinatora@gmail.com>
2021-07-30 12:49:44 +0300

    create RarityCharts page filters and search section responsive ui

---
+++ src/components/input/SortBySelect.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import uuid from 'react-uuid';
+import arrowDown from '../../assets/images/browse-nft-arrow-down.svg';
+import './SortBySelect.scss';
+
+const SortBySelect = (props) => {
+  const { sortData, className, onChange, defaultValue } = props;
+  const [sortValue, setSortValue] = useState(defaultValue);
+  const [showSecondDropdown, setShowSecondDropdown] = useState(false);
+  const ref = useRef(null);
+  const ref2 = useRef(null);
+
+  useEffect(() => {
+    onChange(sortValue);
+  }, [sortValue]);
+
+  return (
+    <div
+      className={
+        !showSecondDropdown
+          ? `sort--by--select--parent ${className}`
+          : `sort--by--select--parent sort--by--select--parent--active ${className}`
+      }
+    >
+      <div
+        className={`dropdown ${showSecondDropdown ? 'open' : ''}`}
+        aria-hidden="true"
+        onClick={() => setShowSecondDropdown(!showSecondDropdown)}
+        ref={ref2}
+      >
+        <span>{sortValue}</span>
+        <img src={arrowDown} alt="Arrow down" className={showSecondDropdown ? 'rotate' : ''} />
+        {showSecondDropdown && (
+          <div className="dropdown--items">
+            {sortData.map((item) => (
+              <div
+                className="dropdown--item"
+                key={uuid()}
+                aria-hidden="true"
+                onClick={() => setSortValue(item)}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="box--shadow--effect--block" />
+    </div>
+  );
+};
+
+SortBySelect.propTypes = {
+  sortData: PropTypes.arrayOf(PropTypes.string),
+  className: PropTypes.string,
+  onChange: PropTypes.func,
+  defaultValue: PropTypes.string,
+};
+
+SortBySelect.defaultProps = {
+  sortData: [
+    'ending son',
+    'lowest price first',
+    'highest price first',
+    'recently listed',
+    'recently created',
+    'recently sold',
+    'most liked',
+  ],
+  className: '',
+  onChange: () => {},
+  defaultValue: 'Sort by',
+};
+
+export default SortBySelect;
