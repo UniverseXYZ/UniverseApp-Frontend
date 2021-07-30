79f860d0e336fbdb365f236d65f2909cdeb7a4e3
entrpyc <contact@a-angelov.eu>
2021-07-30 15:01:49 +0300

    fixed issue with reading stream

--- src/components/myNFTs/DeployedCollections.jsx
+++ src/components/myNFTs/DeployedCollections.jsx
@@ -26,33 +26,32 @@ const DeployedCollections = () => {
               <div className="saved__collection__box__header">
                 {collection.bgImage ? (
                   <img src={URL.createObjectURL(collection.bgImage)} alt={collection.name} />
-                ) : typeof collection.previewImage === 'string' &&
-                  collection.previewImage.startsWith('#') ? (
+                ) : typeof collection.coverUrl === 'string' &&
+                  collection.coverUrl.startsWith('#') ? (
                   <div
                     className="random__bg__color"
-                    style={{ backgroundColor: collection.previewImage }}
+                    style={{ backgroundColor: collection.coverUrl }}
                   />
                 ) : (
                   <img
                     className="blur"
-                    src={URL.createObjectURL(collection.previewImage)}
+                    src={collection.coverUrl && URL.createObjectURL(collection.coverUrl)}
                     alt={collection.name}
                   />
                 )}
               </div>
               <div className="saved__collection__box__body">
-                {typeof collection.previewImage === 'string' &&
-                collection.previewImage.startsWith('#') ? (
+                {typeof collection.coverUrl === 'string' && collection.coverUrl.startsWith('#') ? (
                   <div
                     className="random__avatar__color"
-                    style={{ backgroundColor: collection.previewImage }}
+                    style={{ backgroundColor: collection.coverUrl }}
                   >
                     {collection.name.charAt(0)}
                   </div>
                 ) : (
                   <img
                     className="collection__avatar"
-                    src={URL.createObjectURL(collection.previewImage)}
+                    src={collection.coverUrl && URL.createObjectURL(collection.coverUrl)}
                     alt={collection.name}
                   />
                 )}
