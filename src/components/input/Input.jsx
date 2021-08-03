bc51ca7955fb2b231e4164291b69c0f1411877ff
Hovhannes-S <sargsyanhovhannes213@gmail.com>
2021-08-03 12:37:30 +0400

    Set up an Auction

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import uuid from 'react-uuid';
 import './Inputs.scss';
 
-const Input = ({ label, error, className, hoverBoxShadowGradient, ...restProps }) => (
-  <div className="intup-l">
-    {label && (
-      <label htmlFor={uuid()} className="inp-label">
-        {label}
-      </label>
-    )}
-    <input id={uuid()} className={`${className} ${error ? 'error-inp' : 'inp'}`} {...restProps} />
-    {error && <p className="error-message">{error}</p>}
-    {hoverBoxShadowGradient && <div className="box--shadow--effect--block" />}
-  </div>
-);
+const Input = ({ label, error, className, hoverBoxShadowGradient, ...restProps }) => {
+  const [focusField, setFocusField] = useState('');
+  return (
+    <div className={`intup-l ${focusField}`}>
+      {label && (
+        <label htmlFor={uuid()} className="inp-label">
+          {label}
+        </label>
+      )}
+      <div>
+        <input
+          id={uuid()}
+          className={`${className}
+          ${error ? 'error-inp' : 'inp'}`}
+          onFocus={hoverBoxShadowGradient ? () => setFocusField('input--focus--field') : undefined}
+          onBlur={hoverBoxShadowGradient ? () => setFocusField('') : undefined}
+          {...restProps}
+        />
+        {error && <p className="error-message">{error}</p>}
+        {hoverBoxShadowGradient && <div className="box--shadow--effect--block" />}
+      </div>
+    </div>
+  );
+};
 
 Input.propTypes = {
   label: PropTypes.oneOfType([PropTypes.any]),
