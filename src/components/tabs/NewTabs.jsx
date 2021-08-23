ee307466c78eea4614351aec8353bcf513c878d8
Hovhannes-S <sargsyanhovhannes213@gmail.com>
2021-08-23 12:02:55 +0400

    Updated arrow tabs

--- src/components/tabs/NewTabs.jsx
+++ src/components/tabs/NewTabs.jsx
@@ -2,6 +2,18 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useLocation, useHistory, Switch, Redirect, Route } from 'react-router-dom';
 import './NewTabs.scss';
+import arrowBlackDesktop from '../../assets/images/arrow-tabs/arrow-black-desktop.png';
+import arrowBlackTablet from '../../assets/images/arrow-tabs/arrow-black-tablet.png';
+import arrowBlackMobile from '../../assets/images/arrow-tabs/arrow-black-mobile.png';
+import arrowGreyDesktop from '../../assets/images/arrow-tabs/arrow-grey-desktop.png';
+import arrowGreyTablet from '../../assets/images/arrow-tabs/arrow-grey-tablet.png';
+import arrowGreyMobile from '../../assets/images/arrow-tabs/arrow-grey-mobile.png';
+import arrowWhiteDesktop from '../../assets/images/arrow-tabs/arrow-white-desktop.png';
+import arrowWhiteTablet from '../../assets/images/arrow-tabs/arrow-white-tablet.png';
+import arrowWhiteMobile from '../../assets/images/arrow-tabs/arrow-white-mobile.png';
+import arrowHoverDesktop from '../../assets/images/arrow-tabs/arrow-hover-desktop.png';
+import arrowHoverTablet from '../../assets/images/arrow-tabs/arrow-hover-tablet.png';
+import arrowHoverMobile from '../../assets/images/arrow-tabs/arrow-hover-mobile.png';
 
 const NewTabs = (props) => {
   const { tabData } = props;
@@ -10,11 +22,23 @@ const NewTabs = (props) => {
   const history = useHistory();
   const { pathname } = location;
   const [routesArray, setRoutesArray] = useState([]);
+  const [mobile, setMobile] = useState(false);
+  const [tablet, setTablet] = useState(false);
+  const [hover, setHover] = useState(-1);
+
   useEffect(() => {
     const routes = tabData.map((elem) => elem.route);
     setRoutesArray(routes.splice(0, routes.indexOf(pathname) + 1));
   }, [pathname]);
-  console.log(routesArray);
+
+  useEffect(() => {
+    if (Number(window.innerWidth) <= 768) setMobile(true);
+    else setMobile(false);
+
+    if (Number(window.innerWidth) > 768 && Number(window.innerWidth) <= 1230) setTablet(true);
+    else setTablet(false);
+  }, []);
+
   return (
     <div className="new--tab--parent--block">
       <div className="new--tab--container">
@@ -29,6 +53,33 @@ const NewTabs = (props) => {
                     : routesArray.includes(route)
                     ? 'verification--step--tab'
                     : 'disabled';
+                const arrowBlack = mobile
+                  ? arrowBlackMobile
+                  : tablet
+                  ? arrowBlackTablet
+                  : arrowBlackDesktop;
+                const arrowGrey = mobile
+                  ? arrowGreyMobile
+                  : tablet
+                  ? arrowGreyTablet
+                  : arrowGreyDesktop;
+                const arrowWhite = mobile
+                  ? arrowWhiteMobile
+                  : tablet
+                  ? arrowWhiteTablet
+                  : arrowWhiteDesktop;
+                const arrowHover = mobile
+                  ? arrowHoverMobile
+                  : tablet
+                  ? arrowHoverTablet
+                  : arrowHoverDesktop;
+                const srcArrowTab =
+                  pathname === route
+                    ? arrowBlack
+                    : routesArray.includes(route)
+                    ? arrowWhite
+                    : arrowGrey;
+                let src;
                 const leftPosition = 6;
                 return (
                   <div
@@ -46,7 +97,24 @@ const NewTabs = (props) => {
                       width: `${tabHeaderElemWidth}%`,
                       left: `-${leftPosition * index}px`,
                     }}
+                    onMouseOver={() => {
+                      if (srcArrowTab === arrowWhite) setHover(index);
+                    }}
+                    onFocus={() => {
+                      if (srcArrowTab === arrowWhite) setHover(index);
+                    }}
+                    onMouseOut={() => {
+                      setHover(-1);
+                    }}
+                    onBlur={() => {
+                      setHover(-1);
+                    }}
                   >
+                    <img
+                      className="arrow--tab"
+                      src={hover !== index ? srcArrowTab : arrowHover}
+                      alt="Arrow Tab"
+                    />
                     <li className={className}>
                       <div>
                         <img src={route === pathname ? iconActive : icon} alt={labelText} />
