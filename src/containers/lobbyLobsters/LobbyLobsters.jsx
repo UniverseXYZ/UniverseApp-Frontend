71861d0698735be7412445419443284cf0dc3ee1
VahanMuradyan <10vahan.m16@gmail.com>
2021-08-26 12:00:26 +0400

    Lobby lobster - Hero section

--- src/containers/lobbyLobsters/LobbyLobsters.jsx
+++ src/containers/lobbyLobsters/LobbyLobsters.jsx
@@ -1,31 +1,16 @@
 import React, { useContext, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import './LobbyLobsters.scss';
 import HeroSection from '../../components/lobbyLobsters/heroSection/HeroSection';
-import WelcomeWrapper from '../../components/ui-elements/WelcomeWrapper';
 import AppContext from '../../ContextAPI';
-import heroSectionImage from '../../assets/images/lobby-lobsters/floating-nfts.png';
 
 const LobbyLobsters = () => {
   const { setDarkMode } = useContext(AppContext);
-  const history = useHistory();
   useEffect(() => {
-    setDarkMode(true);
+    setDarkMode(false);
   }, []);
 
   return (
     <div className="lobby--lobsters--page">
-      <WelcomeWrapper
-        title="Lobby Lobsters"
-        hintText="They wear the suits for us so we dont have too"
-        btnText="Mint Lobby Lobster"
-        btnOnClick={() => history.push('/my-nfts')}
-        ellipsesLeft={false}
-        ellipsesRight={false}
-      >
-        <img src={heroSectionImage} alt="Hero" />
-      </WelcomeWrapper>
-      {/* <HeroSection /> */}
+      <HeroSection />
     </div>
   );
 };
