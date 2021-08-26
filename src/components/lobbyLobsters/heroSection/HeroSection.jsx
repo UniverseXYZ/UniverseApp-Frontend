7448f42374c5e849c6e27c9628a731940ace1f3a
VahanMuradyan <10vahan.m16@gmail.com>
2021-08-26 10:26:30 +0400

    Lobby lobsters structure

---
+++ src/components/lobbyLobsters/heroSection/HeroSection.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './HeroSection.scss';
+
+const HeroSection = () => <div className="lobby--lobsters--hero--section">Hero Section</div>;
+
+export default HeroSection;
