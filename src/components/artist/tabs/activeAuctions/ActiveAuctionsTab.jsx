1d60b0a8df4164a30e07f78d446f85814a53725d
Hovhannes-S <sargsyanhovhannes213@gmail.com>
2021-08-30 13:02:04 +0400

    changes after QA

--- src/components/artist/tabs/activeAuctions/ActiveAuctionsTab.jsx
+++ src/components/artist/tabs/activeAuctions/ActiveAuctionsTab.jsx
@@ -1,19 +1,22 @@
 import React, { useState, useContext } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import Pagination from '../../../pagination/Pagionation.jsx';
 import ItemsPerPageDropdown from '../../../pagination/ItemsPerPageDropdown.jsx';
 import ActiveAuctionsList from './ActiveAuctionsList.jsx';
 import { PLACEHOLDER_ACTIVE_AUCTIONS } from '../../../../utils/fixtures/ActiveAuctionsDummyData';
 import AppContext from '../../../../ContextAPI.js';
+import bubleIcon from '../../../../assets/images/text-bubble.png';
+import Exclamation from '../../../../assets/images/Exclamation.svg';
 
 const ActiveAuctionsTab = () => {
-  const { myAuctions, activeAuctions, futureAuctions } = useContext(AppContext);
+  const { myAuctions, activeAuctions, futureAuctions, loggedInArtist } = useContext(AppContext);
   const location = useLocation();
   const [offset, setOffset] = useState(0);
   const [perPage, setPerPage] = useState(6);
   const artistActiveAuctions = PLACEHOLDER_ACTIVE_AUCTIONS.filter(
     (auction) => auction.artist.id === location.state.id
   );
+  const history = useHistory();
 
   return artistActiveAuctions.length ? (
     <>
@@ -24,8 +27,33 @@ const ActiveAuctionsTab = () => {
       </div>
     </>
   ) : (
-    <div className="empty__nfts">
+    <div className="empty__auction">
+      <img src={bubleIcon} alt="Buble" />
       <h3>No active auctions found</h3>
+      {!loggedInArtist.name || !loggedInArtist.avatar ? (
+        <div className="warning__div">
+          <img src={Exclamation} alt="Warning" />
+          <p>
+            Please, fill out the profile details before you set up an auction.{' '}
+            <button type="button" onClick={() => history.push('/my-account')}>
+              Go to my profile
+            </button>
+            .
+          </p>
+        </div>
+      ) : (
+        <p className="desc">Create your first auction by clicking the button below</p>
+      )}
+      <button
+        type="button"
+        className="light-button set_up"
+        onClick={() =>
+          loggedInArtist.name && loggedInArtist.avatar && history.push('/setup-auction')
+        }
+        disabled={!loggedInArtist.name || !loggedInArtist.avatar}
+      >
+        Set up auction
+      </button>
     </div>
   );
 };
--- src/components/artist/tabs/pastAuctions/PastAuctionsTab.jsx
+++ src/components/artist/tabs/pastAuctions/PastAuctionsTab.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
 import Pagination from '../../../pagination/Pagionation.jsx';
 import ItemsPerPageDropdown from '../../../pagination/ItemsPerPageDropdown.jsx';
 import PastAuctionsList from './PastAuctionsList.jsx';
 import { PLACEHOLDER_PAST_AUCTIONS } from '../../../../utils/fixtures/PastAuctionsDummyData';
+import AppContext from '../../../../ContextAPI.js';
+import bubleIcon from '../../../../assets/images/text-bubble.png';
+import Exclamation from '../../../../assets/images/Exclamation.svg';
 
 const PastAuctionsTab = () => {
   const location = useLocation();
@@ -12,6 +15,8 @@ const PastAuctionsTab = () => {
   const artistPastAuctions = PLACEHOLDER_PAST_AUCTIONS.filter(
     (auction) => auction.artist.id === location.state.id
   );
+  const history = useHistory();
+  const { loggedInArtist } = useContext(AppContext);
 
   return artistPastAuctions.length ? (
     <>
@@ -22,8 +27,33 @@ const PastAuctionsTab = () => {
       </div>
     </>
   ) : (
-    <div className="empty__nfts">
+    <div className="empty__auction">
+      <img src={bubleIcon} alt="Buble" />
       <h3>No past auctions found</h3>
+      {!loggedInArtist.name || !loggedInArtist.avatar ? (
+        <div className="warning__div">
+          <img src={Exclamation} alt="Warning" />
+          <p>
+            Please, fill out the profile details before you set up an auction.{' '}
+            <button type="button" onClick={() => history.push('/my-account')}>
+              Go to my profile
+            </button>
+            .
+          </p>
+        </div>
+      ) : (
+        <p className="desc">Create your first auction by clicking the button below</p>
+      )}
+      <button
+        type="button"
+        className="light-button set_up"
+        onClick={() =>
+          loggedInArtist.name && loggedInArtist.avatar && history.push('/setup-auction')
+        }
+        disabled={!loggedInArtist.name || !loggedInArtist.avatar}
+      >
+        Set up auction
+      </button>
     </div>
   );
 };
