b51250daee98cd17a51e9b805c9d51c27b8093d3
VahanMuradyan <10vahan.m16@gmail.com>
2021-08-30 18:55:13 +0400

    Lobby Lobsters page

---
+++ src/components/lobbyLobsters/heroSection/animations/EndlessWaveAnimation.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from '../../../../utils/animations/endless_wave_animation.json';
+
+const EndlessWaveAnimation = () => (
+  <Lottie
+    options={{
+      loop: true,
+      autoplay: true,
+      animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice',
+      },
+    }}
+    isClickToPauseDisabled
+  />
+);
+
+export default EndlessWaveAnimation;
