6e2475c5058eb54c980c154580ffaa0b41ff4d71
VahanMuradyan <10vahan.m16@gmail.com>
2021-09-06 16:28:42 +0400

    Lobby lobsters page changes

--- src/components/lobbyLobsters/heroSection/HeroSection.jsx
+++ src/components/lobbyLobsters/heroSection/HeroSection.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import './HeroSection.scss';
+import { useHistory } from 'react-router-dom';
 import FloatingNFTsAnimation from './animations/FloatingNFTsAnimation';
 import EndlessWaveAnimation from './animations/EndlessWaveAnimation';
 
-const HeroSection = React.forwardRef((props, ref) => (
-  <div className="lobby--lobsters--hero--section">
-    <div className="wave--img">
-      <EndlessWaveAnimation />
-    </div>
-    <div className="hero--img">
-      <FloatingNFTsAnimation />
-    </div>
-    <div className="lobby--lobsters--hero--section--container">
-      <div className="left--section">
-        <h1>Lobby Lobsters</h1>
-        <p>They wear suits, so we don&#39;t have to.</p>
-        <button type="button" onClick={() => ref.current.scrollIntoView()}>
-          Mint Lobby Lobster
-        </button>
+const HeroSection = React.forwardRef((props, ref) => {
+  const history = useHistory();
+
+  return (
+    <div className="lobby--lobsters--hero--section">
+      <div className="wave--img">
+        <EndlessWaveAnimation />
+      </div>
+      <div className="hero--img">
+        <FloatingNFTsAnimation />
+      </div>
+      <div className="lobby--lobsters--hero--section--container">
+        <div className="left--section">
+          <h1>Lobby Lobsters</h1>
+          <p>They wear suits, so we don&#39;t have to.</p>
+          <button
+            type="button"
+            onClick={() => {
+              history.push('/my-nfts');
+            }}
+          >
+            My Lobby Lobster
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-));
+  );
+});
 
 export default HeroSection;
