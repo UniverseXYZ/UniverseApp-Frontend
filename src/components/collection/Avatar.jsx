037869f8c96f7632d8ffdbfdc4ba7c97b6fd0d75
vikinatora <vikinatora@gmail.com>
2021-10-03 20:53:21 +0300

    fix:(universe collection page): shows universe picture

--- src/components/collection/Avatar.jsx
+++ src/components/collection/Avatar.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { getCollectionBackgroundColor } from '../../utils/helpers';
+import { useMyNftsContext } from '../../contexts/MyNFTsContext';
+import universeIcon from '../../assets/images/universe-img.svg';
 
-const Avatar = ({ selectedCollection }) => (
-  <div className="collection__image">
-    {!selectedCollection.coverUrl ? (
-      <div
-        className="random__bg__color"
-        style={{
-          backgroundColor: getCollectionBackgroundColor(selectedCollection),
-        }}
-      >
-        {selectedCollection.name.charAt(0)}
-      </div>
-    ) : (
-      <img
-        className="collection__avatar"
-        src={selectedCollection.coverUrl}
-        alt={selectedCollection.name}
-      />
-    )}
-  </div>
-);
+const Avatar = ({ selectedCollection }) => {
+  const { universeCollectionDisplayName } = useMyNftsContext();
+  return (
+    <div className="collection__image">
+      {selectedCollection.name === universeCollectionDisplayName ? (
+        <img className="bg" src={universeIcon} alt={selectedCollection.name} />
+      ) : !selectedCollection.coverUrl ? (
+        <div
+          className="random__bg__color"
+          style={{
+            backgroundColor: getCollectionBackgroundColor(selectedCollection),
+          }}
+        >
+          {selectedCollection.name.charAt(0)}
+        </div>
+      ) : (
+        <img
+          className="collection__avatar"
+          src={selectedCollection.coverUrl}
+          alt={selectedCollection.name}
+        />
+      )}
+    </div>
+  );
+};
 
 Avatar.propTypes = {
   selectedCollection: PropTypes.oneOfType([PropTypes.object]).isRequired,
