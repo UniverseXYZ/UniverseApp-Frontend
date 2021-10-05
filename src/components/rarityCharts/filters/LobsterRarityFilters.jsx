08ed3ee6d07248ad3cc491caf74bfaeec241d1c1
vikinatora <vikinatora@gmail.com>
2021-10-05 15:02:52 +0300

    fix(universe-nfts): fix collection dropdown to function correctly

--- src/components/rarityCharts/filters/LobsterRarityFilters.jsx
+++ src/components/rarityCharts/filters/LobsterRarityFilters.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import RaritySortBySelect from '../../input/RaritySortBySelect';
 import RaritySortByOrder from '../../input/RaritySortByOrder';
@@ -18,54 +18,71 @@ const LobsterRarityFilters = (props) => {
     CollectionFilter,
   } = props;
 
+  const [width, setWidth] = useState(window.innerWidth);
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
+
   return (
     <div className="lobsters-rarity--charts--search--and--filters--container">
-      <div className="rarity--charts--search--and--filters--row">
-        <CollectionFilter />
-        <div className="rarity--charts--search--and--floor--price">
-          <RaritySearchField
-            placeholder="Search items"
-            searchText={searchText}
-            setSearchText={setSearchText}
-            resetPagination={resetPagination}
-          />
-        </div>
-        <RaritySortByOrder
-          setSortDir={setSortDir}
-          sortDir={sortDir}
-          resetPagination={resetPagination}
-        />
-        <RaritySortBySelect
-          id="sort--select"
-          defaultValue="Rarity Score"
-          sortData={['Rarity Score', 'Rank', 'Polymorph Id']}
-          setSortField={setSortField}
-          resetPagination={resetPagination}
-        />
-      </div>
-      <div className="rarity--charts--mobile">
-        <CollectionFilter />
-        <RaritySortBySelect
-          id="sort--select"
-          defaultValue="Rarity Score"
-          sortData={['Rarity Score', 'Rank', 'Polymorph Id']}
-          setSortField={setSortField}
-          resetPagination={resetPagination}
-        />
-        <div className="rarity--charts--search--and--floor--price">
-          <RaritySearchField
-            placeholder="Search items"
-            searchText={searchText}
-            setSearchText={setSearchText}
-            resetPagination={resetPagination}
-          />
+      {!isMobile ? (
+        <div className="rarity--charts--search--and--filters--row">
+          <CollectionFilter />
+          <div className="rarity--charts--search--and--floor--price">
+            <RaritySearchField
+              placeholder="Search items"
+              searchText={searchText}
+              setSearchText={setSearchText}
+              resetPagination={resetPagination}
+            />
+          </div>
           <RaritySortByOrder
             setSortDir={setSortDir}
             sortDir={sortDir}
             resetPagination={resetPagination}
           />
+          <RaritySortBySelect
+            id="sort--select"
+            defaultValue="Rarity Score"
+            sortData={['Rarity Score', 'Rank', 'Polymorph Id']}
+            setSortField={setSortField}
+            resetPagination={resetPagination}
+          />
         </div>
-      </div>
+      ) : (
+        <div className="rarity--charts--mobile">
+          <CollectionFilter />
+          <RaritySortBySelect
+            id="sort--select"
+            defaultValue="Rarity Score"
+            sortData={['Rarity Score', 'Rank', 'Polymorph Id']}
+            setSortField={setSortField}
+            resetPagination={resetPagination}
+          />
+          <div className="rarity--charts--search--and--floor--price">
+            <RaritySearchField
+              placeholder="Search items"
+              searchText={searchText}
+              setSearchText={setSearchText}
+              resetPagination={resetPagination}
+            />
+            <RaritySortByOrder
+              setSortDir={setSortDir}
+              sortDir={sortDir}
+              resetPagination={resetPagination}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
