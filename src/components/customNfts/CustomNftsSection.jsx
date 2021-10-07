24606e7dd5b43042b1ac694bc1b419329ef7c2a7
unknown <h.petrosyan07@gmail.com>
2021-10-07 14:36:32 +0300

    fix:new component for custom nft switch button section

---
+++ src/components/customNfts/CustomNftsSection.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import './CustomNftsSection.scss';
+import PropTypes from 'prop-types';
+import infoIcon from '../../assets/images/icon.svg';
+
+const CustomNftsSection = ({ custom, setCustom, setValues }) => {
+  const [hideIcon, setHideIcon] = useState(false);
+
+  const handeClickCustom = (e) => {
+    if (e.target.checked) {
+      setValues((prevValue) => ({ ...prevValue, nftsPerWinner: '' }));
+    }
+
+    setCustom(e.target.checked);
+  };
+
+  return (
+    <div className="custom-info">
+      <span>
+        Custom
+        <img
+          src={infoIcon}
+          alt="Info Icon"
+          onMouseOver={() => setHideIcon(true)}
+          onFocus={() => setHideIcon(true)}
+          onMouseLeave={() => setHideIcon(false)}
+          onBlur={() => setHideIcon(false)}
+        />
+      </span>
+      <label className="switch">
+        <input type="checkbox" value={custom} checked={custom} onChange={handeClickCustom} />
+        <span className="slider round" />
+      </label>
+      {hideIcon && (
+        <div className="info-text custom-info-text">
+          <p>
+            Use this parameter in case you want to deposit NFTs from incompatible collections, and
+            it’s impossible to distinguish which one is more rare looking at the token ID. The other
+            use case is when NFTs from different collections could not be distributed evenly.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+CustomNftsSection.propTypes = {
+  custom: PropTypes.bool,
+  setCustom: PropTypes.func.isRequired,
+  setValues: PropTypes.func.isRequired,
+};
+
+CustomNftsSection.defaultProps = {
+  custom: false,
+};
+export default CustomNftsSection;
