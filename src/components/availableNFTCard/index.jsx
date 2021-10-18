dd3349a38b3cd54c2ddae4f94bf14d1ac5a036d5
taskudis <85156108+taskudis@users.noreply.github.com>
2021-10-18 11:14:44 +0300

    fix: Create Reward Tier
    - Preserve state between different Tiers creation, mark all nfts used from other tiers as disabled
    - Properly send the request object to the BE with ordered slots

--- src/components/availableNFTCard/index.jsx
+++ src/components/availableNFTCard/index.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.scss';
 import PropTypes from 'prop-types';
 import audioIcon from '../../assets/images/marketplace/audio-icon.svg';
@@ -8,113 +8,140 @@ import mp3Icon from '../../assets/images/mp3-icon.png';
 import Select from '../availableNFTsEditionSelect';
 import universeIcon from '../../assets/images/universe-img.svg';
 
-const NFTCard = React.memo(({ data, onEditionClick, canSelect, winnersData, selectedWinner }) => {
-  const { nfts, collection, optimized_url: url, artworkType } = data;
+const NFTCard = React.memo(
+  ({ data, onEditionClick, canSelect, winnersData, selectedWinner, auction }) => {
+    const { nfts, collection, optimized_url: url, artworkType } = data;
 
-  const selectOptions = nfts.rewardAndTokenIds.map(({ tokenId, id }) => ({
-    value: `${tokenId}||${id}||${url}||${artworkType}`,
-    label: `#${tokenId}`,
-  }));
+    const selectOptions = nfts.rewardAndTokenIds.map(({ tokenId, id }) => ({
+      value: `${tokenId}||${id}||${url}||${artworkType}`,
+      label: `#${tokenId}`,
+    }));
 
-  const selectedWinnerData = winnersData.find((info) => info.slot === selectedWinner);
-  const selectedWinnerIds = selectedWinnerData && selectedWinnerData.nftIds.map((info) => info.id);
-  const otherWinners = winnersData.filter((info) => info.slot !== selectedWinner) || [];
-  const otherWinnersIds = otherWinners.reduce((res, cur) => {
-    const ids = cur.nftIds.map((i) => i.id);
-    res.push(...ids);
-    return res;
-  }, []);
+    const selectedWinnerData = winnersData.find((info) => info.slot === selectedWinner);
+    const selectedWinnerIds =
+      selectedWinnerData && selectedWinnerData.nftIds.map((info) => info.id);
+    const otherWinners = winnersData.filter((info) => info.slot !== selectedWinner) || [];
+    const otherWinnersIds = otherWinners.reduce((res, cur) => {
+      const ids = cur.nftIds.map((i) => i.id);
+      res.push(...ids);
+      return res;
+    }, []);
 
-  // Mark the options as pre-selected or disabled
-  const updatedOptions = selectOptions.map((info) => {
-    const [edition, id, _url, artWorkType] = info.value.split('||');
-    // Preselect
-    if (selectedWinnerIds && selectedWinnerIds.includes(id)) info.isSelected = true;
+    // Mark the options as pre-selected or disabled
+    const updatedOptionsForCurrentTier = selectOptions.map((info) => {
+      const infoCopy = { ...info };
+      const [edition, id, _url, artWorkType] = infoCopy.value.split('||');
 
-    // Disable
-    if (otherWinnersIds && otherWinnersIds.includes(id)) {
-      info.isDisabled = true;
-      info.isSelected = false;
-    }
-    return info;
-  });
+      if (selectedWinnerIds && selectedWinnerIds.includes(id)) infoCopy.isSelected = true;
 
-  const hasSelectedEditions = updatedOptions.find((item) => item.isSelected);
+      // Disable
+      if (otherWinnersIds && otherWinnersIds.includes(id)) {
+        infoCopy.isDisabled = true;
+        infoCopy.isSelected = false;
+      }
+      return infoCopy;
+    });
 
-  return (
-    <div
-      className={`nft--card nft__box ${canSelect ? 'can--select' : 'disabled'} ${
-        hasSelectedEditions ? 'has-selected-editions' : ''
-      }`}
-    >
-      <div className="nft--card--body" aria-hidden="true">
-        <div aria-hidden="true">
-          {nfts.artworkType !== 'audio/mpeg' && nfts.artworkType !== 'mp4' && (
-            <img className="nft--image" src={nfts.optimized_url} alt={nfts.name} />
-          )}
-          {nfts.artworkType === 'mp4' && (
-            <video
-              onMouseOver={(event) => event.target.play()}
-              onFocus={(event) => event.target.play()}
-              onMouseOut={(event) => event.target.pause()}
-              onBlur={(event) => event.target.pause()}
-              muted
-            >
-              <source src={nfts.optimized_url} type="video/mp4" />
-              <track kind="captions" />
-              Your browser does not support the video tag.
-            </video>
-          )}
-          {nfts.artworkType === 'audio/mpeg' && (
-            <img className="nft--image" src={mp3Icon} alt={nfts.name} />
-          )}
-          {nfts.artworkType === 'audio/mpeg' && (
-            <div className="video--icon">
-              <img src={audioIcon} alt="Video Icon" />
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="nft--card--footer">
-        <div className="name--and--price">
-          <h4>{nfts.name}</h4>
-          <Select options={updatedOptions} onChange={onEditionClick} isMulti />
-        </div>
-        <div className="quantity--and--offer">
-          <div className="collection__details">
-            {collection && (
-              <>
-                {collection.address ===
-                process.env.REACT_APP_UNIVERSE_ERC_721_ADDRESS.toLowerCase() ? (
-                  <img src={universeIcon} alt={collection.name} />
-                ) : (
-                  <img src={collection.coverUrl} alt={collection.name} />
-                )}
-                <span>{collection.name}</span>
-              </>
+    // Mark editions as used if they are being used in other tiers
+    const rewardTiersUsedNFTsIds =
+      auction.rewardTiers &&
+      auction.rewardTiers.reduce((res, curr) => {
+        const ids = [];
+        curr.nftSlots.forEach((slot) => ids.push(...slot.nftIds));
+        res.push(...ids);
+        return res;
+      }, []);
+
+    const updateOptionsWithAllTiersData = updatedOptionsForCurrentTier.map((info) => {
+      const infoCopy = { ...info };
+      const [edition, id, _url, artWorkType] = infoCopy.value.split('||');
+
+      // Disable
+      if (rewardTiersUsedNFTsIds && rewardTiersUsedNFTsIds.includes(id)) {
+        infoCopy.isDisabled = true;
+        infoCopy.isSelected = false;
+      }
+      return infoCopy;
+    });
+
+    const hasSelectedEditions = updatedOptionsForCurrentTier.find((item) => item.isSelected);
+
+    return (
+      <div
+        className={`nft--card nft__box ${canSelect ? 'can--select' : 'disabled'} ${
+          hasSelectedEditions ? 'has-selected-editions' : ''
+        }`}
+      >
+        <div className="nft--card--body" aria-hidden="true">
+          <div aria-hidden="true">
+            {nfts.artworkType !== 'audio/mpeg' && nfts.artworkType !== 'mp4' && (
+              <img className="nft--image" src={nfts.optimized_url} alt={nfts.name} />
+            )}
+            {nfts.artworkType === 'mp4' && (
+              <video
+                onMouseOver={(event) => event.target.play()}
+                onFocus={(event) => event.target.play()}
+                onMouseOut={(event) => event.target.pause()}
+                onBlur={(event) => event.target.pause()}
+                muted
+              >
+                <source src={nfts.optimized_url} type="video/mp4" />
+                <track kind="captions" />
+                Your browser does not support the video tag.
+              </video>
+            )}
+            {nfts.artworkType === 'audio/mpeg' && (
+              <img className="nft--image" src={mp3Icon} alt={nfts.name} />
+            )}
+            {nfts.artworkType === 'audio/mpeg' && (
+              <div className="video--icon">
+                <img src={audioIcon} alt="Video Icon" />
+              </div>
             )}
           </div>
-          <div className="collection__count">{`x${nfts.rewardAndTokenIds.length}`}</div>
         </div>
-      </div>
-      {nfts.rewardAndTokenIds.length > 1 && (
-        <>
-          <div className="nft__box__highlight__one" />
-          <div className="nft__box__highlight__two" />
-        </>
-      )}
-      {/* {selectedNFTsIds && selectedNFTsIds.includes(nft.id) && (
+        <div className="nft--card--footer">
+          <div className="name--and--price">
+            <h4>{nfts.name}</h4>
+            <Select options={updateOptionsWithAllTiersData} onChange={onEditionClick} isMulti />
+          </div>
+          <div className="quantity--and--offer">
+            <div className="collection__details">
+              {collection && (
+                <>
+                  {collection.address ===
+                  process.env.REACT_APP_UNIVERSE_ERC_721_ADDRESS.toLowerCase() ? (
+                    <img src={universeIcon} alt={collection.name} />
+                  ) : (
+                    <img src={collection.coverUrl} alt={collection.name} />
+                  )}
+                  <span>{collection.name}</span>
+                </>
+              )}
+            </div>
+            <div className="collection__count">{`x${nfts.rewardAndTokenIds.length}`}</div>
+          </div>
+        </div>
+        {nfts.rewardAndTokenIds.length > 1 && (
+          <>
+            <div className="nft__box__highlight__one" />
+            <div className="nft__box__highlight__two" />
+          </>
+        )}
+        {/* {selectedNFTsIds && selectedNFTsIds.includes(nft.id) && (
         <div className="nft--selected">
           <img src={checkIcon} alt="img" />
         </div>
       )} */}
-    </div>
-  );
-});
+      </div>
+    );
+  }
+);
 
 NFTCard.propTypes = {
   data: PropTypes.oneOfType([PropTypes.object]).isRequired,
   winnersData: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  auction: PropTypes.oneOfType([PropTypes.object]).isRequired,
   onEditionClick: PropTypes.func.isRequired,
   canSelect: PropTypes.bool.isRequired,
   selectedWinner: PropTypes.number.isRequired,
