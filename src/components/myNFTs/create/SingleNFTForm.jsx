47ca2885c34239c396924415675241a956d353d8
vikinatora <vikinatora@gmail.com>
2021-10-20 16:02:41 +0300

    fix(bugs): from Babenko and Vlad in testing channel

--- src/components/myNFTs/create/SingleNFTForm.jsx
+++ src/components/myNFTs/create/SingleNFTForm.jsx
@@ -50,6 +50,11 @@ const MAX_FIELD_CHARS_LENGTH = {
   royaltiesCount: 5,
 };
 
+const MIN_IMAGE_SIZE = {
+  width: 800,
+  height: 800,
+};
+
 const COLLECTIONS_PER_ROW = 4;
 
 const SingleNFTForm = () => {
@@ -102,7 +107,9 @@ const SingleNFTForm = () => {
   const [royaltyValidAddress, setRoyaltyValidAddress] = useState(true);
   const [royaltiesMapIndexes, setRoyaltiesMapIndexes] = useState({});
   const [propertiesIndexes, setPropertiesMapIndexes] = useState({});
-  const [selectedCollection, setSelectedCollection] = useState(universeCollection);
+  const [selectedCollection, setSelectedCollection] = useState(
+    location.state.collection || universeCollection
+  );
   const [showCongratsPopup, setShowCongratsPopup] = useState(false);
   const [showCongratsMintedSavedForLater, setShowCongratsMintedSavedForLater] = useState(false);
   const [showCongratsPopupOnSaveForLaterClick, setShowCongratsPopupOnSaveForLaterClick] =
@@ -112,7 +119,7 @@ const SingleNFTForm = () => {
   const [border, setBorder] = useState(false);
 
   useEffect(() => {
-    setSelectedCollection(universeCollection);
+    setSelectedCollection(location.state.collection || universeCollection);
   }, [universeCollection]);
 
   const hasAddressError = (royalty, index) => {
@@ -146,7 +153,7 @@ const SingleNFTForm = () => {
     temp.splice(index, 1);
     setRoyaltyAddress(temp);
 
-    const addressErrors = temp.filter((prop) => prop.error !== '');
+    const addressErrors = temp.filter((prop) => prop.error && prop.error !== '');
     setRoyaltyValidAddress(!addressErrors.length);
   };
 
@@ -208,6 +215,7 @@ const SingleNFTForm = () => {
     } else {
       inp.classList.remove('withsign');
     }
+
     const newProperties = royaltyAddress.map((royalty, royaltyIndex) => {
       if (royaltyIndex === index) {
         return {
@@ -221,7 +229,7 @@ const SingleNFTForm = () => {
       (accumulator, current) => accumulator + Number(current.amount),
       0
     );
-    if (result <= 100 && val >= 0) {
+    if (result <= 99 && val >= 0) {
       setRoyaltyAddress(newProperties);
     }
   };
@@ -305,18 +313,56 @@ const SingleNFTForm = () => {
         previewImage: 'File format must be PNG, JPEG, MP3, GIF, WEBP or MP4 (Max Size: 30mb)',
       });
     } else if (
-      (file.type === 'audio/mpeg' ||
-        file.type === 'video/mp4' ||
+      (file.type === 'video/mp4' ||
         file.type === 'image/jpeg' ||
         file.type === 'image/webp' ||
-        file.type === 'audio/mpeg' ||
         file.type === 'image/gif' ||
-        file.type === 'image/jpeg' ||
         file.type === 'image/png') &&
       file.size / 1048576 < 30
     ) {
-      setPreviewImage(file);
-      setErrors({ ...errors, previewImage: '' });
+      if (
+        file.type === 'image/jpeg' ||
+        file.type === 'image/webp' ||
+        file.type === 'image/png' ||
+        file.type === 'image/gif'
+      ) {
+        const reader = new FileReader();
+        // Read the contents of Image File.
+        reader.readAsDataURL(file);
+        reader.onload = function (e) {
+          const image = new Image();
+          image.src = e.target.result;
+          image.onload = function () {
+            const { height, width } = this;
+            if (height < MIN_IMAGE_SIZE.height || width < MIN_IMAGE_SIZE.width) {
+              setErrors({
+                ...errors,
+                previewImage: 'File must be at least 800x800px',
+              });
+            } else {
+              setPreviewImage(file);
+              setErrors({ ...errors, previewImage: '' });
+            }
+          };
+        };
+      } else if (file.type === 'video/mp4') {
+        const video = document.createElement('video');
+        video.addEventListener('loadedmetadata', (event) => {
+          if (
+            video.videoHeight < MIN_IMAGE_SIZE.height ||
+            video.videoWidth < MIN_IMAGE_SIZE.width
+          ) {
+            setErrors({
+              ...errors,
+              previewImage: 'File must be at least 800x800px',
+            });
+          } else {
+            setPreviewImage(file);
+            setErrors({ ...errors, previewImage: '' });
+          }
+        });
+        video.src = URL.createObjectURL(file);
+      }
     } else {
       setPreviewImage('');
       setErrors({
@@ -621,7 +667,6 @@ const SingleNFTForm = () => {
   useEffect(() => {
     setShowPrompt(true);
   }, [location.pathname]);
-
   useEffect(() => {
     if (!showLoadingPopup) setActiveTxHashes([]);
   }, [showLoadingPopup]);
@@ -798,7 +843,7 @@ const SingleNFTForm = () => {
             <h5>
               <span>Description (optional)</span>
               <p className="input-max-chars">
-                {description.length}/{MAX_FIELD_CHARS_LENGTH.description}
+                {description ? description.length : 0}/{MAX_FIELD_CHARS_LENGTH.description}
               </p>
             </h5>
             <textarea
@@ -897,7 +942,8 @@ const SingleNFTForm = () => {
                       <h5>
                         <span>Property name</span>
                         <p className="input-max-chars">
-                          {property.name.length}/{MAX_FIELD_CHARS_LENGTH.propertyName}
+                          {property.name ? property.name.length : 0}/
+                          {MAX_FIELD_CHARS_LENGTH.propertyName}
                         </p>
                       </h5>
                       <Input
@@ -916,7 +962,8 @@ const SingleNFTForm = () => {
                       <h5>
                         <span>Value</span>
                         <p className="input-max-chars">
-                          {property.value.length}/{MAX_FIELD_CHARS_LENGTH.propertyValue}
+                          {property.value ? property.value.length : 0}/
+                          {MAX_FIELD_CHARS_LENGTH.propertyValue}
                         </p>
                       </h5>
                       <Input
