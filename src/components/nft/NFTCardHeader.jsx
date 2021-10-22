fa6c4e87bb546e2dd687e563bd1b0bc48c23c61e
vikinatora <vikinatora@gmail.com>
2021-10-22 14:46:14 +0300

    fix(nft-creator):hide creator if it doesn't exist (nfts from moralis)

--- src/components/nft/NFTCardHeader.jsx
+++ src/components/nft/NFTCardHeader.jsx
@@ -61,8 +61,10 @@ const NFTCardHeader = ({ nft, creator, owner, collection }) => {
   return (
     <div className="nft--card--header">
       <div className="three--images">
-        {creator &&
-        (creator.avatar || (creator.profileImageUrl && creator.profileImageUrl.length > 48)) ? (
+        {!creator ? (
+          <></>
+        ) : creator &&
+          (creator.avatar || (creator.profileImageUrl && creator.profileImageUrl.length > 48)) ? (
           <>
             <div
               className="creator--details"
