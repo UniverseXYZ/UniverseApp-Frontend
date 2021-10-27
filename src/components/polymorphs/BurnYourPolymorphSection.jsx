e1b8cfae9fe76d573b35ebd7e4b9a381bce0f2c4
anahit-71 <>
2021-10-27 10:03:39 +0300

    feat: updated burn polymorph section, added video

--- src/components/polymorphs/BurnYourPolymorphSection.jsx
+++ src/components/polymorphs/BurnYourPolymorphSection.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useEffect, useState } from 'react';
+import React, { useLayoutEffect, useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import uuid from 'react-uuid';
 import { AnimatedOnScroll } from 'react-animated-css-onscroll';
@@ -9,6 +9,7 @@ import './styles/Section4.scss';
 import mobilebackground from '../../assets/images/mobilebackground.png';
 import Section4LeftBackground from '../../assets/images/topmorph.png';
 import Button from '../button/Button';
+import leftSectionVideo from '../../assets/images/burn-to-mint/polymorph_burning.mp4';
 
 const getWindow = (width, changeStateFunc) => {
   const TABLET_SIZE = 992;
@@ -18,23 +19,39 @@ const getWindow = (width, changeStateFunc) => {
   else if (+width <= MOBILE_SIZE) changeStateFunc('mobile');
 };
 
-const leftBlock = (windows, leftBackground, leftBackgroundMobile, backgroundImage) => (
-  <AnimatedOnScroll animationIn="fadeIn" animationInDelay={500}>
-    <div
-      className="section4--left--block"
-      style={
-        windows === 'mobile'
-          ? { height: window.innerWidth }
-          : windows === 'tablet'
-          ? {}
-          : { backgroundImage: `url(${leftBackground})`, height: backgroundImage ? '542px' : '' }
+const leftBlock = (leftVideo) => {
+  const ref = useRef();
+  const [loaded, setLoaded] = useState(false);
+  const VIDEO_READY_STATE = 4;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (ref.current && ref.current.readyState === VIDEO_READY_STATE) {
+        setLoaded(true);
+        clearInterval(interval);
       }
-    >
-      <img alt="mobile" src={leftBackgroundMobile} className="show--on--mobile" />
-      <img alt="tablet" src={leftBackground} className="show--on--tablet" />
-    </div>
-  </AnimatedOnScroll>
-);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [loaded]);
+  return (
+    <AnimatedOnScroll animationIn="fadeIn" animationInDelay={500}>
+      <div className="section4--left--block">
+        <video
+          ref={ref}
+          autoPlay
+          loop
+          muted
+          playsInline
+          style={{ display: loaded ? 'block' : 'none' }}
+        >
+          <source src={leftVideo} type="video/mp4" />
+          <track kind="captions" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </AnimatedOnScroll>
+  );
+};
 
 const rightBlock = (title, hintText, buttonText) => {
   const history = useHistory();
@@ -56,8 +73,15 @@ const rightBlock = (title, hintText, buttonText) => {
 };
 
 const BurnYourPolymorphSection = (props) => {
-  const { title, hintText, buttonText, backgroundImage, leftBackground, leftBackgroundMobile } =
-    props;
+  const {
+    title,
+    hintText,
+    buttonText,
+    backgroundImage,
+    leftBackground,
+    leftBackgroundMobile,
+    leftVideo,
+  } = props;
   const [windows, setWindows] = useState('browser');
   const [width, setWidth] = useState(window.innerWidth);
   useLayoutEffect(() => {
@@ -73,21 +97,16 @@ const BurnYourPolymorphSection = (props) => {
   }, [width]);
 
   return (
-    <div
-      className="polymorph--section4"
-      style={
-        backgroundImage
-          ? {
-              backgroundImage: backgroundImage ? `url(${backgroundImage})` : 'none',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-            }
-          : {}
-      }
-    >
+    <div className="polymorph--section4">
       <WrapperCenter className="polymorph--wrapper--center--section4">
         <WrapperCenterTwoColumns
-          leftBlock={leftBlock(windows, leftBackground, leftBackgroundMobile, backgroundImage)}
+          leftBlock={leftBlock(
+            windows,
+            leftBackground,
+            leftBackgroundMobile,
+            backgroundImage,
+            leftVideo
+          )}
           rightBlock={rightBlock(title, hintText, buttonText, backgroundImage)}
         />
       </WrapperCenter>
@@ -99,6 +118,7 @@ BurnYourPolymorphSection.propTypes = {
   title: PropTypes.string,
   hintText: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
   buttonText: PropTypes.string,
+  leftVideo: PropTypes.string,
   backgroundImage: PropTypes.node,
   leftBackground: PropTypes.node,
   leftBackgroundMobile: PropTypes.node,
@@ -111,6 +131,7 @@ BurnYourPolymorphSection.defaultProps = {
   backgroundImage: null,
   leftBackground: Section4LeftBackground,
   leftBackgroundMobile: mobilebackground,
+  leftVideo: leftSectionVideo,
 };
 
 export default BurnYourPolymorphSection;
