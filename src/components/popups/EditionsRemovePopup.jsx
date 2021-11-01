bbb953de9726c01657703c5ae80e5e1f619d86f7
Ivo <ivostoynovski@gmail.com>
2021-11-01 08:43:39 +0200

    feat: search component in editions remove popup

--- src/components/popups/EditionsRemovePopup.jsx
+++ src/components/popups/EditionsRemovePopup.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import closeIcon from '../../assets/images/cross.svg';
 import Button from '../button/Button.jsx';
 import EditionsSelectRemove from '../editionsSelectRemove/EditionsSelectRemove';
-import SearchField from '../input/SearchField';
+import SearchNFTsField from '../input/SearchNFTsField.jsx';
 
 const EditionsRemovePopup = ({ onClose, nft, onRemoveEdition }) => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const nftCopy = { ...nft };
+  const editions = [...nft.tokenIds];
+  let filteredEditions = [];
+  if (searchValue) {
+    filteredEditions = editions.filter((edition) =>
+      edition.toString().toLowerCase().includes(searchValue.toLocaleLowerCase())
+    );
+    nftCopy.tokenIds = filteredEditions;
+  }
+
   const handleRemoveEdition = (selectedNft) => onRemoveEdition(selectedNft);
   return (
     <div className="editionsRemove">
       <img className="close" src={closeIcon} alt="Close" onClick={onClose} aria-hidden="true" />
       <h3>Select editions to remove</h3>
-      {/* <SearchField
-        data={[nft]}
+      <SearchNFTsField
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
         placeholder="Search"
-        dropdown={false}
-        CardElement={<></>}
-        enterKeyEvent={false}
-      /> */}
-      <EditionsSelectRemove nft={nft} removeEdition={handleRemoveEdition} close={onClose} />
+      />
+      <EditionsSelectRemove nft={nftCopy} removeEdition={handleRemoveEdition} close={onClose} />
     </div>
   );
 };
