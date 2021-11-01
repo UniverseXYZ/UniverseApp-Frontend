eb26c85dbd9d1a5d6dc2828de003ca33e3ae7e7e
vikinatora <vikinatora@gmail.com>
2021-11-01 09:51:58 +0200

    feat(broken-nft): add pretty placeholder for broken nfts

---
+++ src/components/marketplaceNFT/BrokenNFT.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import cogWheel from '../../assets/images/cog.png';
+import './BrokenNFT.scss';
+
+const BrokenNFT = () => (
+  <div className="wrap">
+    <div id="broken-nft-container">
+      <div>
+        <img id="cog-img" src={cogWheel} alt="cogWheel" />
+      </div>
+      <div className="error-text">
+        <span>This NFT can&apos;t load yet</span>
+      </div>
+      <div className="error-text">
+        <span style={{ fontWeight: 400, fontSize: 12 }}>We&apos;re working on it</span>
+      </div>
+    </div>
+  </div>
+);
+
+export default BrokenNFT;
