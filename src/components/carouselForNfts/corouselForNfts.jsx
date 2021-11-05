5689224b883b0e104080029bf89cfab0dfab8d76
Ivo <ivostoynovski@gmail.com>
2021-11-05 16:16:15 +0200

    fix: infinite behaiviour depending on elements length

--- src/components/carouselForNfts/corouselForNfts.jsx
+++ src/components/carouselForNfts/corouselForNfts.jsx
@@ -13,47 +13,49 @@ function SampleArrow(props) {
   const { className, style, onClick } = props;
   return <div className={className} style={{ ...style }} onClick={onClick} />;
 }
-const settings = {
-  dots: true,
-  infinite: true,
-  variableWidth: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  nextArrow: <SampleArrow />,
-  prevArrow: <SampleArrow />,
+
+const CarouselForNfts = ({ winnersData, onRemoveEdition }) => {
+  const settings = {
+    dots: true,
+    infinite: winnersData.length && winnersData.length > 2,
+    variableWidth: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <SampleArrow />,
+    prevArrow: <SampleArrow />,
+  };
+  return (
+    <div className="carousel img-div">
+      <Slider {...settings}>
+        {winnersData.map((winner) => {
+          const result = winner.nftsData.reduce((res, curr) => {
+            if (!res[curr.name])
+              res[curr.name] = {
+                tokenIds: [],
+                url: curr.url,
+                artworkType: curr.artworkType,
+                count: 0,
+              };
+
+            res[curr.name].tokenIds.push(curr.tokenId);
+            res[curr.name].count += 1;
+            return res;
+          }, {});
+          return (
+            <div className="winner-block">
+              <div className="title-winner">Winner #{winner.slot + 1}</div>
+              {Object.values(result).length ? (
+                Object.values(result).map((nft) => (
+                  <WinnerNft key={uuid()} nft={nft} onRemoveEdition={onRemoveEdition} />
+                ))
+              ) : (
+                <div className="placeholder-winners" key={uuid()} />
+              )}
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
+  );
 };
-
-const CarouselForNfts = ({ winnersData, onRemoveEdition }) => (
-  <div className="carousel img-div">
-    <Slider {...settings}>
-      {winnersData.map((winner) => {
-        const result = winner.nftsData.reduce((res, curr) => {
-          if (!res[curr.name])
-            res[curr.name] = {
-              tokenIds: [],
-              url: curr.url,
-              artworkType: curr.artworkType,
-              count: 0,
-            };
-
-          res[curr.name].tokenIds.push(curr.tokenId);
-          res[curr.name].count += 1;
-          return res;
-        }, {});
-        return (
-          <div className="winner-block">
-            <div className="title-winner">Winner #{winner.slot + 1}</div>
-            {Object.values(result).length ? (
-              Object.values(result).map((nft) => (
-                <WinnerNft key={uuid()} nft={nft} onRemoveEdition={onRemoveEdition} />
-              ))
-            ) : (
-              <div className="placeholder-winners" key={uuid()} />
-            )}
-          </div>
-        );
-      })}
-    </Slider>
-  </div>
-);
 export default CarouselForNfts;
