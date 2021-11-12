fb8e30baa1dfef5f85f5d0ba53ecdf57815b9e5b
Ivo <ivostoynovski@gmail.com>
2021-11-12 13:59:00 +0200

    feat: auction house offset pagination

--- src/components/products/auctionHouse/tabs/Tabs.jsx
+++ src/components/products/auctionHouse/tabs/Tabs.jsx
@@ -3,20 +3,28 @@ import { withRouter } from 'react-router-dom';
 import './Tabs.scss';
 import ActiveAuctionsTab from './activeAuctions/ActiveAuctionsTab.jsx';
 import FutureAuctionsTab from './futureAuctions/FutureAuctionsTab.jsx';
-import { getActiveAuctions, getFutureAuctions } from '../../../../utils/api/auctions';
+import { getAllFutureAuctions, getAllActiveAuctions } from '../../../../utils/api/auctions';
 
 const Tabs = () => {
   const [auctions, setAuctions] = useState([]);
   const [selectedTabIndex, setSelectedTabIndex] = useState(0);
   const [loading, setLoading] = useState(false);
+  const [perPage, setPerPage] = useState(12);
+  const [pageCount, setPageCount] = useState(0);
 
-  const getAuctions = async (apiCall) => {
+  const getAuctions = async (request, offset) => {
     setLoading(true);
     try {
-      const response = await apiCall();
+      const response = await request(offset, perPage);
       if (response.auctions?.length) {
         setAuctions(response.auctions);
         setLoading(false);
+      }
+
+      if (response.pagination) {
+        const { total } = response.pagination;
+        const pages = Math.ceil(total / perPage);
+        setPageCount(pages);
       }
     } catch (error) {
       // TODO: handle errors
@@ -25,12 +33,24 @@ const Tabs = () => {
   };
 
   useEffect(async () => {
+    // window.scrollTo(0, 360);
     if (selectedTabIndex === 0) {
-      getAuctions(getActiveAuctions);
+      getAuctions(getAllActiveAuctions, 0, perPage);
     } else if (selectedTabIndex === 1) {
-      getAuctions(getFutureAuctions);
+      getAuctions(getAllFutureAuctions, 0, perPage);
     }
-  }, [selectedTabIndex]);
+  }, [selectedTabIndex, perPage]);
+
+  const handlePageClick = (item) => {
+    // let the user see the tabs after changing page
+    window.scrollTo(0, 360);
+    const offset = Math.ceil(item.selected * perPage);
+    if (selectedTabIndex === 0) {
+      getAuctions(getAllActiveAuctions, offset);
+    } else if (selectedTabIndex === 1) {
+      getAuctions(getAllFutureAuctions, offset);
+    }
+  };
 
   return (
     <div className="auction__house__tabs__section">
@@ -66,9 +86,23 @@ const Tabs = () => {
         </div>
         <div className="tab__content">
           {selectedTabIndex === 0 ? (
-            <ActiveAuctionsTab auctions={auctions} loading={loading} />
+            <ActiveAuctionsTab
+              auctions={auctions}
+              loading={loading}
+              handlePageClick={handlePageClick}
+              pageCount={pageCount}
+              perPage={perPage}
+              setPerPage={setPerPage}
+            />
           ) : (
-            <FutureAuctionsTab auctions={auctions} loading={loading} />
+            <FutureAuctionsTab
+              auctions={auctions}
+              loading={loading}
+              handlePageClick={handlePageClick}
+              pageCount={pageCount}
+              perPage={perPage}
+              setPerPage={setPerPage}
+            />
           )}
         </div>
       </div>
