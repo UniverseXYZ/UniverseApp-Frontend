8f46b90c5ab44edf442e937048f0b740d3b5b289
Ivo <ivostoynovski@gmail.com>
2021-11-30 13:41:50 +0200

    fix: flickering carousel

--- src/components/rewardTiers/WinnersList/WinnerBox/WinnerBox.jsx
+++ src/components/rewardTiers/WinnersList/WinnerBox/WinnerBox.jsx
@@ -4,33 +4,34 @@ import './WinnerBox.scss';
 import WinnerIcon from '../../../../assets/images/winner-icon.svg';
 
 const WinnerBox = ({
-  selectedWinnerIndex,
   selectedWinners,
-  data,
+  winnerNumber,
   tierId,
-  index,
+  nftsCount,
   setSelectedWinners,
+  activeClass,
+  index,
 }) => (
   <div
-    className={selectedWinnerIndex === index ? 'reward__winner-box selected' : 'reward__winner-box'}
+    className={activeClass ? 'reward__winner-box selected' : 'reward__winner-box'}
     aria-hidden="true"
-    key={index}
     onClick={() => setSelectedWinners({ ...selectedWinners, [tierId]: index })}
   >
     <img src={WinnerIcon} alt="winner-icon" />
-    <p>Winner #{data.slot + 1}</p>
-    <span>{data.nftsData?.length} NFTs</span>
+    <p>Winner #{winnerNumber}</p>
+    <span>{nftsCount} NFTs</span>
     <div className="box--shadow--effect--block" />
   </div>
 );
 
 WinnerBox.propTypes = {
-  selectedWinnerIndex: PropTypes.number.isRequired,
   selectedWinners: PropTypes.oneOfType([PropTypes.array]).isRequired,
-  data: PropTypes.oneOfType([PropTypes.object]).isRequired,
   tierId: PropTypes.string.isRequired,
-  index: PropTypes.number.isRequired,
   setSelectedWinners: PropTypes.func.isRequired,
+  winnerNumber: PropTypes.number.isRequired,
+  nftsCount: PropTypes.number.isRequired,
+  activeClass: PropTypes.bool.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default WinnerBox;
