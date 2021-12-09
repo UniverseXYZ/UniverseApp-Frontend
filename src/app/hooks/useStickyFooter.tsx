9762b34ad208359e79db58ff45aeb244358d106e
Eduard <edmoiseenkov@gmail.com>
2021-12-09 12:56:39 +0200

    improved useStickyFooter hook

---
+++ src/app/hooks/useStickyFooter.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { useWindowScroll, useWindowSize } from 'react-use';
+
+import { useLayout } from '../providers';
+
+export const useStickyFooter = (ref: React.RefObject<HTMLElement>) => {
+  const { y: scrollY } = useWindowScroll();
+  const { height } = useWindowSize(0, 0);
+  const { footerRef } = useLayout();
+
+  useEffect(() => {
+    if (ref.current) {
+      const isFixed = !(scrollY + height >= footerRef.current?.offsetTop ?? 0);
+      ref.current.style.bottom = isFixed ? '0' : '';
+      ref.current.style.position = isFixed ? 'fixed' : '';
+    }
+  }, [scrollY, height, footerRef.current, ref.current]);
+};
