f1186807d9e6a1e26470681631a7853f9c2fa4c3
Eduard <edmoiseenkov@gmail.com>
2021-12-10 15:56:26 +0200

    added sale type & nft type filters

---
+++ src/app/modules/marketplace/components/index.ts
@@ -0,0 +1,2 @@
+export * from './filters/nft-type-filter'
+export * from './filters/sale-type-filter'
--- src/app/modules/marketplace/pages/sell/components/tabs/settings-tab/SettingsTab.tsx
+++ src/app/modules/marketplace/pages/sell/components/tabs/settings-tab/SettingsTab.tsx
@@ -1,22 +1,8 @@
 import {
   Box,
   Button,
-  Fade,
-  Flex,
-  Heading,
-  Image,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  Link,
-  SimpleGrid,
-  Text, Tooltip,
-  useDisclosure,
 } from '@chakra-ui/react';
-import React, { useCallback, useMemo, useRef, useState } from 'react';
-
-import { Navigation, FreeMode } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
+import React from 'react';
 
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper.min.css';
@@ -26,25 +12,6 @@ import { useMarketplaceSellData } from '../../../hooks';
 import * as styles from './styles';
 import { SelectNFTs, SettingsTabDutchAuction, SettingsTabEnglishAuction, SettingsTabFixedListing } from './components';
 import { SellAmountType } from '../../../enums';
-import { Dropdown, InputShadow, Select } from '../../../../../../../components';
-
-import arrowLeftIcon from '../../../../../../../../assets/images/marketplace/bundles-left-arrow.svg';
-import arrowRightIcon from '../../../../../../../../assets/images/marketplace/bundles-right-arrow.svg';
-
-import searchIcon from '../../../../../../../../assets/images/search-gray.svg';
-import closeWhiteIcon from '../../../../../../../../assets/images/marketplace/v2/close-white.svg';
-import filtersIcon from '../../../../../../../../assets/images/marketplace/filters2.svg';
-import { SortNftsOptions } from '../../../../../constants';
-
-import saleTypeIcon from '../../../../../../../../assets/images/marketplace/sale-type.svg';
-import priceRangeIcon from '../../../../../../../../assets/images/marketplace/price-range.svg';
-import collectionsIcon from '../../../../../../../../assets/images/marketplace/collections.svg';
-import artistIcon from '../../../../../../../../assets/images/marketplace/artist.svg';
-import { Nfts } from '../../../../../mocks/nfts';
-import { NftItem } from '../../../../../../nft/components';
-import { INft } from '../../../../../../nft/types';
-import { SelectEditionsDropdown } from '../../select-editions-dropdown';
-import { useStickyFooter } from '../../../../../../../hooks';
 
 export const SettingsTab = () => {
   const sellData = useMarketplaceSellData();
