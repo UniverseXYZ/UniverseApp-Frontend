52397a662f78474546964b7a397d7caf1417a294
Eduard <edmoiseenkov@gmail.com>
2021-12-15 15:18:41 +0200

    removed commented code

--- src/app/hooks/useStickyHeader.tsx
+++ src/app/hooks/useStickyHeader.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useIsomorphicLayoutEffect, useWindowScroll, useWindowSize } from 'react-use';
+import { useWindowScroll, useWindowSize } from 'react-use';
 
 import { useLayout } from '../providers';
 
@@ -9,42 +9,6 @@ export const useStickyHeader = (ref: React.RefObject<HTMLElement>) => {
   const { headerRef } = useLayout();
 
   const [originalRefY, setOriginalRefY] = useState<number>(0);
-
-  // useEffect(() => {
-  //   if (ref.current) {
-  //     setOriginalRefY(ref.current.getBoundingClientRect().top + scrollY);
-  //   }
-  // }, [height]);
-  //
-  // useIsomorphicLayoutEffect(() => {
-  //   if (ref.current && headerRef.current) {
-  //     const headerHeight = headerRef.current.offsetHeight;
-  //
-  //     if (scrollY + headerHeight > originalRefY) {
-  //       headerRef.current.style.position = 'absolute';
-  //       headerRef.current.style.top = `${originalRefY - headerHeight}px`;
-  //     } else {
-  //       headerRef.current.style.position = 'fixed';
-  //       headerRef.current.style.top = `0`;
-  //     }
-  //
-  //     if (scrollY > originalRefY) {
-  //       if (ref.current.parentElement) {
-  //         ref.current.parentElement.style.paddingTop = `${ref.current.offsetHeight}px`;
-  //       }
-  //       ref.current.style.position = 'fixed';
-  //       ref.current.style.top = `0`;
-  //       ref.current.style.zIndex = `30`;
-  //     } else {
-  //       if (ref.current.parentElement) {
-  //         ref.current.parentElement.style.paddingTop = ``;
-  //       }
-  //       ref.current.style.position = '';
-  //       ref.current.style.top = ``;
-  //       ref.current.style.zIndex = ``;
-  //     }
-  //   }
-  // }, [originalRefY, scrollY, headerRef.current, ref.current]);
 
   useEffect(() => {
     if (ref.current) {
