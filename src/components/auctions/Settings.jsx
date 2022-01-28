f9ce0f7b0661bc09f83bef1ff1362e946d728ec4
vikinatora <vikinatora@gmail.com>
2022-01-28 09:28:37 +0200

    fix(setup): refactor + visual fixes + address validation fix

--- src/components/auctions/Settings.jsx
+++ src/components/auctions/Settings.jsx
@@ -57,7 +57,6 @@ const AuctionSettings = () => {
   const endDateRef = useRef(null);
   const [dropDown, setDropDown] = useState('');
   const { web3Provider } = useAuthContext();
-  console.log(auction);
   const [isValidFields, setIsValidFields] = useState({
     name: true,
     startDate: true,
@@ -284,19 +283,14 @@ const AuctionSettings = () => {
     !values.startDate ||
     !values.endDate ||
     !values.name ||
-    royaltyAddress.find(
-      (el, i) =>
-        el.address.trim().length === 0 ||
-        !utils.isAddress(el.address) ||
-        !+el.percentAmount ||
-        hasAddressError(el.address, i)
-    );
-  console.log(continueButtonDisabled);
-  console.log(
-    royaltyAddress.find(
-      (el) => el.address.trim().length === 0 || !utils.isAddress(el.address) || !+el.percentAmount
-    )
-  );
+    (royalities &&
+      royaltyAddress.find(
+        (el, i) =>
+          el.address.trim().length === 0 ||
+          !utils.isAddress(el.address) ||
+          !+el.percentAmount ||
+          hasAddressError(el.address, i)
+      ));
   return (
     <div className="auction-settings container">
       <div>
