fd94ef355e4be362bba8f4255b927ef12db18f54
IvanShterev <schterev11@gmail.com>
2022-01-28 09:29:59 +0200

    date format changed

--- src/components/calendar/utils.js
+++ src/components/calendar/utils.js
@@ -2,10 +2,10 @@ import { format } from 'date-fns';
 
 export const parseDateForDatePicker = (date) => ({
   month: format(date, 'LLL'),
-  day: format(date, 'dd'),
-  year: format(date, 'y'),
-  hours: format(date, 'hh'),
-  minutes: format(date, 'mm'),
+  day: Number(format(date, 'dd')),
+  year: Number(format(date, 'y')),
+  hours: Number(format(date, 'hh')),
+  minutes: Number(format(date, 'mm')),
   timezone: format(date, 'O'),
   format: format(date, 'aa'),
 });
