ac9a50b5eeff5d2d77e0f93c3b2c8809cdd9d433
vikinatora <vikinatora@gmail.com>
2022-01-28 09:31:44 +0200

    fix(past-auction-card): fix links and styling

--- src/components/auctionsCard/pastAuction/PastAuctionCard.jsx
+++ src/components/auctionsCard/pastAuction/PastAuctionCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './PastAuctionCard.scss';
 import { getPromoImageProps, bidsInUsd, createNftsPerWinnerMarkup } from '../utils';
 import { getBidTypeByName } from '../../../utils/fixtures/BidOptions';
@@ -22,32 +22,21 @@ const PastAuctionCard = ({ auction }) => {
   const promoImageProps = getPromoImageProps(auction.promoImageUrl, auction.user.profileImageUrl);
 
   return (
-    <div
-      aria-hidden
-      className="past__auction__item"
-      onClick={() => {
-        history.push(`/${auction.user.universePageAddress}/${auction.name}`);
-      }}
-    >
-      <div
+    <div className="past__auction__item">
+      <Link
         className={`past__auction__image timeLeft ${auction.promoImageUrl ? '' : 'show__avatar'}`}
+        to={`/${auction.user.universePageUrl}/${auction.link}`}
       >
         <img className={promoImageProps.class} src={promoImageProps.src} alt={auction.name} />
-      </div>
+      </Link>
       <div className="past__auction__details">
         <div className="title">
           <h2>{auction.name}</h2>
         </div>
-        <div className="creator">
+        <Link to={`/${auction.user.universePageUrl}`} className="creator">
           <img src={auction.user.profileImageUrl} alt={auction.user.displayName} />
-          <span>by</span>
-          <a
-            aria-hidden="true"
-            onClick={() => history.push(`/${auction.user.universePageAddress}`)}
-          >
-            {auction.user.displayName}
-          </a>
-        </div>
+          <span>by {auction.user.displayName}</span>
+        </Link>
         <div className="statistics">
           <div>
             <label>Winners</label>
