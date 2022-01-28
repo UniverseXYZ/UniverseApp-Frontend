f0395a3287ed7687f2df5fc824880f8dc6f06e82
Ivo <ivostoynovski@gmail.com>
2022-01-28 09:26:25 +0200

    feat: auction house offset pagination

--- src/components/products/auctionHouse/tabs/activeAuctions/ActiveAuctionsTab.jsx
+++ src/components/products/auctionHouse/tabs/activeAuctions/ActiveAuctionsTab.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import ReactPaginate from 'react-paginate';
 import ActiveAuctionsFilters from './Filters.jsx';
-import { isAfterNow } from '../../../../../utils/dates';
 import ActiveAuctionsList from '../../../../auctionsCard/activeAuction/ActiveAuctionsList.jsx';
 import ItemsPerPageDropdown from '../../../../pagination/ItemsPerPageDropdown.jsx';
-import Pagination from '../../../../pagination/Pagionation.jsx';
+import leftArrow from '../../../../../assets/images/left-arrow.svg';
+import rightArrow from '../../../../../assets/images/right-arrow.svg';
 
-const ActiveAuctionsTab = ({ auctions, loading }) => {
-  const [offset, setOffset] = useState(0);
-  const [perPage, setPerPage] = useState(12);
+const LeftArrow = () => <img src={leftArrow} alt="left arrow" />;
+const RightArrow = () => <img src={rightArrow} alt="right arrow" />;
+
+const ActiveAuctionsTab = ({
+  auctions,
+  loading,
+  handlePageClick,
+  pageCount,
+  perPage,
+  setPerPage,
+}) => {
+  console.info();
 
   return (
     <div className="active__auctions__tab">
@@ -17,7 +27,19 @@ const ActiveAuctionsTab = ({ auctions, loading }) => {
           <ActiveAuctionsFilters />
           <ActiveAuctionsList data={auctions} loading={loading} />
           <div className="pagination__container">
-            <Pagination data={auctions} perPage={perPage} setOffset={setOffset} />
+            <ReactPaginate
+              previousLabel={<LeftArrow />}
+              nextLabel={<RightArrow />}
+              breakLabel="..."
+              breakClassName="break-me"
+              pageCount={pageCount}
+              marginPagesDisplayed={1}
+              pageRangeDisplayed={3}
+              onPageChange={handlePageClick}
+              containerClassName="pagination"
+              subContainerClassName="pages pagination"
+              activeClassName="active"
+            />
             <ItemsPerPageDropdown
               perPage={perPage}
               setPerPage={setPerPage}
@@ -35,8 +57,12 @@ const ActiveAuctionsTab = ({ auctions, loading }) => {
 };
 
 ActiveAuctionsTab.propTypes = {
-  auctions: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  auctions: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   loading: PropTypes.bool.isRequired,
+  handlePageClick: PropTypes.func.isRequired,
+  pageCount: PropTypes.number.isRequired,
+  perPage: PropTypes.number.isRequired,
+  setPerPage: PropTypes.func.isRequired,
 };
 
 export default ActiveAuctionsTab;
--- src/components/products/auctionHouse/tabs/futureAuctions/FutureAuctionsTab.jsx
+++ src/components/products/auctionHouse/tabs/futureAuctions/FutureAuctionsTab.jsx
@@ -1,42 +1,60 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import ReactPaginate from 'react-paginate';
 import FutureAuctionsFilters from './Filters.jsx';
-import { isAfterNow } from '../../../../../utils/dates';
 import FutureAuctionsList from '../../../../auctionsCard/futureAuction/FutureAuctionsList.jsx';
-import Pagination from '../../../../pagination/Pagionation.jsx';
 import ItemsPerPageDropdown from '../../../../pagination/ItemsPerPageDropdown.jsx';
+import leftArrow from '../../../../../assets/images/left-arrow.svg';
+import rightArrow from '../../../../../assets/images/right-arrow.svg';
 
-const FutureAuctionsTab = ({ auctions, loading }) => {
-  const [offset, setOffset] = useState(0);
-  const [perPage, setPerPage] = useState(12);
+const LeftArrow = () => <img src={leftArrow} alt="left arrow" />;
+const RightArrow = () => <img src={rightArrow} alt="right arrow" />;
 
-  return (
-    <div className="future__auctions__tab">
-      {auctions.length ? (
-        <>
-          <FutureAuctionsFilters />
-          <FutureAuctionsList data={auctions} loading={loading} />
-          <div className="pagination__container">
-            <Pagination data={auctions} perPage={perPage} setOffset={setOffset} />
-            <ItemsPerPageDropdown
-              perPage={perPage}
-              setPerPage={setPerPage}
-              itemsPerPage={[12, 24]}
-            />
-          </div>
-        </>
-      ) : (
-        <div className="empty__nfts">
-          <h3>No future auctions found</h3>
+const FutureAuctionsTab = ({
+  auctions,
+  loading,
+  handlePageClick,
+  pageCount,
+  perPage,
+  setPerPage,
+}) => (
+  <div className="future__auctions__tab">
+    {auctions.length ? (
+      <>
+        <FutureAuctionsFilters />
+        <FutureAuctionsList data={auctions} loading={loading} />
+        <div className="pagination__container">
+          <ReactPaginate
+            previousLabel={<LeftArrow />}
+            nextLabel={<RightArrow />}
+            breakLabel="..."
+            breakClassName="break-me"
+            pageCount={pageCount}
+            marginPagesDisplayed={1}
+            pageRangeDisplayed={3}
+            onPageChange={handlePageClick}
+            containerClassName="pagination"
+            subContainerClassName="pages pagination"
+            activeClassName="active"
+          />
+          <ItemsPerPageDropdown perPage={perPage} setPerPage={setPerPage} itemsPerPage={[12, 24]} />
         </div>
-      )}
-    </div>
-  );
-};
+      </>
+    ) : (
+      <div className="empty__nfts">
+        <h3>No future auctions found</h3>
+      </div>
+    )}
+  </div>
+);
 
 FutureAuctionsTab.propTypes = {
-  auctions: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  auctions: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   loading: PropTypes.bool.isRequired,
+  handlePageClick: PropTypes.func.isRequired,
+  pageCount: PropTypes.number.isRequired,
+  perPage: PropTypes.number.isRequired,
+  setPerPage: PropTypes.func.isRequired,
 };
 
 export default FutureAuctionsTab;
