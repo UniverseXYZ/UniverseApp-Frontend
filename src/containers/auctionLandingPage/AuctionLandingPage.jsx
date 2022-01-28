58191a67d85e7b46f2e8996cd5bd485f3e9adf12
vikinatora <vikinatora@gmail.com>
2022-01-28 09:31:51 +0200

    fix(wrong useEffect props): socket listeners weren't being initialized

--- src/containers/auctionLandingPage/AuctionLandingPage.jsx
+++ src/containers/auctionLandingPage/AuctionLandingPage.jsx
@@ -209,7 +209,7 @@ const AuctionLandingPage = () => {
   };
 
   useEffect(() => {
-    if (auction?.id) {
+    if (auction?.auction?.id) {
       // Ready & Tested
       subscribeTo({
         auctionId: auction.auction.id,
@@ -244,7 +244,7 @@ const AuctionLandingPage = () => {
         cb: handeAuctionExtendedEvent,
       });
     }
-  }, [auction?.id]);
+  }, [auction?.auction?.id]);
 
   const mockAuctionPreviewData = (auctionData) => {
     const { name, avatar, universePageAddress } = loggedInArtist;
