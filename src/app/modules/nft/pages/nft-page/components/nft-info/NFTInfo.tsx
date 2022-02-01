bcaf5d49636f4d19e3fd767d52c7959249141f28
Eduard <edmoiseenkov@gmail.com>
2022-02-01 15:29:41 +0200

    added "more from the collection" section

--- src/app/modules/nft/pages/nft-page/components/nft-info/NFTInfo.tsx
+++ src/app/modules/nft/pages/nft-page/components/nft-info/NFTInfo.tsx
@@ -1,24 +1,28 @@
 import {
   Box,
+  Button,
+  Container,
   Flex,
   Heading,
   Link,
+  SimpleGrid,
   Tab,
   TabPanel,
   TabPanels,
   Tabs,
   Text,
 } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import ReactReadMoreReadLess from 'react-read-more-read-less';
 import { UseMeasureRect } from 'react-use/lib/useMeasure';
+import { useHistory } from 'react-router-dom';
 
 import { Bindings } from '../../mocks';
 import { LineTabList } from '../../../../../../components';
 import { NFTAssetImage, NFTAssetAudio, NFTBuySection, NFTAssetVideo } from '../';
 import { useNFTPageData } from '../../NFTPage.context';
 import { isNFTAssetAudio, isNFTAssetImage, isNFTAssetVideo } from '../../../../helpers';
-import { NFTMenu } from '../../../../components';
+import { NftItem, NFTMenu } from '../../../../components';
 import { TabBids, TabHistory, TabMetadata, TabOffers, TabOwners, TabProperties } from './components';
 import { CollectionPageLoader } from '../../../../../../../containers/collection/CollectionPageLoader';
 import NotFound from '../../../../../../../components/notFound/NotFound';
@@ -26,9 +30,17 @@ import * as styles from '../../styles';
 
 // TODO: hide metadata tab for not Polymorph NFT type
 export const NFTInfo = () => {
+  const router = useHistory();
+
   const { NFT, isLoading } = useNFTPageData();
 
   const [buySectionMeasure, setBuySectionMeasure] = useState<UseMeasureRect>();
+
+  const handleClickViewCollection = useCallback(() => {
+    if (NFT.moreFromCollection && NFT.moreFromCollection[0].collection) {
+      router.push(`/collection/${NFT.moreFromCollection[0].collection.address}`);
+    }
+  }, [NFT?.moreFromCollection]);
 
   return (
     <>
@@ -39,129 +51,153 @@ export const NFTInfo = () => {
           </div>
         )
       : NFT ? (
-          <Box>
-            <Box {...styles.NFTAssetContainerStyle}>
-              {isNFTAssetImage(NFT.artworkType) &&
-                <NFTAssetImage image={NFT.originalUrl} />
-              }
-              {isNFTAssetVideo(NFT.artworkType) &&
-                <NFTAssetVideo video={NFT.originalUrl} />
-              }
-              {isNFTAssetAudio(NFT.artworkType) &&
-                <NFTAssetAudio audio={NFT.originalUrl} />
-              }
-            </Box>
-            <Box {...styles.NFTDetailsContainerStyle}>
-              <Box sx={{ p: '60px 40px', minH: `calc(100vh - 80px - ${buySectionMeasure?.height}px)`, }}>
-                <Flex sx={{
-                  alignItems: 'center',
-                  mb: '12px',
-                  justifyContent: 'space-between'
-                }}>
-                  <Heading as={'h2'} sx={{ fontSize: '26px', }}>{NFT.name}</Heading>
-                  <Box>
-                  {/*  <NFTLike*/}
-                  {/*    likes={NFT.likes}*/}
-                  {/*    isLiked={false}*/}
-                  {/*    sx={{*/}
-                  {/*      padding: '15px 17px',*/}
-                  {/*      height: '42px',*/}
-                  {/*      borderRadius: '12px',*/}
-                  {/*      mr: '7px',*/}
-                  {/*    }}*/}
-                  {/*    onOpen={() => setIsLikesPopupOpened(true)}*/}
-                  {/*  />*/}
+          <>
+            <Box>
+              <Box {...styles.NFTAssetContainerStyle}>
+                {isNFTAssetImage(NFT.artworkType) &&
+                  <NFTAssetImage image={NFT.originalUrl} />
+                }
+                {isNFTAssetVideo(NFT.artworkType) &&
+                  <NFTAssetVideo video={NFT.originalUrl} />
+                }
+                {isNFTAssetAudio(NFT.artworkType) &&
+                  <NFTAssetAudio audio={NFT.originalUrl} />
+                }
+              </Box>
+              <Box {...styles.NFTDetailsContainerStyle}>
+                <Box sx={{ p: '60px 40px', minH: `calc(100vh - 80px - ${buySectionMeasure?.height}px)`, }}>
+                  <Flex sx={{
+                    alignItems: 'center',
+                    mb: '12px',
+                    justifyContent: 'space-between'
+                  }}>
+                    <Heading as={'h2'} sx={{ fontSize: '26px', }}>{NFT.name}</Heading>
+                    <Box>
+                      {/*  <NFTLike*/}
+                      {/*    likes={NFT.likes}*/}
+                      {/*    isLiked={false}*/}
+                      {/*    sx={{*/}
+                      {/*      padding: '15px 17px',*/}
+                      {/*      height: '42px',*/}
+                      {/*      borderRadius: '12px',*/}
+                      {/*      mr: '7px',*/}
+                      {/*    }}*/}
+                      {/*    onOpen={() => setIsLikesPopupOpened(true)}*/}
+                      {/*  />*/}
 
-                    <NFTMenu nft={NFT} />
-                  </Box>
-                </Flex>
+                      <NFTMenu nft={NFT} />
+                    </Box>
+                  </Flex>
 
-                <Text
-                  sx={{
-                    color: 'rgba(0, 0, 0, 0.6)',
-                    fontSize: '14px',
-                    fontWeight: 500,
-                    mb: '30px',
-                  }}
-                >
-                  Edition&nbsp;
-                  {`${NFT.tokenIds ? NFT.tokenIds.length : 1}/${NFT.numberOfEditions || (NFT.tokenIds || [NFT.tokenId]).length}`}
-                </Text>
+                  <Text
+                    sx={{
+                      color: 'rgba(0, 0, 0, 0.6)',
+                      fontSize: '14px',
+                      fontWeight: 500,
+                      mb: '30px',
+                    }}
+                  >
+                    Edition&nbsp;
+                    {`${NFT.tokenIds ? NFT.tokenIds.length : 1}/${NFT.numberOfEditions || (NFT.tokenIds || [NFT.tokenId]).length}`}
+                  </Text>
 
-                <Flex mb={'24px'}>
-                  {Bindings.map((binding, i) => (
-                    <Link to={binding.getLink(NFT)} sx={{
-                      _hover: {
-                        textDecoration: 'none !important',
-                        'div p:nth-of-type(2)': {
-                          textDecoration: 'underline',
-                        }
-                      },
-                      _notLast: {
-                        '> div > div': {
-                          _last: {
-                            pr: '20px'
+                  <Flex mb={'24px'}>
+                    {Bindings.map((binding, i) => (
+                      <Link to={binding.getLink(NFT)} sx={{
+                        _hover: {
+                          textDecoration: 'none !important',
+                          'div p:nth-of-type(2)': {
+                            textDecoration: 'underline',
                           }
                         },
-                      },
-                    }}>
-                      <Flex key={i} sx={{
-                        alignItems: 'center',
-                        flex: 1,
+                        _notLast: {
+                          '> div > div': {
+                            _last: {
+                              pr: '20px'
+                            }
+                          },
+                        },
                       }}>
-                        {binding.getImage(NFT)}
-                        <Box fontSize={'12px'} ml={'10px'} w={'110px'}>
-                          <Text color={'rgba(0, 0, 0, 0.4)'} fontWeight={500}>{binding.name}</Text>
-                          <Text isTruncated fontWeight={700}>{binding.getValue(NFT)}</Text>
-                        </Box>
-                      </Flex>
-                    </Link>
-                  ))}
-                </Flex>
+                        <Flex key={i} sx={{
+                          alignItems: 'center',
+                          flex: 1,
+                        }}>
+                          {binding.getImage(NFT)}
+                          <Box fontSize={'12px'} ml={'10px'} w={'110px'}>
+                            <Text color={'rgba(0, 0, 0, 0.4)'} fontWeight={500}>{binding.name}</Text>
+                            <Text isTruncated fontWeight={700}>{binding.getValue(NFT)}</Text>
+                          </Box>
+                        </Flex>
+                      </Link>
+                    ))}
+                  </Flex>
 
-                <Text sx={{
-                  color: 'rgba(0, 0, 0, 0.6)',
-                  fontSize: '14px',
-                  mb: '40px',
+                  <Text sx={{
+                    color: 'rgba(0, 0, 0, 0.6)',
+                    fontSize: '14px',
+                    mb: '40px',
 
-                  a: {
-                    color: 'black',
-                    fontWeight: 'bold',
-                    ml: '6px',
-                  },
-                }}>
-                  <ReactReadMoreReadLess
-                    charLimit={150}
-                    readMoreText="Read more"
-                    readLessText="Read less"
-                  >
-                    {NFT.description || ''}
-                  </ReactReadMoreReadLess>
-                </Text>
+                    a: {
+                      color: 'black',
+                      fontWeight: 'bold',
+                      ml: '6px',
+                    },
+                  }}>
+                    <ReactReadMoreReadLess
+                      charLimit={150}
+                      readMoreText="Read more"
+                      readLessText="Read less"
+                    >
+                      {NFT.description || ''}
+                    </ReactReadMoreReadLess>
+                  </Text>
 
-                <Tabs>
-                  <LineTabList>
-                    <Tab>Properties</Tab>
-                    <Tab>Metadata</Tab>
-                    <Tab>Owners</Tab>
-                    <Tab>Bids</Tab>
-                    <Tab>Offers</Tab>
-                    <Tab>History</Tab>
-                  </LineTabList>
+                  <Tabs>
+                    <LineTabList>
+                      <Tab>Properties</Tab>
+                      <Tab>Metadata</Tab>
+                      <Tab>Owners</Tab>
+                      <Tab>Bids</Tab>
+                      <Tab>Offers</Tab>
+                      <Tab>History</Tab>
+                    </LineTabList>
 
-                  <TabPanels sx={{ '> div' : { px: 0, pb: 0, pt: '30px', }}}>
-                    <TabPanel><TabProperties properties={NFT.properties || []} /></TabPanel>
-                    <TabPanel><TabMetadata /></TabPanel>
-                    <TabPanel><TabOwners /></TabPanel>
-                    <TabPanel><TabBids /></TabPanel>
-                    <TabPanel><TabOffers /></TabPanel>
-                    <TabPanel><TabHistory /></TabPanel>
-                  </TabPanels>
-                </Tabs>
+                    <TabPanels sx={{ '> div' : { px: 0, pb: 0, pt: '30px', }}}>
+                      <TabPanel><TabProperties properties={NFT.properties || []} /></TabPanel>
+                      <TabPanel><TabMetadata /></TabPanel>
+                      <TabPanel><TabOwners /></TabPanel>
+                      <TabPanel><TabBids /></TabPanel>
+                      <TabPanel><TabOffers /></TabPanel>
+                      <TabPanel><TabHistory /></TabPanel>
+                    </TabPanels>
+                  </Tabs>
+                </Box>
+                <NFTBuySection onMeasureChange={(measure) => setBuySectionMeasure(measure)} />
               </Box>
-              <NFTBuySection onMeasureChange={(measure) => setBuySectionMeasure(measure)} />
             </Box>
-          </Box>
+            {NFT.moreFromCollection && (
+              <Box {...styles.MoreNFTsWrapperStyle}>
+                <Heading {...styles.MoreNFTsTitleStyle}>More from this collection</Heading>
+                <Container
+                  {...styles.MoreNFTsContainerStyle}
+                  /*TODO: move 1110px to styles*/
+                  w={NFT.moreFromCollection.length < 4 ? `calc(1110px / 4 * ${NFT.moreFromCollection.length})` : '100%'}
+                >
+                  <SimpleGrid
+                    columns={{
+                      base: 1,
+                      md: 2,
+                      lg: NFT.moreFromCollection.length < 4 ? NFT.moreFromCollection.length : 4,
+                    }}
+                    spacingX={'20px'}
+                  >
+                    {NFT.moreFromCollection.map((NFT) => (<NftItem key={NFT.id} nft={NFT} />))}
+                  </SimpleGrid>
+                </Container>
+                <Button {...styles.MoreNFTsButtonStyle} onClick={handleClickViewCollection}>View collection</Button>
+              </Box>
+            )}
+          </>
         )
       : (<NotFound />)}
     </>
