735e57c9b83c314f6a8d74b16abb431993489be2
Eduard <edmoiseenkov@gmail.com>
2022-02-02 12:44:50 +0200

    improved like button

--- src/app/modules/nft/components/nft-item/components/nft-like/NFTLike.tsx
+++ src/app/modules/nft/components/nft-item/components/nft-like/NFTLike.tsx
@@ -9,66 +9,75 @@ import {
   PopoverArrow,
   PopoverBody,
   PopoverContent,
-  PopoverTrigger, Text,
+  PopoverTrigger,
+  Text,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 import heartFilled from '../../../../../../../assets/images/marketplace/heart-filled.svg';
 import heart from '../../../../../../../assets/images/marketplace/heart.svg';
 import heartHover from '../../../../../../../assets/images/marketplace/heart-hover.svg';
 
+import { NFTLikesPopup } from '../../../nft-likes-popup';
+import { NftLikes } from '../../../../../../mocks';
+
 interface INFTLikeProps extends ButtonProps {
   likes: string[];
   isLiked: boolean;
   onToggle?: (isLiked: boolean) => void;
-  onOpen?: () => void;
 }
 
-export const NFTLike = ({ isLiked, likes, onToggle, onOpen, sx = {}, ...rest }: INFTLikeProps) => {
+export const NFTLike = ({ isLiked, likes, onToggle, sx = {}, ...rest }: INFTLikeProps) => {
+
+  const [isLikesPopupOpened, setIsLikesPopupOpened] = useState(false);
+
   return (
-    <Popover trigger={'hover'} placement={'top'} variant={'tooltip'}>
-      <PopoverTrigger>
-        <Button
-          leftIcon={
-            <>
-              <Image src={isLiked ? heartFilled : heart} w={'14px'} display={'inline'} />
-              <Image src={isLiked ? heartFilled : heartHover} w={'14px'} display={'none'} />
-            </>
-          }
-          size={'sm'}
-          variant='simpleOutline'
-          sx={{
-            '--hover-color': '#FF4949',
-            color: isLiked ? 'var(--hover-color)' : 'rgba(0, 0, 0, 0.4)',
-            fontSize: '12px',
-            _hover: {
-              color: 'var(--hover-color)',
-              '.chakra-button__icon:first-of-type img': {
-                '&:nth-of-type(1)': { display: 'none', },
-                '&:nth-of-type(2)': { display: 'inline', }
-              }
-            },
-            ...sx,
-          }}
-          {...rest}
-          onClick={() => onToggle && onToggle(!isLiked)}
-        >
-          {likes.length}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent width={'fit-content'}>
-        <PopoverArrow />
-        <PopoverBody>
-          <Box sx={{ cursor: 'pointer' }} onClick={() => onOpen && onOpen()}>
-            <Text fontWeight={700} mb={'6px'}>{likes.length} people liked this</Text>
-            <AvatarGroup size='md' spacing={'-6px'}>
-              {likes.slice(0, 6).map((avatar, i) => (
-                <Avatar key={i} src={avatar} w={'26px'} h={'26px'} />
-              ))}
-            </AvatarGroup>
-          </Box>
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
+    <>
+      <Popover trigger={'hover'} placement={'top'} variant={'tooltip'}>
+        <PopoverTrigger>
+          <Button
+            leftIcon={
+              <>
+                <Image src={isLiked ? heartFilled : heart} w={'14px'} display={'inline'} />
+                <Image src={isLiked ? heartFilled : heartHover} w={'14px'} display={'none'} />
+              </>
+            }
+            size={'sm'}
+            variant='simpleOutline'
+            sx={{
+              '--hover-color': '#FF4949',
+              color: isLiked ? 'var(--hover-color)' : 'rgba(0, 0, 0, 0.4)',
+              fontSize: '12px',
+              _hover: {
+                color: 'var(--hover-color)',
+                '.chakra-button__icon:first-of-type img': {
+                  '&:nth-of-type(1)': { display: 'none', },
+                  '&:nth-of-type(2)': { display: 'inline', }
+                }
+              },
+              ...sx,
+            }}
+            {...rest}
+            onClick={() => onToggle && onToggle(!isLiked)}
+          >
+            {NftLikes.length}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent width={'fit-content'}>
+          <PopoverArrow />
+          <PopoverBody>
+            <Box sx={{ cursor: 'pointer' }} onClick={() => setIsLikesPopupOpened(true)}>
+              <Text fontWeight={700} mb={'6px'}>{NftLikes.length} people liked this</Text>
+              <AvatarGroup size='md' spacing={'-6px'}>
+                {NftLikes.slice(0, 6).map((like, i) => (
+                  <Avatar key={i} src={like.avatar} w={'26px'} h={'26px'} />
+                ))}
+              </AvatarGroup>
+            </Box>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+      <NFTLikesPopup isOpen={isLikesPopupOpened} onClose={() => setIsLikesPopupOpened(false)} />
+    </>
   );
 };
--- src/app/modules/nft/pages/nft-page/NFTPage.tsx
+++ src/app/modules/nft/pages/nft-page/NFTPage.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
-import { NFTLikesPopup } from '../../components';
 import { NFTInfo } from './components';
 
 import NFTPageProvider from './NFTPage.context'
@@ -10,16 +9,11 @@ import { useThemeContext } from '../../../../../contexts/ThemeContext';
 export const NFTPage = () => {
   const { setDarkMode } = useThemeContext() as any;
 
-  const [isLikesPopupOpened, setIsLikesPopupOpened] = useState(false);
-
   useEffect(() => setDarkMode(false), []);
 
   return (
     <NFTPageProvider>
       <NFTInfo />
-
-      {/*TODO: move to nft-like component*/}
-      <NFTLikesPopup isOpen={isLikesPopupOpened} onClose={() => setIsLikesPopupOpened(false)} />
     </NFTPageProvider>
   );
 };
