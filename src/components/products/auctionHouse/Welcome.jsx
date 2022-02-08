22344e4bf635834d9d51c1b97795c948232abd28
VahanMuradyan <10vahan.m16@gmail.com>
2022-02-08 17:19:02 +0200

    feat: updated hero section on auction house page

--- src/components/products/auctionHouse/Welcome.jsx
+++ src/components/products/auctionHouse/Welcome.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
 import './Welcome.scss';
+import { useHistory } from 'react-router-dom';
 import { AnimatedOnScroll } from 'react-animated-css-onscroll';
-import ellipses from '../../../assets/images/ellipses.svg';
+import Button from '../../button/Button';
+import { useAuctionContext } from '../../../contexts/AuctionContext';
+import { ReactComponent as PlusIcon } from '../../../assets/images/plus.svg';
 
-const Welcome = () => (
-  <div className="auction__house__welcome__section">
-    <img className="ellipse-l" src={ellipses} alt="Ellipses" />
-    <img className="ellipse-r" src={ellipses} alt="Ellipses" />
-    <div className="auction__house__welcome__section__container">
-      <AnimatedOnScroll animationIn="fadeIn" animationInDelay={200}>
-        <h1 className="title">Welcome to the Auction House</h1>
-      </AnimatedOnScroll>
-      <AnimatedOnScroll animationIn="fadeIn" animationInDelay={1000}>
-        <p className="desc">Check out creative releases from artists that have partnered with us</p>
-      </AnimatedOnScroll>
+const Welcome = () => {
+  const history = useHistory();
+  const { setAuction } = useAuctionContext();
+
+  return (
+    <div className="auction__house__welcome__section">
+      <div className="auction__house__welcome__section__container">
+        <AnimatedOnScroll animationIn="fadeIn" animationInDelay={200}>
+          <h2 className="subtitle">Welcome to the</h2>
+          <h1 className="title">Auction House</h1>
+        </AnimatedOnScroll>
+        <AnimatedOnScroll animationIn="fadeIn" animationInDelay={1000}>
+          <p className="desc">
+            Check out on creative releases from artists that partnered with us or set up your own
+            auction.
+          </p>
+          <div className="setup--auction--btn">
+            <Button
+              className="light-border-button"
+              onClick={() => {
+                setAuction({ rewardTiers: [] });
+                history.push('/setup-auction');
+              }}
+            >
+              Set up auction
+              <PlusIcon />
+            </Button>
+          </div>
+        </AnimatedOnScroll>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Welcome;
