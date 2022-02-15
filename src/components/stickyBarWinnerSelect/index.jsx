b4d38b1d0e51bec696fa75b136677e6557e1c617
IvanShterev <schterev11@gmail.com>
2022-02-15 09:57:00 +0200

    Added Select component render delay

--- src/components/stickyBarWinnerSelect/index.jsx
+++ src/components/stickyBarWinnerSelect/index.jsx
@@ -147,6 +147,7 @@ const styles = {
 
 const SelectComponent = (props) => {
   const { options, onChange, selectedWinner } = props;
+  const [render, setRender] = useState(false);
   const ref = useRef(null);
   const [isOpen, toggleIsOpen] = useState(false);
 
@@ -169,35 +170,46 @@ const SelectComponent = (props) => {
     };
   });
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setRender(true);
+    }, 100);
+
+    return () => clearTimeout(timeout);
+  }, [render]);
+
+  // react-select library issue -> need to delay the render in order to resolve some styling issues
   return (
-    <div
-      className="react-select-wrapper"
-      ref={ref}
-      aria-hidden
-      role="button"
-      onClick={handleToggle}
-    >
-      <Select
-        options={options}
-        isOptionDisabled={(option) => option.isDisabled}
-        onChange={onChange}
-        styles={styles}
-        components={{
-          Option,
-          ValueContainer,
-          IndicatorSeparator: () => null,
-        }}
-        menuPlacement="auto"
-        defaultValue={options[0]}
-        value={options[selectedWinner]}
-        closeMenuOnSelect
-        hideSelectedOptions
-        isClearable={false}
-        getOptionValue={(option) => option}
-        menuIsOpen={isOpen}
-      />
-      <div className="box--shadow--effect--block" />
-    </div>
+    render && (
+      <div
+        className="react-select-wrapper"
+        ref={ref}
+        aria-hidden
+        role="button"
+        onClick={handleToggle}
+      >
+        <Select
+          options={options}
+          isOptionDisabled={(option) => option.isDisabled}
+          onChange={onChange}
+          styles={styles}
+          components={{
+            Option,
+            ValueContainer,
+            IndicatorSeparator: () => null,
+          }}
+          menuPlacement="auto"
+          defaultValue={options[0]}
+          value={options[selectedWinner]}
+          closeMenuOnSelect
+          hideSelectedOptions
+          isClearable={false}
+          getOptionValue={(option) => option}
+          menuIsOpen={isOpen}
+        />
+        <div className="box--shadow--effect--block" />
+      </div>
+    )
   );
 };
 
