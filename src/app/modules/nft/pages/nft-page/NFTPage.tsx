366511b10a2217e608c4efb2290afce230a75509
Eduard <edmoiseenkov@gmail.com>
2022-02-17 09:10:19 +0200

    updated browse page styles

--- src/app/modules/nft/pages/nft-page/NFTPage.tsx
+++ src/app/modules/nft/pages/nft-page/NFTPage.tsx
@@ -5,7 +5,6 @@ import { NFTInfo } from './components';
 import NFTPageProvider from './NFTPage.context'
 import { useThemeContext } from '../../../../../contexts/ThemeContext';
 
-// TODO: hide metadata tab for not Polymorph NFT type
 export const NFTPage = () => {
   const { setDarkMode } = useThemeContext() as any;
 
