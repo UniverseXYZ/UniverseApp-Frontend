66db958e85235314bc94454117ba1e32fb93efb6
Eduard <edmoiseenkov@gmail.com>
2022-02-17 09:03:14 +0200

    improved nft-type-filter

--- src/app/modules/marketplace/components/filters/nft-type-filter/NFTTypeFilter.tsx
+++ src/app/modules/marketplace/components/filters/nft-type-filter/NFTTypeFilter.tsx
@@ -1,93 +1,86 @@
-import { Box, Checkbox, Image, SimpleGrid, Text } from '@chakra-ui/react';
-import { useFormik } from 'formik';
-import React  from 'react';
+import { Box, Image, SimpleGrid, Text } from '@chakra-ui/react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
-import { Dropdown, DropdownFilterContainer } from '../../../../../components';
+import { Checkbox, Dropdown, DropdownFilterContainer } from '../../../../../components';
+import { NftTypeFilterOptions } from './constants';
+import { INFTTypeFilterProps, INftTypeFilterValue } from './types';
 
 import nftTypeIcon from '../../../../../../assets/images/select-type-icon.svg';
 
-interface IFormValues {
-  singleItem: boolean;
-  bundle: boolean;
-  composition: boolean;
-  stillImage: boolean;
-  gif: boolean;
-  audio: boolean;
-  video: boolean;
-}
+export const NFTTypeFilter = ({ value: _value, onChange, onClear }: INFTTypeFilterProps) => {
+  const [isOpened, setIsOpened] = useState(false);
+  const [value, setValue] = useState<INftTypeFilterValue>({} as INftTypeFilterValue);
 
-interface INFTTypeFilterProps {
-  onChange: (values: IFormValues) => void;
-}
+  const handleSave = useCallback(() => {
+    onChange(value);
+    setIsOpened(false);
+  }, [value, onChange]);
 
-export const NFTTypeFilter = ({ onChange }: INFTTypeFilterProps) => {
-  const form = useFormik<IFormValues>({
-    initialValues: {
-      singleItem: false,
-      bundle: false,
-      composition: false,
-      stillImage: false,
-      gif: false,
-      audio: false,
-      video: false,
-    },
-    onSubmit: (values) => {
-      onChange(values);
-    },
-  });
+  const handleClear = useCallback(() => {
+    setValue(_value);
+    onClear();
+  }, [_value, onClear]);
+
+  const handleChangeOption = useCallback((optionName: keyof INftTypeFilterValue, optionValue: boolean) => {
+    setValue({ ...value, [optionName]: optionValue });
+  }, [value]);
+
+  const valueLabel = useMemo(() => {
+    const selectedOptions = NftTypeFilterOptions.reduce<Array<[number, number]>>((acc, optionGroup, i) => {
+      optionGroup.options.forEach((option, j) => {
+        if (_value[option.key]) {
+          acc.push([i, j]);
+        }
+      })
+      return acc;
+    }, []);
+
+    if (!selectedOptions.length) {
+      return null;
+    }
+
+    return `${NftTypeFilterOptions[selectedOptions[0][0]].options[selectedOptions[0][1]].label}${selectedOptions.length > 1 ? ` +${selectedOptions.length - 1}` : ''}`;
+  }, [_value]);
+
+  useEffect(() => setValue(_value), [_value]);
 
   return (
     <Dropdown
       label={'NFT type'}
+      value={valueLabel}
       buttonProps={{ leftIcon: <Image src={nftTypeIcon} /> }}
+      isOpened={isOpened}
+      onOpen={() => setIsOpened(true)}
+      onClose={() => {
+        setValue(_value);
+        setIsOpened(false);
+      }}
     >
       <DropdownFilterContainer
-        onSave={() => form.submitForm()}
-        onClear={() => form.resetForm()}
+        onSave={handleSave}
+        onClear={handleClear}
       >
-        <Text fontSize={'14px'} color={'rgba(0, 0, 0, 0.4)'} mb={'20px'}>NFT type</Text>
-        <SimpleGrid columns={2} spacingY={'20px'} mb={'40px'}>
-          <Box>
-            <Checkbox size={'lg'} name={'singleItem'} isChecked={form.values.singleItem} onChange={form.handleChange}>
-              <Text fontSize={'14px'} fontWeight={500}>Single item</Text>
-            </Checkbox>
+        {NftTypeFilterOptions.map((optionsGroup, i) => (
+          <Box key={i} sx={{
+            mb: '40px',
+            _last: {
+              mb: 0,
+            }
+          }}>
+            <Text color={'rgba(0, 0, 0, 0.4)'} fontSize={'14px'} fontWeight={500} mb={'20px'}>{optionsGroup.name}</Text>
+            <SimpleGrid columns={2} spacingY={'20px'}>
+              {optionsGroup.options.map(({ key, label }) => (
+                <Box key={key}>
+                  <Checkbox
+                    size={'lg'}
+                    isChecked={value[key]}
+                    onChange={(e) => handleChangeOption(key, e.target.checked)}
+                  >{label}</Checkbox>
+                </Box>
+              ))}
+            </SimpleGrid>
           </Box>
-          <Box>
-            <Checkbox size={'lg'} name={'bundle'} isChecked={form.values.bundle} onChange={form.handleChange}>
-              <Text fontSize={'14px'} fontWeight={500}>Bundle</Text>
-            </Checkbox>
-          </Box>
-          <Box>
-            <Checkbox size={'lg'} name={'composition'} isChecked={form.values.composition} onChange={form.handleChange}>
-              <Text fontSize={'14px'} fontWeight={500}>Composition</Text>
-            </Checkbox>
-          </Box>
-        </SimpleGrid>
-
-
-        <Text fontSize={'14px'} color={'rgba(0, 0, 0, 0.4)'} mb={'20px'}>File format</Text>
-        <SimpleGrid columns={2} spacingY={'20px'}>
-          <Box>
-            <Checkbox size={'lg'} name={'stillImage'} isChecked={form.values.stillImage} onChange={form.handleChange}>
-              <Text fontSize={'14px'} fontWeight={500}>Still image</Text>
-            </Checkbox>
-          </Box>
-          <Box>
-            <Checkbox size={'lg'} name={'gif'} isChecked={form.values.gif} onChange={form.handleChange}>
-              <Text fontSize={'14px'} fontWeight={500}>GIF</Text>
-            </Checkbox>
-          </Box>
-          <Box>
-            <Checkbox size={'lg'} name={'audio'} isChecked={form.values.audio} onChange={form.handleChange}>
-              <Text fontSize={'14px'} fontWeight={500}>Audio</Text>
-            </Checkbox>
-          </Box>
-          <Box>
-            <Checkbox size={'lg'} name={'video'} isChecked={form.values.video} onChange={form.handleChange}>
-              <Text fontSize={'14px'} fontWeight={500}>Video</Text>
-            </Checkbox>
-          </Box>
-        </SimpleGrid>
+        ))}
       </DropdownFilterContainer>
     </Dropdown>
   );
---
+++ src/app/modules/marketplace/components/filters/nft-type-filter/types/nft-type-filter-value.ts
@@ -0,0 +1,9 @@
+export interface INftTypeFilterValue {
+  singleItem: boolean;
+  bundle: boolean;
+  composition: boolean;
+  stillImage: boolean;
+  gif: boolean;
+  audio: boolean;
+  video: boolean;
+}
