f2f7b414e5fe54d9f64a46192edb50d9eef9eafe
Ivo <ivostoynovski@gmail.com>
2022-02-18 16:18:26 +0200

    scraper call change

--- src/utils/api/marketplace.ts
+++ src/utils/api/marketplace.ts
@@ -1,5 +1,36 @@
 import { utils } from 'ethers';
 
+const GET_TOKENS_PER_ADDRESS = (address: string, page: number, perPage: number) =>
+  `${process.env.REACT_APP_DATASCRAPER_BACKEND}/v1/users/${address}/tokens?page=${page}&size=${perPage}`;
+
+/**
+ *
+ * @param {string} address
+ * @param {int} page
+ * @param {int} perPage
+ * @returns {array} of NFT objects
+ * [
+ *  {_id,
+ *  contractAddress,
+ *  tokenId,
+ *  createdAt,
+ *  needToRefresh,
+ *  alternativeMediaFiles: [{type, url}], firstOwner, owners: [{address, transactionHash, value}],
+ *  tokenType,
+ *  updatedAt,
+ *  sentAt,
+ *  externalDomainViewUrl,
+ *  metadata: {description, name, image, attributes: [], external_url},
+ *  sentForMediaAt
+ *  ]
+ */
+
+export const getNftsPerAddress = async (address: string, page: number, perPage: number) => {
+  const request = await fetch(GET_TOKENS_PER_ADDRESS(address, page, perPage));
+  const result = await request.json();
+  return result;
+};
+
 export const sendReportRequest = async (values: any) => {
   const result = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/report`, {
     method: 'POST',
