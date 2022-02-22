3076c3e0914c4e79574c2347e250c23e7c154c6c
Ivo <ivostoynovski@gmail.com>
2022-02-22 09:25:23 +0200

    fix: remove TOS checkboxes

--- src/app/modules/nft/pages/nft-page/components/nft-checkout-popup/NFTCheckoutPopup.tsx
+++ src/app/modules/nft/pages/nft-page/components/nft-checkout-popup/NFTCheckoutPopup.tsx
@@ -160,11 +160,6 @@ export const NFTCheckoutPopup = ({ NFT, NFTs, order, isOpen, onClose }: INFTChec
                   </Text>
                 </Checkbox>
               )}
-              <Checkbox size={'lg'} isChecked={agree} onChange={(e) => setAgree(e.target.checked)}>
-                <Text fontSize={'12px'} fontWeight={400}>
-                  By checking this box, I agree to Universe’s <Link textDecor={'underline'} target={'_blank'} href={'https://github.com/UniverseXYZ/UniverseXYZ-Whitepaper'}>Terms of Service</Link>
-                </Text>
-              </Checkbox>
 
               <Box {...styles.ButtonsContainerStyle}>
                 <Button boxShadow={'lg'} onClick={handleCheckoutClick} disabled={!agree || !agreeBundle}>Checkout</Button>
