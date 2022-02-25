c416f670cc8b9fa4d80453fdcd8fecd5118eca26
VahanMuradyan <10vahan.m16@gmail.com>
2022-02-25 13:06:38 +0200

    fix: added background and engraving effect for titles

--- src/app/modules/nft/pages/nft-page/styles/nft-details-container.style.ts
+++ src/app/modules/nft/pages/nft-page/styles/nft-details-container.style.ts
@@ -1,6 +1,7 @@
 import { BoxProps } from '@chakra-ui/react';
 
 export const NFTDetailsContainerStyle: BoxProps = {
+  backgroundColor: {xl: 'rgba(255, 255, 255, 0.5)'},
   borderLeft: '1px solid #E4E4E4',
   float: {xl: 'right'},
   w: {xl: '550px'},
