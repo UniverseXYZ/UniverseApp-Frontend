5f47f8db2d852104a13a1e1922e2574c649ce3ed
IvanShterev <schterev11@gmail.com>
2022-03-07 16:19:11 +0200

    get collection nfts method refactoring

--- src/app/modules/nft/pages/collection-page/components/collection-info/CollectionInfo.tsx
+++ src/app/modules/nft/pages/collection-page/components/collection-info/CollectionInfo.tsx
@@ -16,7 +16,7 @@ import { CollectionPageLoader } from '../../../../../../../containers/collection
 import { useErrorContext } from '../../../../../../../contexts/ErrorContext';
 import { CollectionStatistics } from './components/index';
 import { useInfiniteQuery } from 'react-query';
-import { getCollectionNFTsApi } from '../../../../api/new-get-nft.api';
+import { GetCollectionNFTsApi } from '../../../../api/new-get-nft.api';
 import { utils } from "ethers"
 import NotFound from '../../../../../../../components/notFound/NotFound';
 import BGImage from '../../../../../../../assets/images/v2/stone_bg.jpg';
@@ -41,7 +41,7 @@ export const CollectionInfo = () => {
 
   const { data: NFTsPages, fetchNextPage, hasNextPage, isFetching, isLoading, isIdle } = useInfiniteQuery(
     ['user', collectionAddress, 'NFTs'],
-    ({ pageParam = 1 }) => getCollectionNFTsApi(utils.getAddress(collectionAddress), pageParam, PER_PAGE),
+    ({ pageParam = 1 }) => GetCollectionNFTsApi(utils.getAddress(collectionAddress), pageParam, PER_PAGE),
     {
       enabled: !!collectionAddress,
       retry: false,
