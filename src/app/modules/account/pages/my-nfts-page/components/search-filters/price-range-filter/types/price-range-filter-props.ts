4059cdd4a7d32da6ffadf0fbedb261c196763a2e
Eduard <edmoiseenkov@gmail.com>
2022-03-12 00:16:07 +0200

    added price-range-filter

--- src/app/modules/account/pages/my-nfts-page/components/search-filters/price-range-filter/types/price-range-filter-props.ts
+++ src/app/modules/account/pages/my-nfts-page/components/search-filters/price-range-filter/types/price-range-filter-props.ts
@@ -2,7 +2,5 @@ import { IPriceRangeFilterValue } from './price-range-filter-value';
 
 export interface IPriceRangeFilterProps {
   value: IPriceRangeFilterValue;
-  isDirty: boolean;
-  onChange: (values: IPriceRangeFilterValue) => void;
-  onClear: () => void;
+  onChange: (value: IPriceRangeFilterValue) => void;
 }
