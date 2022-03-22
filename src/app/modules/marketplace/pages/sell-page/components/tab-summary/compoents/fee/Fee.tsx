65b9a73760a0bc3db479ea448d052f90a4b0d9ca
taskudis <85156108+taskudis@users.noreply.github.com>
2022-03-22 14:17:44 +0200

    fix: always show % in fee summary

--- src/app/modules/marketplace/pages/sell-page/components/tab-summary/compoents/fee/Fee.tsx
+++ src/app/modules/marketplace/pages/sell-page/components/tab-summary/compoents/fee/Fee.tsx
@@ -12,6 +12,6 @@ export const Fee = ({ name, amount, total, ticker }: IFeeProps) => (
   <Flex py={'5px'}>
     <Box>{name}</Box>
     <Flex flex={1} borderBottom={'2px dotted rgba(0, 0, 0, 0.1)'} m={'5px'} />
-    <Box>{amount ? `${amount}% from ` : ''}{parseFloat(total?.toFixed(4) || '0')} {ticker} </Box>
+    <Box>{(amount || amount === 0) ? `${amount}% from ` : ''} {parseFloat(total?.toFixed(4) || '0')} {ticker} </Box>
   </Flex>
 )
