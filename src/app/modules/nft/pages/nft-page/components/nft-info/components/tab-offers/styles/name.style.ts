3aa9ac718980d22c02cff97c898be01c229e376d
taskudis <85156108+taskudis@users.noreply.github.com>
2022-03-23 13:17:11 +0200

    fix: nft offers tab,strip offerer name to elipses

--- src/app/modules/nft/pages/nft-page/components/nft-info/components/tab-offers/styles/name.style.ts
+++ src/app/modules/nft/pages/nft-page/components/nft-info/components/tab-offers/styles/name.style.ts
@@ -3,4 +3,12 @@ import { TextProps } from '@chakra-ui/react';
 export const NameStyle: TextProps = {
   fontSize: '14px',
   fontWeight: 700,
+  whiteSpace: "nowrap",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  maxW: {
+    sm: '70px',
+    md: '150px',
+    lg: '190px',
+  },
 };
