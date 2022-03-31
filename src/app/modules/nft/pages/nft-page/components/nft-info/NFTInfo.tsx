a6eb8a441af196cfed8c17979c4df3b6d5ca411f
Eduard <edmoiseenkov@gmail.com>
2022-03-31 12:15:06 +0300

    moved mime-type detection to OpenGraph component

--- src/app/modules/nft/pages/nft-page/components/nft-info/NFTInfo.tsx
+++ src/app/modules/nft/pages/nft-page/components/nft-info/NFTInfo.tsx
@@ -49,7 +49,6 @@ import { NFTAcceptOfferPopup } from './components/tab-offers/components';
 import { userKeys } from '../../../../../../utils/query-keys';
 import { TabListings } from './components/tab-listings';
 import { useErc20PriceStore } from '../../../../../../../stores/erc20PriceStore';
-import { lookup } from "mime-types";
 
 import * as styles from '../../styles';
 import * as styles2 from './NFTInfo.style';
@@ -182,7 +181,6 @@ export const NFTInfo = () => {
         description={NFT?.description || undefined}
         image={NFT.optimizedUrl || NFT.previewUrl || NFT.originalUrl || undefined}
         imageAlt={NFT?.name || ''}
-        mimeType={lookup(NFT.optimizedUrl || NFT.previewUrl || NFT.originalUrl)}
       />
       {isLoading ? (
         <div className="loader-wrapper">
