901f34c6464e0a685a87c2c3e9fe5bd4bd947510
Eduard <edmoiseenkov@gmail.com>
2022-04-14 09:32:49 +0300

    added polymorph page

--- src/components/popups/PolymorphMetadataLoading.jsx
+++ src/components/popups/PolymorphMetadataLoading.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../button/Button.jsx';
-import './PopupStyle.scss';
+// import './PopupStyle.scss';
 import closeIcon from '../../assets/images/cross.svg';
 
 const PolymorphMetadataLoading = ({ onClose }) => (
