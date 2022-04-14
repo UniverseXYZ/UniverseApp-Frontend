659316bb40a0ec4666282417b3ab9ab239bc1158
vikinatora <vikinatora@gmail.com>
2022-04-14 09:33:37 +0300

    feat(collection-page-ssr): add collection page SSR and OpenGraph integration

--- src/app/modules/nft/pages/collection-page/CollectionPage.tsx
+++ src/app/modules/nft/pages/collection-page/CollectionPage.tsx
@@ -1,23 +1,34 @@
 import React, { useEffect } from 'react';
-import { NextPageContext } from 'next';
 
 import { CollectionInfo } from './components';
 import CollectionPageProvider from './CollectionPage.context'
 import FiltersContextProvider from '../../../account/pages/my-nfts-page/components/search-filters/search-filters.context';
 import { useThemeContext } from '../../../../../contexts/ThemeContext';
+import { GetCollectionApi } from '../../api';
+import { GetServerSideProps } from 'next';
 
-export const CollectionPage = () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { collectionAddress } = context.params as { collectionAddress: string;  };
+
+  const collection = await GetCollectionApi(collectionAddress);
+
+  return {
+    props: {
+      collection: JSON.parse(JSON.stringify(collection)), // TRICK
+    }
+  };
+}
+
+export const CollectionPage = ({ collection }: any) => {
   const { setDarkMode } = useThemeContext() as any;
 
   useEffect(() => setDarkMode(false), []);
 
   return (
-    <CollectionPageProvider>
+    <CollectionPageProvider collection={collection}>
       <FiltersContextProvider defaultSorting={0}>
         <CollectionInfo />
       </FiltersContextProvider>
     </CollectionPageProvider>
   );
 };
-
-CollectionPage.getInitialProps = (ctx: NextPageContext) => ({});
