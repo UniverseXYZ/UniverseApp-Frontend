322d44e3fee1ec7682bddefe7df0d91951716be8
Eduard <edmoiseenkov@gmail.com>
2022-04-14 09:32:49 +0300

    added collection page

--- src/containers/collection/Collection.jsx
+++ src/containers/collection/Collection.jsx
@@ -1,118 +1,91 @@
-import React, { useContext, useEffect, useState, useRef } from 'react';
-import { Contract } from 'ethers';
-import { useHistory, useParams, useLocation } from 'react-router-dom';
-import Contracts from '../../contracts/contracts.json';
+import React, { useContext, useEffect, useState, useRef, useMemo, useCallback } from 'react';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
 import NotFound from '../../components/notFound/NotFound.jsx';
-import './Collection.scss';
+// import './Collection.scss';
 import Cover from '../../components/collection/Cover.jsx';
 import Avatar from '../../components/collection/Avatar.jsx';
+import Title from '../../components/collection/Title.jsx';
 import Description from '../../components/collection/Description.jsx';
 import Button from '../../components/button/Button.jsx';
+import pencilIcon from '../../assets/images/edit.svg';
 import NFTCard from '../../components/nft/NFTCard.jsx';
+import bubbleIcon from '../../assets/images/text-bubble.png';
+import { getCollectionData } from '../../utils/api/mintNFT';
 import { useThemeContext } from '../../contexts/ThemeContext.jsx';
+import { useMyNftsContext } from '../../contexts/MyNFTsContext.jsx';
 import { useAuthContext } from '../../contexts/AuthContext.jsx';
-import { getCollectionData } from '../../utils/api/marketplace.ts';
-import '../../components/pagination/Pagination.scss';
+import ItemsPerPageDropdown from '../../components/pagination/ItemsPerPageDropdown.jsx';
+// import '../../components/pagination/Pagination.scss';
 import ApiPagination from '../../components/pagination/ApiPagination.jsx';
 import ApiCollectionSearchFilters from '../../components/nft/ApiCollectionSearchFilters.jsx';
 import { useSearchCollection } from '../../utils/hooks/useCollectionPageDebouncer.js';
+import RarityChartsLoader from '../rarityCharts/RarityChartsLoader.jsx';
+import { renderLoaders } from '../rarityCharts/renderLoaders.js';
 import { CollectionPageLoader } from './CollectionPageLoader.jsx';
-import ApiItemsPerPageDropdown from '../../components/pagination/ApiItemsPerPageDropdown.jsx';
-import SocialLinks from '../../components/collection/SocialLinks.jsx';
-import EditIcon from '../../components/svgs/EditIcon.jsx';
-import Statistics from '../../components/collection/Statistics.jsx';
-import Tabs from '../../components/tabs/Tabs.jsx';
-import EmptyData from '../../components/collection/EmptyData.jsx';
-import LoadMore from '../../components/pagination/LoadMore';
+import { useRouter } from 'next/router';
 
-const Collection = () => {
-  const tabs = ['Items', 'Description'];
-  const [selectedTabIndex, setSelectedTabIndex] = useState(0);
-  const { address, signer } = useAuthContext();
+const Collection = (props) => {
+  const { setSavedCollectionID } = useMyNftsContext();
+  const { address } = useAuthContext();
   const { setDarkMode } = useThemeContext();
-  const { collectionAddress } = useParams();
-  const history = useHistory();
+  const router = useRouter();
+  const { collectionAddress } = router.query;
   const ref = useRef(null);
 
-  const location = useLocation();
+  const location = useMemo(() => {
+    const search = window.location?.search?.substr(1);
 
-  // const {
-  //   inputText,
-  //   setInputText,
-  //   apiPage,
-  //   setApiPage,
-  //   search: { loading },
-  //   results,
-  //   notFound,
-  //   isLastPage,
-  //   setIsLastPage,
-  //   loadedPages,
-  //   setLoadedPages,
-  // } = useSearchCollection(collectionAddress);
+    let state = search ? JSON.parse(
+      `{"${search.replace(/&/g, '","').replace(/=/g, '":"')}"}`,
+      (key, value) => (key === '' ? value : decodeURIComponent(value))
+    ) : {};
+    return { state };
+  }, []);
+
+  const {
+    inputText,
+    setInputText,
+    apiPage,
+    setApiPage,
+    search: { loading },
+    results,
+    isLastPage,
+    setIsLastPage,
+    loadedPages,
+    setLoadedPages,
+  } = useSearchCollection(collectionAddress);
 
   const [collectionData, setCollectionData] = useState(null);
-  const [collectionNfts, setCollectionNfts] = useState(null);
-  const [page, setPage] = useState(1);
-  const [notFound, setNotFound] = useState(false);
+  const [perPage, setPerPage] = useState(8);
+  const [page, setPage] = useState(0);
+  const [offset, setOffset] = useState(0);
   const [isSearching, setIsSearching] = useState(false);
-  const [showLoadMore, setShowLoadMore] = useState(false);
 
   const [isDropdownOpened, setIsDropdownOpened] = useState(false);
   const [ownersCount, setOwnersCount] = useState(0);
-  const [collectionOwner, setCollectionOwner] = useState('');
 
-  const nftsContainerRef = useRef(null);
+  useEffect(() => setDarkMode(false), []);
 
-  const getCollection = async () => {
-    if (collectionAddress) {
-      try {
-        const response = await getCollectionData(collectionAddress, page);
-        if (response.error) {
-          setNotFound(true);
-          setShowLoadMore(false);
-          return;
-        }
-        if (collectionData) {
-          setCollectionNfts({
-            ...collectionNfts,
-            data: [...collectionNfts.data, ...response.nfts.data],
-          });
-        } else {
-          setCollectionData(response);
-          setCollectionNfts(response.nfts);
-        }
-        setPage(page + 1);
-        setShowLoadMore(true);
-        setIsSearching(false);
-      } catch (error) {
-        console.error(error);
-      }
+  useEffect(() => {
+    if (results.collection) {
+      setCollectionData(results);
+      setIsSearching(false);
     }
-  };
+  }, [results]);
 
-  useEffect(async () => {
-    setDarkMode(false);
+  const handleEdit = useCallback((id) => {
+    setSavedCollectionID(id);
+    router.push('/my-nfts/create?tabIndex=1&nftType=collection');
   }, []);
 
-  useEffect(async () => {
-    getCollection();
-  }, [collectionAddress]);
-
-  const handleEdit = (id) => {
-    history.push('/my-nfts/create', {
-      tabIndex: 1,
-      nftType: 'collection',
-      collection: collectionData?.collection,
-    });
+  const resetPagination = () => {
+    setOffset(0);
+    setPage(0);
+    setApiPage(0);
+    setIsSearching(true);
   };
 
-  const scrollToNftContainer = () => {
-    if (nftsContainerRef && nftsContainerRef.current) {
-      nftsContainerRef.current.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-    }
-  };
-
-  return !collectionData && !notFound ? (
+  return !collectionData ? (
     <div className="loader-wrapper">
       <CollectionPageLoader />
     </div>
@@ -121,101 +94,103 @@ const Collection = () => {
       <Cover selectedCollection={collectionData.collection} />
 
       <div className="collection__details__section">
-        <div className="collection__details__header">
-          <div className="collection__details__header__tp">
-            <div className="collection__details__header__tp__left">
-              <Avatar selectedCollection={collectionData.collection} />
-              <SocialLinks
-                instagramLink={collectionData.collection.instagramLink || ''}
-                siteLink={collectionData.collection.siteLink || ''}
-                mediumLink={collectionData.collection.mediumLink || ''}
-                discordLink={collectionData.collection.discordLink || ''}
-                telegramLink={collectionData.collection.telegramLink || ''}
-                twitterLink=""
-              />
-            </div>
-            <div className="collection__details__header__tp__right">
-              {address.toLowerCase() === collectionOwner ? ( //
-                <div className="collection__vote__edit">
-                  {/* <div>
-                    <Button className="light-button">
-                      <span>Vote</span>
-                    </Button>
-                  </div> */}
-                  <div>
-                    <Button
-                      className="light-button"
-                      onClick={() => handleEdit(collectionData.collection.id)}
-                    >
-                      <span>Edit</span>
-                      <EditIcon />
-                    </Button>
-                  </div>
-                </div>
-              ) : (
-                <></>
-              )}
-            </div>
-          </div>
-          <div className="collection__details__header__bp">
-            <Statistics nftsCount={collectionNfts?.total} ownersCount={ownersCount} />
-          </div>
-        </div>
-        <div className="collection--nfts--container">
-          <Tabs
-            items={tabs.map((tab, index) => ({
-              name: tab,
-              active: selectedTabIndex === index,
-              handler: setSelectedTabIndex.bind(this, index),
-            }))}
+        <div className="collection__details__container">
+          <Avatar selectedCollection={collectionData.collection} />
+          <Title
+            selectedCollection={collectionData.collection}
+            saved={location.state?.saved}
+            nftsCount={collectionData?.pagination?.totalCount}
+            ownersCount={ownersCount}
           />
-          {selectedTabIndex === 0 ? (
+        </div>
+        <Description selectedCollection={collectionData.collection} />
+        {address === collectionData?.collection?.owner ? (
+          <div className="collection__edit">
+            <Button
+              className="light-border-button"
+              onClick={() => handleEdit(collectionData.collection.id)}
+            >
+              <span>Edit</span>
+              <img src={pencilIcon} alt="Edit Icon" />
+            </Button>
+          </div>
+        ) : (
+          <></>
+        )}
+        <div className="collection--nfts--container">
+          <ApiCollectionSearchFilters
+            searchText={inputText}
+            search={setInputText}
+            resetPagination={resetPagination}
+          />
+
+          {isSearching ? (
+            <CollectionPageLoader />
+          ) : collectionData?.nfts?.filter((nft) => !nft.hidden).length ? (
             <>
-              {/* <ApiCollectionSearchFilters
-                searchText={inputText}
-                search={setInputText}
-                resetPagination={resetPagination}
-              /> */}
-              {isSearching ? (
-                <CollectionPageLoader />
-              ) : collectionNfts?.data.filter((nft) => !nft.hidden).length ? (
-                <>
-                  <div className="nfts__lists" ref={nftsContainerRef}>
-                    {collectionNfts?.data
-                      // .slice(offset, offset + perPage)
-                      .filter((nft) => !nft.hidden)
-                      .map((nft) => {
-                        if (nft.metadata) {
-                          nft.metadata.image = nft.metadata?.image_thumbnail_url;
-                        }
-                        return (
-                          <NFTCard
-                            key={nft.id}
-                            nft={{
-                              ...nft,
-                              collection: collectionData?.collection,
-                            }}
-                            collectionAddress={collectionAddress}
-                          />
-                        );
-                      })}
-                  </div>
-                  {showLoadMore ? <LoadMore loadMore={getCollection} pageAndSize /> : null}
-                </>
-              ) : (
-                <EmptyData text="No items found" />
-              )}
-            </>
-          ) : selectedTabIndex === 1 ? (
-            <>
-              {collectionData.collection.description ? (
-                <Description selectedCollection={collectionData.collection} />
-              ) : (
-                <EmptyData text="This collection doesn’t have a description yet" />
-              )}
+              <div className="nfts__lists">
+                {collectionData?.nfts
+                  .slice(offset, offset + perPage)
+                  .filter((nft) => !nft.hidden)
+                  .map((nft) => (
+                    <NFTCard
+                      key={nft.id}
+                      nft={{
+                        ...nft,
+                        collection: collectionData.collection,
+                      }}
+                      collectionAddress={collectionAddress}
+                    />
+                  ))}
+              </div>
+
+              {isLastPage ? <CollectionPageLoader /> : <></>}
+
+              <div className="pagination__container">
+                <ApiPagination
+                  data={collectionData?.nfts}
+                  perPage={perPage}
+                  setOffset={setOffset}
+                  setApiPage={setApiPage}
+                  apiPage={apiPage}
+                  setIsLastPage={setIsLastPage}
+                  page={page}
+                  setPage={setPage}
+                  loadedPages={loadedPages}
+                  setLoadedPages={setLoadedPages}
+                  pagination={collectionData.pagination}
+                />
+                <ItemsPerPageDropdown
+                  perPage={perPage}
+                  setPerPage={setPerPage}
+                  itemsPerPage={[8, 16, 32]}
+                  offset={offset}
+                  page={page}
+                  setPage={setPage}
+                />
+              </div>
             </>
           ) : (
-            <></>
+            <div className="empty__nfts">
+              <div className="tabs-empty">
+                <div className="image-bubble">
+                  <img src={bubbleIcon} alt="bubble-icon" />
+                </div>
+                <h3>No NFTs found</h3>
+                <p>Create NFTs or NFT collections with our platform by clicking the button below</p>
+                <Button
+                  ref={ref}
+                  className={`create--nft--dropdown  ${
+                    isDropdownOpened ? 'opened' : ''
+                  } light-button`}
+                  /*TODO: pass collection: collectionData.collection*/
+                  onClick={() => router.push('/my-nfts/create?tabIndex=1&nftType=single')}
+                  aria-hidden="true"
+                >
+                  Create NFT
+                </Button>
+              </div>
+            </div>
           )}
         </div>
       </div>
