e5802101e19af0554da6ad5963b9123e6d1a1a36
Eduard <edmoiseenkov@gmail.com>
2022-04-15 11:39:58 +0300

    added error message to report popup

---
+++ src/app/modules/nft/components/nft-report-popup/NFTReportPopup.styles.tsx
@@ -0,0 +1,19 @@
+import { BoxProps, ModalHeaderProps } from '@chakra-ui/react';
+
+export const ErrorStyle: BoxProps = {
+  background: 'rgba(255, 73, 73, 0.04)',
+  border: '1px solid rgba(255, 73, 73, 0.4)',
+  borderRadius: '12px',
+  color: '#FF4949',
+  fontWeight: 600,
+  fontSize: '14px',
+  lineHeight: '24px',
+  mb: '30px',
+  padding: '16px',
+};
+
+export const ModalHeaderStyle: ModalHeaderProps = {
+  pt: '40px !important',
+  pb: '20px !important',
+  textAlign: 'center',
+};
