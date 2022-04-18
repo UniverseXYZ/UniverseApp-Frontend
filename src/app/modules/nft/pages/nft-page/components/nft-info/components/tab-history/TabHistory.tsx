bd3d77831aa0355a06fa845ce6dee0e044da9cda
Eduard <edmoiseenkov@gmail.com>
2022-04-18 16:30:15 +0300

    added info alert

--- src/app/modules/nft/pages/nft-page/components/nft-info/components/tab-history/TabHistory.tsx
+++ src/app/modules/nft/pages/nft-page/components/nft-info/components/tab-history/TabHistory.tsx
@@ -1,16 +1,33 @@
-import { Box } from '@chakra-ui/react';
-import { IOrder } from '../../../../../../types';
-import { INFTHistory } from '../../../../../../api';
+import { Box, Icon } from '@chakra-ui/react';
+
+import { ReactComponent as InfoSVG } from '@assets/images/info.svg';
+import { IOrder } from '@app/modules/nft/types';
+import { INFTHistory } from '@app/modules/nft/api';
+
 import HistoryEvent from '../shared/history-listings-event/HistoryEvent';
+import * as styles from './styles';
 
 interface ITabHistoryProps {
   historyData?: INFTHistory;
 }
 
-export const TabHistory = ({ historyData = { orderHistory: [], mintEvent: null as any } }: ITabHistoryProps) => {
+export const TabHistory = (props: ITabHistoryProps) => {
+  const {
+    historyData = {
+      orderHistory: [],
+      mintEvent: null as any
+    }
+  } = props;
+
   const events = [...historyData?.orderHistory, historyData?.mintEvent];
   return (
     <Box>
+      <Box {...styles.AlertStyle}>
+        <Icon viewBox='0 0 25 25' color='#4D66EB' boxSize={'22px'} mr={'10px'} mt={0}>
+          <InfoSVG />
+        </Icon>
+        Currently we show just the Universe history but we working on implementing the general NFT history.
+      </Box>
       {events.map(
         (event: IOrder | any, i: number) => event && <HistoryEvent key={i} event={event} />
       )}
--- src/app/modules/nft/pages/nft-page/components/nft-info/components/tab-history/styles/index.ts
+++ src/app/modules/nft/pages/nft-page/components/nft-info/components/tab-history/styles/index.ts
@@ -1,6 +1,7 @@
 export * from './action-icon.style';
 export * from './action-label.style';
 export * from './added-label.style';
+export * from './alert.style';
 export * from './etherscan-icon.style';
 export * from './etherscan-tooltip.style';
 export * from './expired.style';
