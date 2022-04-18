4609eb577c6e09fc54c1858875e4885542959e97
Eduard <edmoiseenkov@gmail.com>
2022-04-18 17:09:55 +0300

    added missed files

---
+++ src/app/modules/nft/components/nft-item/components/nft-item-asset/hooks/useNFTAsset.ts
@@ -0,0 +1,47 @@
+import { useMemo } from 'react';
+
+import { INFT } from '@app/modules/nft/types';
+
+import { getNFTPreviewImage } from '../helpers';
+
+interface IUseAssetResult {
+  asset: {
+    url: string;
+    type: 'image' | 'video' | 'audio';
+  };
+  preview?: string;
+}
+
+export const useNFTAsset = (NFT: INFT) => {
+  return useMemo<IUseAssetResult>(() => {
+    const previewUrl = getNFTPreviewImage(NFT);
+
+    let asset: IUseAssetResult['asset'];
+
+    // CHECK IS VIDEO
+    if (NFT.videoUrl) {
+      asset = {
+        url: NFT.videoUrl,
+        type: 'video',
+      }
+    } else if ((NFT as any).audioUrl) {
+      asset = {
+        url: (NFT as any).audioUrl,
+        type: 'audio',
+      }
+    } else {
+      asset = {
+        url: [
+          NFT.gifUrl,
+          NFT.optimizedUrl,
+          NFT.originalUrl,
+          NFT.previewUrl,
+          NFT.thumbnailUrl,
+        ].find(url => !!url) ?? '',
+        type: 'image',
+      };
+    }
+
+    return { asset, preview: previewUrl };
+  }, [NFT]);
+}
---
+++ src/app/modules/nft/pages/nft-page/components/nft-asset-image/NFTAssetImage.styles.tsx
@@ -0,0 +1,10 @@
+import { BoxProps } from '@chakra-ui/react';
+import React  from 'react';
+
+export const ImageWrapperStyle: BoxProps = {
+  pos: 'relative',
+  maxH: {sm: '335px', md: '600px'},
+  maxW: {sm: '335px', md: '600px'},
+  height: 'calc(100vh - 84px - 120px)',
+  width: 'calc(100vh - 84px - 120px)',
+};
