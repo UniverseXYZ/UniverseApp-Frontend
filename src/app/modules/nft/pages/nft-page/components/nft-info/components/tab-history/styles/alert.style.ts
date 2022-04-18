5255c2259b4ff33c234411125a29ee0b1c41f560
Eduard <edmoiseenkov@gmail.com>
2022-04-18 17:09:57 +0300

    added info alert

---
+++ src/app/modules/nft/pages/nft-page/components/nft-info/components/tab-history/styles/alert.style.ts
@@ -0,0 +1,14 @@
+import { BoxProps } from '@chakra-ui/react';
+
+export const AlertStyle: BoxProps = {
+  background: 'rgba(79, 106, 229, 0.04)',
+  border: '1px solid rgba(79, 106, 229, 0.4)',
+  borderRadius: '12px',
+  color: '#4D66EB',
+  display: 'flex',
+  fontWeight: 600,
+  fontSize: '14px',
+  lineHeight: '24px',
+  padding: '16px',
+  mb: '30px',
+};
