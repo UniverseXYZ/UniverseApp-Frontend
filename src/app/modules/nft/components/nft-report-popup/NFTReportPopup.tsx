cabaa00f3e4ab663a6d612b5a920e640fdcff9d0
Eduard <edmoiseenkov@gmail.com>
2022-04-18 17:09:56 +0300

    added error message to report popup

--- src/app/modules/nft/components/nft-report-popup/NFTReportPopup.tsx
+++ src/app/modules/nft/components/nft-report-popup/NFTReportPopup.tsx
@@ -10,17 +10,22 @@ import {
   ModalHeader,
   ModalOverlay,
   Text,
-  Button
+  Button,
+  Box,
+  Icon,
 } from '@chakra-ui/react';
 import { useFormik } from 'formik';
 import ReCAPTCHA from 'react-google-recaptcha';
-
 import { useEffect, useState } from 'react';
+
+import { ReactComponent as WarningSVG } from '@assets/images/warning.svg';
+
 import { Select } from '../../../../components';
 import { ReportPopupValidationSchema } from './constants';
 import { sendReportRequest } from '../../../../../utils/api/marketplace';
 import { ReportStatusPopup, Status as ReportState } from '../../pages/nft-page/components/nft-info/components/tab-offers/components/report-status-popup';
 import { useAuthStore } from '../../../../../stores/authStore';
+import * as styles from './NFTReportPopup.styles';
 
 interface INFTReportPopupProps {
   isOpen: boolean;
@@ -32,7 +37,10 @@ interface INFTReportPopupProps {
 export const NFTReportPopup = ({ isOpen, onClose, collectionAddress, tokenId }: INFTReportPopupProps) => {
   const [reportStatus, setReportStatus] = useState(ReportState.HIDDEN);
 
-  const { isAuthenticated, loginFn } = useAuthStore(s => ({isAuthenticated: s.isAuthenticated, loginFn: s.loginFn}))
+  const { isAuthenticated, loginFn } = useAuthStore(s => ({
+    isAuthenticated: s.isAuthenticated,
+    loginFn: s.loginFn
+  }));
 
   const submitReport = async (values: any) => {
     if (!isAuthenticated) {
@@ -55,8 +63,13 @@ export const NFTReportPopup = ({ isOpen, onClose, collectionAddress, tokenId }: 
         setReportStatus(ReportState.SUCCESS);
         onModalClose();
         return;
+      } else {
+        setError(true);
+        setReportStatus(ReportState.HIDDEN);
+        console.error(response);
       }
     } catch(err) {
+      setError(true);
       setReportStatus(ReportState.HIDDEN);
       console.log(err);
     }
@@ -75,6 +88,8 @@ export const NFTReportPopup = ({ isOpen, onClose, collectionAddress, tokenId }: 
     validationSchema: ReportPopupValidationSchema,
     onSubmit: submitReport,
   });
+
+  const [error, setError] = useState(false);
   
   const clearValues = () => {
     formik.resetForm();
@@ -95,11 +110,7 @@ export const NFTReportPopup = ({ isOpen, onClose, collectionAddress, tokenId }: 
       <Modal isOpen={isOpen} onClose={onModalClose} closeOnEsc={false} closeOnOverlayClick={false}>
         <ModalOverlay />
         <ModalContent maxW={'480px'}>
-          <ModalHeader sx={{
-            pt: '40px !important',
-            pb: '20px !important',
-            textAlign: 'center',
-          }}>Report this item</ModalHeader>
+          <ModalHeader {...styles.ModalHeaderStyle}>Report this item</ModalHeader>
           <ModalCloseButton />
           <ModalBody pt={'0 !important'}>
             <Text sx={{
@@ -108,6 +119,17 @@ export const NFTReportPopup = ({ isOpen, onClose, collectionAddress, tokenId }: 
               mb: '30px',
               w: '300px',
             }}>Explain why you think this item should be removed from marketplace</Text>
+
+            {error && (
+              <Box {...styles.ErrorStyle}>
+                <Text>
+                  <Icon viewBox='0 0 25 25' color='#FF4949' boxSize={'22px'} mr={'10px'} mt={'-3px'}>
+                    <WarningSVG />
+                  </Icon>
+                  Something went wrong! Please try again.
+                </Text>
+              </Box>
+            )}
 
             <FormControl mb={'20px'}>
               <FormLabel>Reason</FormLabel>
@@ -152,7 +174,7 @@ export const NFTReportPopup = ({ isOpen, onClose, collectionAddress, tokenId }: 
 
             <Flex justifyContent={'center'}>
               <Button variant={'outline'} mr={'15px'} onClick={onModalClose}>Cancel</Button>
-              <Button disabled={!formik.isValid} boxShadow={'lg'} onClick={() => formik.submitForm()}>Report</Button>
+              <Button boxShadow={'lg'} onClick={() => formik.submitForm()}>Report</Button>
             </Flex>
           </ModalBody>
         </ModalContent>
