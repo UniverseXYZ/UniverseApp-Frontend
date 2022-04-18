698406b532d5a02480722043602bfd4ca559fc77
vikinatora <vikinatora@gmail.com>
2022-04-18 17:09:55 +0300

    improve(zustand): upgrade theme store

--- src/stores/themeStore.ts
+++ src/stores/themeStore.ts
@@ -24,3 +24,8 @@ export const useThemeStore = create<
     }))
   }
 })))
+
+useThemeStore.subscribe(s => s.darkMode, () => {
+  const darkMode = useThemeStore.getState().darkMode;
+  console.log(`dark mode: ${darkMode}`);
+})
