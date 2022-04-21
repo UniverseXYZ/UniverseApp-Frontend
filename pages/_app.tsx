9c570c8fab6bb067506d414af075f944c4acee6c
vikinatora <vikinatora@gmail.com>
2022-04-21 11:25:05 +0300

    feat(next-js): add loader for pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,156 +1,146 @@
-import '../styles/globals.css'
-
-import '../src/assets/scss/normalize.scss';
-import '../src/assets/scss/_variables.scss';
-
-import '../public/fonts/style.css';
-
-import 'react-datepicker/dist/react-datepicker.css'
-
-import '../src/components/input/Inputs.scss';
-import '../src/components/header/Header.scss';
-import '../src/components/header/dimensions/desktop/DesktopView.scss';
-import '../src/components/header/dimensions/mobile/MobileView.scss';
-import '../src/components/header/dimensions/tablet/TabletView.scss';
-import '../src/components/footer/Footer.scss';
-import '../src/components/button/Button.scss';
-import '../src/components/polymorphs/styles/About.scss';
-import '../src/components/polymorphs/styles/Characters.scss';
-import '../src/components/polymorphs/styles/PolymorphsActivity.scss';
-import '../src/components/polymorphs/styles/PolymorphsActivityTableRow.scss';
-import '../src/components/polymorphs/styles/PolymorphsActivityTableRowMobile.scss';
-import '../src/components/polymorphs/styles/Section4.scss';
-import '../src/components/polymorphs/styles/Section6.scss';
-import '../src/components/polymorphs/styles/WrapperCenter.scss';
-import '../src/components/polymorphs/styles/WrapperCenterTwoColumns.scss';
-import '../src/components/ui-elements/styles/WelcomeWrapper.scss';
-import '../src/components/pagination/Pagination.scss';
-import '../src/components/polymorphUniverse/aboutSection/AboutSection.scss';
-import '../src/components/polymorphUniverse/battlePolymorphSection/BattlePolymorphSection.scss';
-import '../src/components/polymorphUniverse/heroSection/HeroSection.scss';
-import '../src/components/polymorphUniverse/latestFeaturesSection/LatestFeaturesSection.scss';
-import '../src/components/notFound/NotFound.scss';
-import '../src/components/input/SearchField.scss';
-import '../src/components/input/RaritySortByOrder.scss';
-import '../src/components/input/RaritySortBySelect.scss';
-import '../src/components/rarityCharts/filters/RarityFilters.scss';
-import '../src/components/rarityCharts/list/RarityList.scss';
-import '../src/components/rarityCharts/welcome/Welcome.scss';
-import '../src/components/myProfile/styles/CommunitieItem.scss';
-import '../src/components/myProfile/styles/CoverPhoto.scss';
-import '../src/components/myProfile/styles/GalleryPhotoItem.scss';
-import '../src/components/myProfile/styles/GallerySection.scss';
-import '../src/components/myProfile/styles/MyCommunities.scss';
-import '../src/components/myProfile/styles/MyProfileContainer.scss';
-import '../src/components/myProfile/styles/RecentActivity.scss';
-import '../src/components/myProfile/styles/RightBlockTopLinks.scss';
-import '../src/components/myProfile/styles/Tabs.scss';
-import '../src/components/myProfile/styles/Transactions.scss';
-import '../src/components/myProfile/styles/UserDataBlock.scss';
-import '../src/components/popups/PopupStyle.scss';
-import '../src/components/myNFTs/MyNFTs.scss';
-import '../src/components/products/minting/Minting.scss';
-import '../src/components/general/LoadingImage.scss';
-import '../src/components/input/SearchField.scss';
-import '../src/components/input/SearchTokenIdField.scss';
-import '../src/components/marketplace/browseNFT/NFTsList.scss';
-import '../src/components/marketplaceNFT/BrokenNFT.scss';
-import '../src/components/myNFTs/UniverseNFTs.scss';
-import '../src/components/myNFTs/pendingDropdown/pendingAccordion/PendingAccordion.scss';
-import '../src/components/myNFTs/pendingDropdown/pendingCollections/PendingCollections.scss';
-import '../src/components/myNFTs/pendingDropdown/pendingNFTs/PendingNFTs.scss';
-import '../src/components/nft/NFTCard.scss';
-import '../src/components/nft/SearchFilters.scss';
-import '../src/components/rarityCharts/filters/LobsterRarityFilters.scss';
-
-import '../src/containers/homepage/Homepage.scss';
-import '../src/containers/products/about/About.scss';
-import '../src/containers/team/Team.scss'
-import '../src/containers/polymorphs/Polymorphs.scss';
-import '../src/containers/polymorphUniverse/PolymorphUniverse.scss';
-import '../src/containers/rarityCharts/RarityCharts.scss';
-import '../src/containers/rarityCharts/RarityCharsLoader.scss';
-import '../src/containers/myProfile/MyProfile';
-import '../src/containers/myAccount/MyAccount.scss';
-import '../src/containers/rarityCharts/PolymorphRarityCharts.scss';
-import '../src/containers/rarityCharts/LobsterRarityCharts.scss';
-
-import '../src/components/rarityCharts/list/RarityLobsterList.scss';
-import '../src/components/skeletons/nftCardSkeleton/NftCardSkeleton.scss';
-import '../src/components/tabs/Tabs.scss';
-import '../src/containers/rarityCharts/RarityCharts.scss';
-import '../src/containers/rarityCharts/RarityChartsLoader';
-import '../src/containers/marketplaceNFT/MarketplaceNFT.scss';
-import '../src/components/myNFTs/create/CreateNFT.scss';
-import '../src/components/myNFTs/create/CreateSingleNft.scss';
-import '../src/components/nft/SearchFilters.scss';
-import '../src/containers/collection/Collection.scss';
-import '../src/components/lobbyLobsters/donate/Donate.scss';
-import '../src/components/lobbyLobsters/helpUs/HelpUsBeHeard.scss';
-import '../src/components/lobbyLobsters/heroSection/HeroSection.scss';
-import '../src/components/lobbyLobsters/mintSection/MintLobbyLobsterSection.scss';
-import '../src/components/lobbyLobsters/sliderSection/SliderSection.scss';
-import '../src/components/polymorphs/mint-polymorph/styles/BondingCurve.scss';
-import '../src/components/popups/PopupStyle.scss';
-import '../src/components/popups/LobsterLoader.scss';
-import '../src/components/ui-elements/styles/HorizontalSlider.scss';
-import '../src/components/ui-elements/styles/QuantityUpDownGroup.scss';
-import '../src/components/auctionsCard/ActiveAuctionsCard.scss';
-import '../src/components/auctionsCard/FutureAuctionsCard.scss';
-import '../src/containers/artist/Artist.scss';
-import '../src/components/polymorphs/scramble/styles/PolymorphScramblePage.scss';
-import '../src/components/polymorphs/scramble/styles/PolymorphScrambleProp.scss';
-import '../src/components/select/SelectComponent.scss';
-import '../src/components/lobbyLobsters/info/styles/LobstersInfoPage.scss';
-import '../src/components/marketplaceTabComponents/MarketplaceTabComponent.scss';
-
-import '../src/components/badge/Badge.scss';
-import '../src/components/nft/NFTCard.scss';
-import '../src/components/popups/transferNFT/TransferNFTPopup.scss';
-import '../src/app/modules/account/pages/my-nfts-page/components/search-filters/search-filters/SearchFilters.scss';
-import '../src/app/modules/account/pages/my-nfts-page/components/search-filters/search-nft-filed/SearchField.scss';
-import '../src/app/modules/marketplace/pages/sell-page/components/tab-summary/SummaryTab.scss';
-import '../src/app/modules/nft/pages/nft-page/components/nft-info/components/tab-properties/TabProperties.scss';
-import '../src/components/skeletons/collectionCardSkeleton/CollectionCardSkeleton.scss';
-import '../src/containers/collection/Collection.scss';
-import '../src/components/myNFTs/revenueSplits/RevenueSplits.scss';
-import '../src/components/myNFTs/socialConnections/SocialConnections.scss';
-
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-import type { AppProps } from 'next/app'
-import React, { useCallback, useEffect, useState } from 'react';
-import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
-
-import { AuctionContextProvider } from '../src/contexts/AuctionContext';
-import Header from '../src/components/header/Header';
-import Footer from '../src/components/footer/Footer';
-import { Popups } from '../src/app/components/AppPopups';
-import { Theme } from '../src/app/theme';
-import { LayoutProvider } from '../src/app/providers';
-import { useErc20PriceStore } from '../src/stores/erc20PriceStore';
-import { useAuthStore } from '../src/stores/authStore';
-import Cookies from 'js-cookie';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import Cookies from "js-cookie";
+import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
+import React, { useCallback, useEffect, useState } from "react";
+import "react-datepicker/dist/react-datepicker.css";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "../public/fonts/style.css";
+import { Popups } from "../src/app/components/AppPopups";
+import "../src/app/modules/account/pages/my-nfts-page/components/search-filters/search-filters/SearchFilters.scss";
+import "../src/app/modules/account/pages/my-nfts-page/components/search-filters/search-nft-filed/SearchField.scss";
+import "../src/app/modules/marketplace/pages/sell-page/components/tab-summary/SummaryTab.scss";
+import "../src/app/modules/nft/pages/nft-page/components/nft-info/components/tab-properties/TabProperties.scss";
+import { LayoutProvider } from "../src/app/providers";
+import { Theme } from "../src/app/theme";
+import "../src/assets/scss/normalize.scss";
+import "../src/assets/scss/_variables.scss";
+import "../src/components/auctionsCard/ActiveAuctionsCard.scss";
+import "../src/components/auctionsCard/FutureAuctionsCard.scss";
+import "../src/components/badge/Badge.scss";
+import "../src/components/button/Button.scss";
+import Footer from "../src/components/footer/Footer";
+import "../src/components/footer/Footer.scss";
+import "../src/components/general/LoadingImage.scss";
+import "../src/components/header/dimensions/desktop/DesktopView.scss";
+import "../src/components/header/dimensions/mobile/MobileView.scss";
+import "../src/components/header/dimensions/tablet/TabletView.scss";
+import Header from "../src/components/header/Header";
+import "../src/components/header/Header.scss";
+import "../src/components/input/Inputs.scss";
+import "../src/components/input/RaritySortByOrder.scss";
+import "../src/components/input/RaritySortBySelect.scss";
+import "../src/components/input/SearchField.scss";
+import "../src/components/input/SearchTokenIdField.scss";
+import "../src/components/lobbyLobsters/donate/Donate.scss";
+import "../src/components/lobbyLobsters/helpUs/HelpUsBeHeard.scss";
+import "../src/components/lobbyLobsters/heroSection/HeroSection.scss";
+import "../src/components/lobbyLobsters/info/styles/LobstersInfoPage.scss";
+import "../src/components/lobbyLobsters/mintSection/MintLobbyLobsterSection.scss";
+import "../src/components/lobbyLobsters/sliderSection/SliderSection.scss";
+import "../src/components/marketplace/browseNFT/NFTsList.scss";
+import "../src/components/marketplaceNFT/BrokenNFT.scss";
+import "../src/components/marketplaceTabComponents/MarketplaceTabComponent.scss";
+import "../src/components/myNFTs/create/CreateNFT.scss";
+import "../src/components/myNFTs/create/CreateSingleNft.scss";
+import "../src/components/myNFTs/MyNFTs.scss";
+import "../src/components/myNFTs/pendingDropdown/pendingAccordion/PendingAccordion.scss";
+import "../src/components/myNFTs/pendingDropdown/pendingCollections/PendingCollections.scss";
+import "../src/components/myNFTs/pendingDropdown/pendingNFTs/PendingNFTs.scss";
+import "../src/components/myNFTs/revenueSplits/RevenueSplits.scss";
+import "../src/components/myNFTs/socialConnections/SocialConnections.scss";
+import "../src/components/myNFTs/UniverseNFTs.scss";
+import "../src/components/myProfile/styles/CommunitieItem.scss";
+import "../src/components/myProfile/styles/CoverPhoto.scss";
+import "../src/components/myProfile/styles/GalleryPhotoItem.scss";
+import "../src/components/myProfile/styles/GallerySection.scss";
+import "../src/components/myProfile/styles/MyCommunities.scss";
+import "../src/components/myProfile/styles/MyProfileContainer.scss";
+import "../src/components/myProfile/styles/RecentActivity.scss";
+import "../src/components/myProfile/styles/RightBlockTopLinks.scss";
+import "../src/components/myProfile/styles/Tabs.scss";
+import "../src/components/myProfile/styles/Transactions.scss";
+import "../src/components/myProfile/styles/UserDataBlock.scss";
+import "../src/components/nft/NFTCard.scss";
+import "../src/components/nft/SearchFilters.scss";
+import "../src/components/notFound/NotFound.scss";
+import "../src/components/pagination/Pagination.scss";
+import "../src/components/polymorphs/mint-polymorph/styles/BondingCurve.scss";
+import "../src/components/polymorphs/scramble/styles/PolymorphScramblePage.scss";
+import "../src/components/polymorphs/scramble/styles/PolymorphScrambleProp.scss";
+import "../src/components/polymorphs/styles/About.scss";
+import "../src/components/polymorphs/styles/Characters.scss";
+import "../src/components/polymorphs/styles/PolymorphsActivity.scss";
+import "../src/components/polymorphs/styles/PolymorphsActivityTableRow.scss";
+import "../src/components/polymorphs/styles/PolymorphsActivityTableRowMobile.scss";
+import "../src/components/polymorphs/styles/Section4.scss";
+import "../src/components/polymorphs/styles/Section6.scss";
+import "../src/components/polymorphs/styles/WrapperCenter.scss";
+import "../src/components/polymorphs/styles/WrapperCenterTwoColumns.scss";
+import "../src/components/polymorphUniverse/aboutSection/AboutSection.scss";
+import "../src/components/polymorphUniverse/battlePolymorphSection/BattlePolymorphSection.scss";
+import "../src/components/polymorphUniverse/heroSection/HeroSection.scss";
+import "../src/components/polymorphUniverse/latestFeaturesSection/LatestFeaturesSection.scss";
+import "../src/components/popups/LobsterLoader.scss";
+import "../src/components/popups/PopupStyle.scss";
+import "../src/components/popups/transferNFT/TransferNFTPopup.scss";
+import "../src/components/products/minting/Minting.scss";
+import "../src/components/rarityCharts/filters/LobsterRarityFilters.scss";
+import "../src/components/rarityCharts/filters/RarityFilters.scss";
+import "../src/components/rarityCharts/list/RarityList.scss";
+import "../src/components/rarityCharts/list/RarityLobsterList.scss";
+import "../src/components/rarityCharts/welcome/Welcome.scss";
+import "../src/components/select/SelectComponent.scss";
+import "../src/components/skeletons/collectionCardSkeleton/CollectionCardSkeleton.scss";
+import "../src/components/skeletons/nftCardSkeleton/NftCardSkeleton.scss";
+import "../src/components/tabs/Tabs.scss";
+import "../src/components/ui-elements/styles/HorizontalSlider.scss";
+import "../src/components/ui-elements/styles/QuantityUpDownGroup.scss";
+import "../src/components/ui-elements/styles/WelcomeWrapper.scss";
+import "../src/containers/artist/Artist.scss";
+import "../src/containers/collection/Collection.scss";
+import "../src/containers/homepage/Homepage.scss";
+import "../src/containers/marketplaceNFT/MarketplaceNFT.scss";
+import "../src/containers/myAccount/MyAccount.scss";
+import "../src/containers/myProfile/MyProfile";
+import "../src/containers/polymorphs/Polymorphs.scss";
+import "../src/containers/polymorphUniverse/PolymorphUniverse.scss";
+import "../src/containers/products/about/About.scss";
+import "../src/containers/rarityCharts/LobsterRarityCharts.scss";
+import "../src/containers/rarityCharts/PolymorphRarityCharts.scss";
+import "../src/containers/rarityCharts/RarityCharsLoader.scss";
+import "../src/containers/rarityCharts/RarityCharts.scss";
+import "../src/containers/rarityCharts/RarityChartsLoader";
+import "../src/containers/team/Team.scss";
+import { AuctionContextProvider } from "../src/contexts/AuctionContext";
+import { useAuthStore } from "../src/stores/authStore";
+import { useErc20PriceStore } from "../src/stores/erc20PriceStore";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      }
-    },
-  }));
-  
-  const fetchPrices = useErc20PriceStore(s => useCallback(s.fetchPrices, []));
-  const setProviderName = useAuthStore(s => useCallback(s.setProviderName, []));
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+
+  const fetchPrices = useErc20PriceStore((s) => useCallback(s.fetchPrices, []));
+  const setProviderName = useAuthStore((s) =>
+    useCallback(s.setProviderName, [])
+  );
 
   useEffect(() => {
     fetchPrices();
 
-    // Refetch erc20 prices every 60 seconds  
+    // Refetch erc20 prices every 60 seconds
     const priceInterval = setInterval(() => {
       fetchPrices();
     }, 60000);
@@ -160,25 +150,26 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   // SSR causes a mismatch between providerName if we set it in authStore as default value
   useEffect(() => {
-    const providerName = Cookies.get('providerName');
+    const providerName = Cookies.get("providerName");
     if (providerName) {
       setProviderName(providerName);
     }
-  }, [])
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
         <AuctionContextProvider>
-            <LayoutProvider>
-              <Theme>
-                <div id="root">
-                  <Header />
-                  <Component {...pageProps} />
-                  <Footer />
-                  <Popups />
-                </div>
-              </Theme>
-            </LayoutProvider>
+          <LayoutProvider>
+            <Theme>
+              <div id="root">
+                <NextNProgress color="linear-gradient(135deg, rgb(188, 235, 0) 15.57%, rgb(0, 234, 234) 84.88%)" />
+                <Header />
+                <Component {...pageProps} />
+                <Footer />
+                <Popups />
+              </div>
+            </Theme>
+          </LayoutProvider>
         </AuctionContextProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </Hydrate>
@@ -186,4 +177,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp
+export default MyApp;
