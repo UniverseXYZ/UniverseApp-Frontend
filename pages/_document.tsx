d1103030c5ed69ec9204fd7ec2238435f6b5bc6c
vikinatora <vikinatora@gmail.com>
2022-05-03 17:23:10 +0300

    add space grotesk font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,7 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
           <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
         <Main />
