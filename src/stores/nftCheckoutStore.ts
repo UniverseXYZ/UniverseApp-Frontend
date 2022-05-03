57078694c2b49a1f49ab59cfa1ae7eb6d0c61d0b
vikinatora <vikinatora@gmail.com>
2022-05-03 17:23:11 +0300

    feat(zustand-fix): add 2 more missing stores (loadingStore, nftCheckoutStore)

---
+++ src/stores/nftCheckoutStore.ts
@@ -0,0 +1,74 @@
+import { ICollection, INFT, IOrder } from "@app/modules/nft/types";
+import create from "zustand"
+
+type INftCheckoutStore = {
+  NFT: INFT,
+  setNFT: (NFT: INFT) => void,
+  collection: ICollection,
+  setCollection: (collection: ICollection) => void,
+  NFTs: INFT[],
+  setNFTs: (nfts: INFT[]) => void,
+  order: IOrder,
+  setOrder: (order: IOrder) => void,
+  isOpen: boolean,
+  setIsOpen: (isOpen: boolean) => void,
+  onClose: () => void,
+  setOnClose: (onClose: () => void) => void,
+  closeCheckout: () => void;
+}
+
+export const useNftCheckoutStore = create<INftCheckoutStore>((set, get) => ({
+  NFT: {} as INFT,
+  collection: {} as ICollection,
+  NFTs: [],
+  order: {} as IOrder,
+  isOpen: false,
+  closeCheckout: () => {
+    set(state => ({
+      ...state,
+      isOpen: false,
+      NFT: {} as INFT,
+      collection: {} as ICollection,
+      NFTs: [],
+      order: {} as IOrder,
+      onClose: () => {}
+    }))
+  },
+  onClose: () => {},
+  setNFT: (NFT) => {
+    set(state => ({
+      ...state,
+      NFT
+    }))
+  },
+  setNFTs: (NFTs) => {
+    set(state => ({
+      ...state,
+      NFTs
+    }))
+  },
+  setCollection: (collection) => {
+    set(state => ({
+      ...state,
+      collection
+    }))
+  },
+  setOrder: (order) => {
+    set(state => ({
+      ...state,
+      order
+    }))
+  },
+  setIsOpen: (isOpen) => {
+    set(state => ({
+      ...state,
+      isOpen
+    }))
+  },
+  setOnClose: (onClose) => {
+    set(state => ({
+      ...state,
+      onClose
+    }))
+  },
+}))
