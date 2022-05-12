9d5b547a0ef094df5048912ecaff91d32f40c10f
taskudis <85156108+taskudis@users.noreply.github.com>
2022-05-12 15:51:39 +0300

    Merge pull request #1603 from UniverseXYZ/eduardmoiseenkov5271/sc-4189
    Show "Copied" message on click on collection address

--- src/app/modules/nft/components/nft-card/components/nft-card-content/NFTCardContent.tsx
+++ src/app/modules/nft/components/nft-card/components/nft-card-content/NFTCardContent.tsx
@@ -1,12 +1,12 @@
 import { CollectionPreview, TokenIcon } from "@app/components";
 import { getTokenByAddress, TOKENS_MAP } from "@app/constants";
 import { TokenTicker } from "@app/enums";
-import { Box, Flex, HStack, Link, Text } from "@chakra-ui/react";
+import { Box, Flex, HStack, Link, Text, Tooltip } from "@chakra-ui/react";
 import { utils } from "ethers";
 import NextLink from "next/link";
 import React, { useMemo } from "react";
 import { ICollection } from "../../../../../collection/types";
-import { IERC20AssetType, IOrder } from "../../../../types";
+import { IERC20AssetType, INFT, IOrder } from "../../../../types";
 import { formatPrice, formatSecondaryPrice } from "./helpers";
 import * as styles from "./NFTCardContent.styles";
 
@@ -83,7 +83,11 @@ export const NFTCardContent = (props: INFTCardContentProps) => {
         <Text {...styles.NFTName}>{NFT.name}</Text>
         {order && (
           <HStack spacing={"4px"}>
-            <TokenIcon ticker={priceToken.ticker} boxSize={"20px"} />
+            <Tooltip label={priceToken.ticker} hasArrow placement={"top"}>
+              <Box>
+                <TokenIcon ticker={priceToken.ticker} boxSize={"20px"} />
+              </Box>
+            </Tooltip>
             {/*TODO: provide text "top bit" | "min bit" in case of auction*/}
             <Text>{formatPrice(price)}</Text>
           </HStack>
@@ -115,7 +119,11 @@ export const NFTCardContent = (props: INFTCardContentProps) => {
           {additionPriceValue && (
             <HStack spacing={"4px"} fontSize={"11px"} fontWeight={600}>
               <Text color={"#00000066"}>{additionPriceLabel}</Text>
-              <TokenIcon ticker={additionPriceToken} boxSize={"16px"} />
+              <Tooltip label={additionPriceToken} hasArrow placement={"top"}>
+                <Box>
+                  <TokenIcon ticker={additionPriceToken} boxSize={"16px"} />
+                </Box>
+              </Tooltip>
               <Text color={"black"}>
                 {formatSecondaryPrice(additionPriceValue)}
               </Text>
