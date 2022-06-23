25cb2051023bfa6e6de753d2c76890ed88923ad0
IvanShterev <schterev11@gmail.com>
2022-06-23 12:52:47 +0300

    added error handling on get coingecko method

--- src/app/modules/nft/pages/nft-page/components/nft-make-an-offer-popup/NFTMakeAnOfferPopup.tsx
+++ src/app/modules/nft/pages/nft-page/components/nft-make-an-offer-popup/NFTMakeAnOfferPopup.tsx
@@ -240,6 +240,11 @@ export const NFTMakeAnOfferPopup: React.FC = () => {
     ["tokenPrice", formik.values?.token],
     async () => {
       const response = await getTokenPriceCoingecko(TOKENS_MAP[formik.values?.token?.ticker].coingeckoId);
+
+      if (!response) {
+        throw new Error('404');
+      }
+
       return response['usd'];
     }
   );
