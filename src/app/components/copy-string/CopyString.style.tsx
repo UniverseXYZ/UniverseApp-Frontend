24ba63ba1e21ed60fb1b56b635001fc534215dc2
nadezhda <nadezhda@devlabs.bg>
2022-06-29 18:02:37 +0300

    Added resuable copy string component

---
+++ src/app/components/copy-string/CopyString.style.tsx
@@ -0,0 +1,10 @@
+import { BoxProps } from '@chakra-ui/react';
+
+export const CopyWrapperStyle: BoxProps = {
+  borderRadius: '10px',
+  padding: '6px 8px',
+  _hover: {
+    bg: 'rgba(0, 0, 0, 0.05)',
+  }
+}
+
