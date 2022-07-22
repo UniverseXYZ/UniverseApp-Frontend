5b74a03f479fa0be712ee6a203edcc18dde1920c
taskudis <85156108+taskudis@users.noreply.github.com>
2022-07-22 15:56:05 +0300

    feat: Introduce Base Maintenance Component
    - Update the desired routes to used it in  App.js

---
+++ src/app/components/maintanance/Maintanance.tsx
@@ -0,0 +1,14 @@
+export const Maintanance = () => (
+  <div  style={{background: 'green', height: '100%'}}>
+    <p>Maintenance</p>
+    <p>Maintenance</p>
+    <p>Maintenance</p>
+    <p>Maintenance</p>
+    <p>Maintenance</p>
+    <p>Maintenance</p>
+    <p>Maintenance</p>
+    <p>Maintenance</p>
+    <p>Maintenance</p>
+    <p>Maintenance</p>
+  </div>
+);
